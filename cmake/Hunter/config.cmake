5a630724339d3999b479797aced90fb70fd30e07
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2020-08-04 10:34:33 +0200

    Add Hunter config at top level * repoint FILEPATH to local hunter config * fix ethash version 0.5.2 which fixes https://github.com/chfast/ethash/issues/154

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,29 @@
+#[[
+   Copyright 2020 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+]]
+
+hunter_config(
+    ethash
+    VERSION 0.5.2
+    URL https://github.com/chfast/ethash/archive/v0.5.2.tar.gz
+    SHA1 8d2859a9d09721ed7007007bd59a77c0bd9b2bee
+)
+
+hunter_config(
+    intx
+    VERSION 0.5.0
+    URL https://github.com/chfast/intx/archive/v0.5.0.tar.gz
+    SHA1 7f917cfe9046278d348a5cf36c20f9847d351807
+)

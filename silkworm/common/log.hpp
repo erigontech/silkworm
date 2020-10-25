52f697fbf454bb5262ee73f2c4eac82a382cb363
Greg Colvin <greg@colvin.org>
2020-10-25 06:05:22 -0400

    quick and dirty logging (#54)
    * logging
    
    * get it up to snuff
    
    * test cases
    
    * attempt to fix put_time
    
    * add LogNone level
    
    * execute tests
    
    * debugging
    
    * make tags constexpr

---
+++ silkworm/common/log.hpp
@@ -0,0 +1,57 @@
+/*
+   Copyright 2020 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef SILKWORM_COMMON_LOG_H_
+#define SILKWORM_COMMON_LOG_H_
+
+#include "tee.hpp"
+
+#include <iomanip>
+
+namespace silkworm {
+
+enum LogLevels {
+    LogTrace, LogDebug, LogInfo, LogWarn, LogError, LogCrit, LogNone
+};
+
+// Log to two output streams - typically the console and a log file.
+class Logger {
+public:
+    Logger(std::ostream& o1, std::ostream& o2, LogLevels level=LogNone)
+    : stream(o1, o2), verbosity(level) {}
+
+    int level(LogLevels level) { return verbosity = level; }
+    int level() { return verbosity; }
+
+    std::ostream& log(LogLevels level, const char* file, int line){
+    std::time_t time = std::time(nullptr);
+    return stream << LogTags[level] \
+                  << std::put_time(std::gmtime(&time), " %Y-%m-%d %H:%M:%S %Z ") \
+                  << file << ":" << line << "| ";
+    }
+private:
+    teestream stream;
+    LogLevels verbosity;
+    static constexpr char const LogTags[7][6] = { "TRACE", "DEBUG", "INFO ", "WARN ", "ERROR", "CRIT ", "NONE" };
+};
+
+// log to whatever logger is in scope
+#define SILKWORM_LOG(level_) \
+    if (logger.level() < (level_)) {} \
+    else logger.log(level_, __FILE__, __LINE__)
+
+}
+#endif
---
+++ silkworm/common/log_test.cpp
@@ -0,0 +1,83 @@
+/*
+   Copyright 2020 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "log.hpp"
+#include "util.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <regex>
+
+#include <catch2/catch.hpp>
+
+namespace silkworm {
+
+namespace {
+    std::ostringstream stream1, stream2;
+    Logger logger(stream1, stream2);
+    const std::string infix(
+        "[A-Z ].\\d.\\d.\\d-\\d.-\\d.-\\d.\\d. \\d.\\d.:\\d.\\d.:\\d.\\d._UTC| "
+        "log_test.cpp:\\d*| ");
+
+    bool test_log(std::string prefix,std::string infix, std::string suffix)
+    {
+        std::string string1(stream1.str());
+        std::string string2(stream2.str());
+        stream1.clear();
+        stream1.str("");
+        stream2.clear();
+        stream2.str("");
+        if (string1 != string2) return false;
+
+        const std::string pattern = prefix + infix + suffix;
+        const std::regex rx(pattern);
+        if (!std::regex_search(string1, rx)) return false;
+
+        return true;
+    }
+}
+
+bool test_logging()
+{
+    // test true branch of macro
+    logger.level(LogCrit);
+    SILKWORM_LOG(LogCrit)  << "LogCrit"  << std::endl;
+    if (!test_log("CRIT ", infix, "LogCrit"))  return false;
+    SILKWORM_LOG(LogError) << "LogError" << std::endl;
+    if (!test_log("ERROR", infix, "LogError")) return false;
+    SILKWORM_LOG(LogWarn)  << "LogWarn"  << std::endl;
+    if (!test_log("WARN ", infix, "LogWarn"))  return false;
+    SILKWORM_LOG(LogInfo)  << "LogInfo"  << std::endl;
+    if (!test_log("INFO ", infix, "LogInfo"))  return false;
+    SILKWORM_LOG(LogDebug) << "LogDebug" << std::endl;
+    if (!test_log("DEBUG", infix, "LogDebug")) return false;
+    SILKWORM_LOG(LogTrace) << "LogTrace" << std::endl;
+    if (!test_log("TRACE", infix, "LogTrace")) return false;
+
+    // test false branch of macro
+    logger.level(LogTrace);
+    SILKWORM_LOG(LogDebug) << "LogDebug" << std::endl;
+    if (!test_log("", "", "")) return false;
+
+    return true;
+}
+
+TEST_CASE("Logging") {
+    CHECK(test_logging() == true);
+}
+
+}

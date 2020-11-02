db1bf69453c6d9bacda9987a841920150cab31f8
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2020-11-02 09:38:44 +0100

    Missing known tables

--- silkworm/db/tables.hpp
+++ silkworm/db/tables.hpp
@@ -26,91 +26,99 @@ see its common/dbutils/bucket.go.
 namespace silkworm::db::table {
 
 /* Canonical tables */
-constexpr lmdb::TableConfig kPlainState{"PLAIN-CST2", MDB_DUPSORT};
-constexpr lmdb::TableConfig kPlainContractCode{"PLAIN-contractCode"};
-constexpr lmdb::TableConfig kPlainAccountChangeSet{"PLAIN-ACS"};
-constexpr lmdb::TableConfig kPlainStorageChangeSet{"PLAIN-SCS"};
-constexpr lmdb::TableConfig kCurrentState{"CST2", MDB_DUPSORT};
-constexpr lmdb::TableConfig kAccountHistory{"hAT"};
-constexpr lmdb::TableConfig kStorageHistory{"hST"};
-constexpr lmdb::TableConfig kCode{"CODE"};
-constexpr lmdb::TableConfig kContractCode{"contractCode"};
-constexpr lmdb::TableConfig kIncarnationMap{"incarnationMap"};
 constexpr lmdb::TableConfig kAccountChangeSet{"ACS"};
-constexpr lmdb::TableConfig kStorageChangeSet{"SCS"};
-constexpr lmdb::TableConfig kIntermediateTrieHash{"iTh2", MDB_DUPSORT, lmdb::TableCustomKeyComparator::None,
-                                                  lmdb::TableCustomDupComparator::ExcludeSuffix32};
-constexpr lmdb::TableConfig kDatabaseInfo{"DBINFO"};
-constexpr lmdb::TableConfig kSnapshotInfo{"SNINFO"};
-constexpr lmdb::TableConfig kDatabaseVersion{"DatabaseVersion"};
-constexpr lmdb::TableConfig kBlockHeaders{"h"};
-constexpr lmdb::TableConfig kHeaderNumbers{"H"};
+constexpr lmdb::TableConfig kAccountHistory{"hAT"};
 constexpr lmdb::TableConfig kBlockBodies{"b"};
+constexpr lmdb::TableConfig kBlockHeaders{"h"};
 constexpr lmdb::TableConfig kBlockReceipts{"r"};
-constexpr lmdb::TableConfig kLogs{"log"};
-constexpr lmdb::TableConfig kLogTopicIndex{"log_topic_index"};
-constexpr lmdb::TableConfig kLogAddressIndex{"log_address_index"};
+constexpr lmdb::TableConfig kBloomBitsIndex{"iB"};
+constexpr lmdb::TableConfig kBloomBits{"B"};
 constexpr lmdb::TableConfig kCallFromIndex{"call_from_index"};
 constexpr lmdb::TableConfig kCallToIndex{"call_to_index"};
-constexpr lmdb::TableConfig kTxLookup{"l"};
-constexpr lmdb::TableConfig kBloomBits{"B"};
-constexpr lmdb::TableConfig kBloomBitsIndex{"iB"};
-constexpr lmdb::TableConfig kPreimage{"secure-key-"};
-constexpr lmdb::TableConfig kConfig{"ethereum-config-"};
-constexpr lmdb::TableConfig kSyncStageProgress{"SSP2"};
-constexpr lmdb::TableConfig kSyncStageUnwind{"SSU2"};
 constexpr lmdb::TableConfig kClique{"clique-"};
-constexpr lmdb::TableConfig kSenders{"txSenders"};
+constexpr lmdb::TableConfig kCode{"CODE"};
+constexpr lmdb::TableConfig kConfig{"ethereum-config-"};
+constexpr lmdb::TableConfig kContractCode{"contractCode"};
+constexpr lmdb::TableConfig kCurrentState{"CST2", MDB_DUPSORT};
+constexpr lmdb::TableConfig kDatabaseInfo{"DBINFO"};
+constexpr lmdb::TableConfig kDatabaseVersion{"DatabaseVersion"};
 constexpr lmdb::TableConfig kFastTrieProgress{"TrieSync"};
 constexpr lmdb::TableConfig kHeadBlock{"LastBlock"};
 constexpr lmdb::TableConfig kHeadFastBlock{"LastFast"};
 constexpr lmdb::TableConfig kHeadHeader{"LastHeader"};
+constexpr lmdb::TableConfig kHeaderNumbers{"H"};
+constexpr lmdb::TableConfig kIncarnationMap{"incarnationMap"};
+constexpr lmdb::TableConfig kIntermediateTrieHash{"iTh2", MDB_DUPSORT, lmdb::TableCustomKeyComparator::None,
+                                                  lmdb::TableCustomDupComparator::ExcludeSuffix32};
+constexpr lmdb::TableConfig kLogAddressIndex{"log_address_index"};
+constexpr lmdb::TableConfig kLogTopicIndex{"log_topic_index"};
+constexpr lmdb::TableConfig kLogs{"log"};
 constexpr lmdb::TableConfig kMigrations{"migrations"};
+constexpr lmdb::TableConfig kPlainAccountChangeSet{"PLAIN-ACS"};
+constexpr lmdb::TableConfig kPlainContractCode{"PLAIN-contractCode"};
+constexpr lmdb::TableConfig kPlainState{"PLAIN-CST2", MDB_DUPSORT};
+constexpr lmdb::TableConfig kPlainStorageChangeSet{"PLAIN-SCS"};
+constexpr lmdb::TableConfig kPreimage{"secure-key-"};
+constexpr lmdb::TableConfig kSenders{"txSenders"};
+constexpr lmdb::TableConfig kSnapshotInfo{"SNINFO"};
+constexpr lmdb::TableConfig kStorageChangeSet{"SCS"};
+constexpr lmdb::TableConfig kStorageHistory{"hST"};
+constexpr lmdb::TableConfig kSyncStageProgress{"SSP2"};
+constexpr lmdb::TableConfig kSyncStageUnwind{"SSU2"};
+constexpr lmdb::TableConfig kTxLookup{"l"};
 
 /* Deprecated Tables */
-constexpr lmdb::TableConfig kPlainStateOld1{"PLAIN-CST", MDB_DUPSORT};
 constexpr lmdb::TableConfig kCurrentStateOld1{"CST"};
+constexpr lmdb::TableConfig kIntermediateTrieHashOld1{"iTh"};
+constexpr lmdb::TableConfig kPlainStateOld1{"PLAIN-CST", MDB_DUPSORT};
 constexpr lmdb::TableConfig kSyncStageProgressOld1{"SSP"};
 constexpr lmdb::TableConfig kSyncStageUnwindOld1{"SSU"};
-constexpr lmdb::TableConfig kIntermediateTrieHashOld1{"iTh"};
 
-constexpr lmdb::TableConfig kTables[]{kPlainState,
-                                      kPlainContractCode,
-                                      kPlainAccountChangeSet,
-                                      kPlainStorageChangeSet,
-                                      kCurrentState,
-                                      kAccountHistory,
-                                      kStorageHistory,
-                                      kCode,
-                                      kContractCode,
-                                      kBlockHeaders,
-                                      kBlockBodies,
-                                      kBlockReceipts,
-                                      kSenders,
-                                      kIncarnationMap,
-                                      kAccountChangeSet,
-                                      kStorageChangeSet,
-                                      kIntermediateTrieHash,
-                                      kDatabaseInfo,
-                                      kDatabaseVersion,
-                                      kLogTopicIndex,
-                                      kLogAddressIndex,
-                                      kTxLookup,
-                                      kBloomBits,
-                                      kBloomBitsIndex,
-                                      kPreimage,
-                                      kConfig,
-                                      kSyncStageProgress,
-                                      kSyncStageUnwind,
-                                      kClique,
-                                      kMigrations,
-                                      kFastTrieProgress,
-                                      kHeadBlock,
-                                      kHeadFastBlock,
-                                      kHeadHeader};
+constexpr lmdb::TableConfig kTables[]{
+    kAccountChangeSet,
+    kAccountHistory,
+    kBlockBodies,
+    kBlockHeaders,
+    kBlockReceipts,
+    kBloomBits,
+    kBloomBitsIndex,
+    kCallFromIndex,
+    kCallToIndex,
+    kClique,
+    kCode,
+    kConfig,
+    kContractCode,
+    kCurrentState,
+    kDatabaseInfo,
+    kDatabaseVersion,
+    kFastTrieProgress,
+    kHeadBlock,
+    kHeadFastBlock,
+    kHeadHeader,
+    kHeaderNumbers,
+    kIncarnationMap,
+    kIntermediateTrieHash,
+    kLogAddressIndex,
+    kLogTopicIndex,
+    kLogs,
+    kMigrations,
+    kPlainAccountChangeSet,
+    kPlainContractCode,
+    kPlainState,
+    kPlainStorageChangeSet,
+    kPreimage,
+    kSenders,
+    kSnapshotInfo,
+    kStorageChangeSet,
+    kStorageHistory,
+    kSyncStageProgress,
+    kSyncStageUnwind,
+    kTxLookup,
+};
 
-constexpr lmdb::TableConfig kDeprecatedTables[]{kPlainStateOld1, kCurrentStateOld1, kSyncStageProgressOld1,
-                                                kSyncStageUnwindOld1, kIntermediateTrieHashOld1};
+constexpr lmdb::TableConfig kDeprecatedTables[]{
+    kCurrentStateOld1, kIntermediateTrieHashOld1, kPlainStateOld1, kSyncStageProgressOld1, kSyncStageUnwindOld1,
+};
 
 // Create all tables that do not yet exist.
 void create_all(lmdb::Transaction& txn);

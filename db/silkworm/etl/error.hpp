b9111f82a1740dd36f976ce10a9506fe96805125
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2020-12-21 08:50:36 +0100

    Missing license header

--- db/silkworm/etl/error.hpp
+++ db/silkworm/etl/error.hpp
@@ -1,6 +1,21 @@
+/*
+   Copyright 2020 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #ifndef SILKWORM_ETL_ERROR_H
 #define SILKWORM_ETL_ERROR_H
-
 
 #include <stdexcept>
 
@@ -12,4 +27,4 @@ class ETLError : public std::runtime_error {
 };
 
 }
-#endif
+#endif // !SILKWORM_ETL_ERROR_H

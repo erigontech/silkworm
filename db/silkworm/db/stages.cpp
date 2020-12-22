bdd528b4e17b35505ffa60c9a33313e5383c2e47
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2020-12-22 16:23:52 +0100

    Clang format stages.cpp

--- db/silkworm/db/stages.cpp
+++ db/silkworm/db/stages.cpp
@@ -20,20 +20,18 @@
 
 namespace silkworm::db::stages {
 
-    uint64_t get_stage_progress(std::unique_ptr<lmdb::Transaction>& txn, const char* stage_name)
-    {
-        MDB_val key{std::strlen(stage_name), (void*)stage_name};
-        auto data{txn->data_lookup(silkworm::db::table::kSyncStageProgress, &key)};
-        if (!data.has_value()) return 0;
-        return boost::endian::load_big_u64(data->c_str());
-    }
+uint64_t get_stage_progress(std::unique_ptr<lmdb::Transaction>& txn, const char* stage_name) {
+    MDB_val key{std::strlen(stage_name), (void*)stage_name};
+    auto data{txn->data_lookup(silkworm::db::table::kSyncStageProgress, &key)};
+    if (!data.has_value()) return 0;
+    return boost::endian::load_big_u64(data->c_str());
+}
 
-    void set_stage_progress(std::unique_ptr<lmdb::Transaction>& txn, const char* stage_name, uint64_t block_num)
-    {
-        Bytes value{ '\0', sizeof(uint64_t) };
-        boost::endian::store_big_u64(&value[0], block_num);
-        db::Entry entry{ {(uint8_t*)(stage_name), std::strlen(stage_name)}, {value} };
-        lmdb::err_handler(txn->data_upsert(silkworm::db::table::kSyncStageProgress, entry));
-    }
+void set_stage_progress(std::unique_ptr<lmdb::Transaction>& txn, const char* stage_name, uint64_t block_num) {
+    Bytes value{'\0', sizeof(uint64_t)};
+    boost::endian::store_big_u64(&value[0], block_num);
+    db::Entry entry{{(uint8_t*)(stage_name), std::strlen(stage_name)}, {value}};
+    lmdb::err_handler(txn->data_upsert(silkworm::db::table::kSyncStageProgress, entry));
+}
 
 }  // namespace silkworm::db::stages

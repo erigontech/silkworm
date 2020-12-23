9ebf30eadc13ab3015057706f1844404ab52a21c
yperbasis <andrey.ashikhmin@gmail.com>
2020-12-23 15:13:16 +0100

    Fix include dirs

--- db/silkworm/types/receipt_cbor.cpp
+++ db/silkworm/types/receipt_cbor.cpp
@@ -16,8 +16,8 @@
 
 #include "receipt_cbor.hpp"
 
-#include "cbor-cpp/src/encoder.h"
-#include "cbor-cpp/src/output_dynamic.h"
+#include <cbor/encoder.h>
+#include <cbor/output_dynamic.h>
 
 namespace silkworm {
 

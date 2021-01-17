2b88506d45e5e20c0c45dff1c4d3e0fd2c73100c
yperbasis <andrey.ashikhmin@gmail.com>
2021-01-17 17:45:19 +0100

    Fix ecdsa recover for Wasm

--- core/silkworm/crypto/ecdsa.cpp
+++ core/silkworm/crypto/ecdsa.cpp
@@ -22,8 +22,6 @@
 
 namespace silkworm::ecdsa {
 
-static secp256k1_context* kDefaultContext{secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY)};
-
 RecoveryId get_signature_recovery_id(const intx::uint256& v) {
     RecoveryId res{};
     if (v == 27 || v == 28) {
@@ -57,23 +55,26 @@ bool is_valid_signature(const intx::uint256& r, const intx::uint256& s, bool hom
 }
 
 std::optional<Bytes> recover(ByteView message, ByteView signature, uint8_t recovery_id) {
+    static secp256k1_context* context{secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY)};
+
     if (message.length() != 32 || signature.length() != 64) {
-        return {};
+        return std::nullopt;
     }
 
     secp256k1_ecdsa_recoverable_signature sig;
-    if (!secp256k1_ecdsa_recoverable_signature_parse_compact(kDefaultContext, &sig, &signature[0], recovery_id)) {
-        return {};
+    if (!secp256k1_ecdsa_recoverable_signature_parse_compact(context, &sig, &signature[0], recovery_id)) {
+        return std::nullopt;
     }
 
     secp256k1_pubkey pub_key;
-    if (!secp256k1_ecdsa_recover(kDefaultContext, &pub_key, &sig, &message[0])) {
-        return {};
+    if (!secp256k1_ecdsa_recover(context, &pub_key, &sig, &message[0])) {
+        return std::nullopt;
     }
 
     size_t kOutLen{65};
     Bytes out(kOutLen, '\0');
-    secp256k1_ec_pubkey_serialize(kDefaultContext, &out[0], &kOutLen, &pub_key, SECP256K1_EC_UNCOMPRESSED);
+    secp256k1_ec_pubkey_serialize(context, &out[0], &kOutLen, &pub_key, SECP256K1_EC_UNCOMPRESSED);
     return out;
 }
+
 }  // namespace silkworm::ecdsa

7f614514a4aa3fd7a7997a91da70a30a925341fb
Giulio Rebuffo <giuliorebuffo@MacBook-Pro-di-Giulio.local>
2021-01-24 16:08:34 +0100

    added unique ids to files

--- db/silkworm/etl/file_provider.cpp
+++ db/silkworm/etl/file_provider.cpp
@@ -23,7 +23,7 @@ namespace silkworm::etl {
 namespace fs = boost::filesystem;
 
 FileProvider::FileProvider(const std::string &working_path, size_t id) : id_{id} {
-    fs::path path{fs::path(working_path) / fs::path("tmp-" + std::to_string(id))};
+    fs::path path{fs::path(working_path) / fs::path("tmp-" + std::to_string(rand()))};
     file_name_ = path.string();
 }
 

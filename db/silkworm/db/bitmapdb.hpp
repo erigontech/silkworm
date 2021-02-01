9305f3e2c4f0794ec39bc89262b93497698f3d00
Giulio Rebuffo <giuliorebuffo@MacBook-Pro-di-Giulio.local>
2021-02-01 14:14:32 +0100

    added bitmapdb

---
+++ db/silkworm/db/bitmapdb.hpp
@@ -0,0 +1,6 @@
+#include "roaring64map.hh"
+
+namespace silkworm::bitmapdb{
+// Return value in bitmap that is higher than cap
+std::optional<uint64_t> seek_in_bitmap(roaring::Roaring64Map &bitmap, uint64_t cap);
+};

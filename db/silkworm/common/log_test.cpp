532bc65e0f8566b288250a7dfb0b9f66afe93643
Greg Colvin <greg@colvin.org>
2021-02-08 04:11:38 -0500

    Welcome to 2021!!! (#151)
    * Welcome to 2021!!!
    
    * Update copyright year
    
    Co-authored-by: Andrea Lanfranchi <andrea.lanfranchi@gmail.com>

--- db/silkworm/common/log_test.cpp
+++ db/silkworm/common/log_test.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2020 The Silkworm Authors
+   Copyright 2020 - 2021 The Silkworm Authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

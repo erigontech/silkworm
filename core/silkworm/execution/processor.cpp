d0edd868bf456e6d083c7cb257da8e5cda0caffe
yperbasis <andrey.ashikhmin@gmail.com>
2021-03-18 12:05:45 +0100

    kBlockGasLimitReached -> kBlockGasLimitExceeded

--- core/silkworm/execution/processor.cpp
+++ core/silkworm/execution/processor.cpp
@@ -49,7 +49,7 @@ ValidationResult ExecutionProcessor::validate_transaction(const Transaction& txn
         // Corresponds to the final condition of Eq (58) in Yellow Paper Section 6.2 "Execution".
         // The sum of the transaction’s gas limit and the gas utilized in this block prior
         // must be no greater than the block’s gas limit.
-        return ValidationResult::kBlockGasLimitReached;
+        return ValidationResult::kBlockGasLimitExceeded;
     }
 
     return ValidationResult::kOk;

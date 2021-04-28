58783be6f868d63a8785c5fccb0b8b4605615e18
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-04-28 13:36:27 +0200

    Prepare work for ethash implementation

--- ethash/ethash/hash_types.hpp
+++ ethash/ethash/hash_types.hpp
@@ -2,14 +2,47 @@
 // Copyright 2018-2019 Pawel Bylica.
 // Licensed under the Apache License, Version 2.0.
 
-#pragma once
+// Modified by Andrea Lanfranchi for Silkworm 2021:
+// type aliasing removed
 
-#include <ethash/hash_types.h>
+// #pragma once
+#ifndef ETHASH_HASH_TYPES_HPP_
+#define ETHASH_HASH_TYPES_HPP_
 
-namespace ethash
-{
-using hash256 = ethash_hash256;
-using hash512 = ethash_hash512;
-using hash1024 = ethash_hash1024;
-using hash2048 = ethash_hash2048;
+#include <stdint.h>
+
+namespace ethash {
+
+union hash256 {
+    uint64_t word64s[4];
+    uint32_t word32s[8];
+    uint8_t bytes[32];
+    char str[32];
+};
+
+union hash512 {
+    uint64_t word64s[8];
+    uint32_t word32s[16];
+    uint8_t bytes[64];
+    char str[64];
+};
+
+union hash1024 {
+    union hash512 hash512s[2];
+    uint64_t word64s[16];
+    uint32_t word32s[32];
+    uint8_t bytes[128];
+    char str[128];
+};
+
+union hash2048 {
+    union hash512 hash512s[4];
+    uint64_t word64s[32];
+    uint32_t word32s[64];
+    uint8_t bytes[256];
+    char str[256];
+};
+
 }  // namespace ethash
+
+#endif  // !ETHASH_HASH_TYPES_HPP_
--- ethash/ethash/keccak.hpp
+++ ethash/ethash/keccak.hpp
@@ -3,33 +3,28 @@
 // Licensed under the Apache License, Version 2.0.
 
 #pragma once
+#ifndef ETHASH_KECCAK_HPP_
+#define ETHASH_KECCAK_HPP_
 
-#include <ethash/keccak.h>
-#include <ethash/hash_types.hpp>
+#include "hash_types.hpp"
 
-namespace ethash
-{
-inline hash256 keccak256(const uint8_t* data, size_t size) noexcept
-{
-    return ethash_keccak256(data, size);
-}
+#if defined(_MSC_VER)
+#include <string.h>
+#define __builtin_memcpy memcpy
+#endif
 
-inline hash256 keccak256(const hash256& input) noexcept
-{
-    return ethash_keccak256_32(input.bytes);
-}
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define to_le64(X) __builtin_bswap64(X)
+#else
+#define to_le64(X) X
+#endif
 
-inline hash512 keccak512(const uint8_t* data, size_t size) noexcept
-{
-    return ethash_keccak512(data, size);
-}
 
-inline hash512 keccak512(const hash512& input) noexcept
-{
-    return ethash_keccak512_64(input.bytes);
-}
+namespace ethash {
 
-static constexpr auto keccak256_32 = ethash_keccak256_32;
-static constexpr auto keccak512_64 = ethash_keccak512_64;
+hash256 keccak256(const uint8_t* input, size_t input_size);
+hash256 keccak256(const hash256& input);
 
 }  // namespace ethash
+
+#endif  // !ETHASH_KECCAK_HPP_

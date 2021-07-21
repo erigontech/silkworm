d377c61d44ec7c709c8cdf75cfa9e9f427dd2775
yperbasis <andrey.ashikhmin@gmail.com>
2021-07-21 14:21:54 +0200

    Better comment formatting

--- core/silkworm/crypto/sha-256.c
+++ core/silkworm/crypto/sha-256.c
@@ -141,10 +141,15 @@ static bool calc_chunk(uint8_t chunk[CHUNK_SIZE], struct buffer_state *state) {
 static inline ALWAYS_INLINE void sha_256_implementation(uint32_t h[8], const void *input, size_t len) {
     /*
      * Note 1: All integers (expect indexes) are 32-bit unsigned integers and addition is calculated modulo 2^32.
-     * Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i], 0 = i
-     * = 63 Note 3: The compression function uses 8 working variables, a through h Note 4: Big-endian convention is used
-     * when expressing the constants in this pseudocode, and when parsing message block data from bytes to words, for
-     * example, the first word of the input message "abc" after padding is 0x61626380
+     *
+     * Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i],
+     * 0 = i = 63
+     *
+     * Note 3: The compression function uses 8 working variables, a through h
+     *
+     * Note 4: Big-endian convention is used when expressing the constants in this pseudocode,
+     *     and when parsing message block data from bytes to words, for example,
+     *     the first word of the input message "abc" after padding is 0x61626380
      */
 
     unsigned i, j;

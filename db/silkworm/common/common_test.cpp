26840efb1cb6cfdd0ec0311e09f839352dc4b414
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-07-23 13:03:47 +0200

    Introduce StopWatch and tests

---
+++ db/silkworm/common/common_test.cpp
@@ -0,0 +1,68 @@
+/*
+   Copyright 2020-2021 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+
+#include <catch2/catch.hpp>
+
+#include "stopwatch.hpp"
+
+namespace db {
+
+TEST_CASE("Stop Watch") {
+
+    using namespace std::chrono_literals;
+    silkworm::StopWatch sw1{};
+
+    auto [lap_time0, duration0] = sw1.lap();
+    CHECK(duration0.count() == 0);
+    CHECK(lap_time0 == silkworm::StopWatch::TimePoint());
+
+    auto start_time = sw1.start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    auto [lap_time1, duration1] = sw1.lap();
+    CHECK(duration1.count() >= 5 * 1000);
+    CHECK(start_time < lap_time1);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    auto [lap_time2, duration2] = sw1.lap();
+    CHECK(duration2.count() >= 10 * 1000);
+    CHECK(lap_time1 < lap_time2);
+
+    auto duration3 = sw1.since(lap_time2);
+    CHECK(duration3.count() == (duration1.count() + duration2.count()));
+
+    CHECK(sw1.laps().size() == 3);  // Start + 2 laps
+    for (auto& [t, _] : sw1.laps()) {
+        CHECK(t >= start_time);
+    }
+
+    CHECK(sw1.format(duration3) != "");
+    CHECK(sw1.format(918734032564785ns) == "10d 15h:12m:14.032s");
+    CHECK(sw1.format(432034ms) == "7m:12.034s");
+    CHECK(sw1.format(1ms) == "1ms");
+    CHECK(sw1.format(1200ms) == "1.200s");
+    CHECK(sw1.format(1200us) == "1.200ms");
+    CHECK(sw1.format(200us) == "200us");
+
+    CHECK(silkworm::StopWatch::format(918734032564785ns) == "10d 15h:12m:14.032s");
+
+}
+
+}  // namespace db

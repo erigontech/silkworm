f9103458110f7003f8bd39053650e313b5cce39d
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-08-12 18:26:14 +0200

    Embed initialize_with_genesis into dbtool

--- cmd/dbtool.cpp
+++ cmd/dbtool.cpp
@@ -14,6 +14,8 @@
    limitations under the License.
 */
 
+#include <assert.h>
+
 #include <csignal>
 #include <filesystem>
 #include <iostream>
@@ -26,9 +28,11 @@
 #include <magic_enum.hpp>
 
 #include <silkworm/chain/config.hpp>
+#include <silkworm/chain/genesis.h>
 #include <silkworm/common/data_dir.hpp>
 #include <silkworm/common/endian.hpp>
 #include <silkworm/db/access_layer.hpp>
+#include <silkworm/db/buffer.hpp>
 #include <silkworm/db/mdbx.hpp>
 #include <silkworm/db/stages.hpp>
 #include <silkworm/db/tables.hpp>
@@ -125,16 +129,6 @@ struct dbFreeInfo {
     size_t pages{0};
     size_t size{0};
     std::vector<dbFreeEntry> entries{};
-};
-
-struct DbOptions {
-    std::string datadir{DataDirectory{}.get_chaindata_path().string()};  // Where data file is located
-    bool shared{false};                                                  // Whether db has to be opened in shared mode
-};
-
-struct StageSetOptions {
-    std::string name{};  // Name of the stage to set;
-    uint32_t height{0};  // New height to set
 };
 
 void sig_handler(int signum) {
@@ -745,11 +739,189 @@ void do_copy(db::EnvConfig& src_config, std::string target_dir, bool create, boo
     std::cout << "\n All done!" << std::endl;
 }
 
+void do_init_genesis(DataDirectory& data_dir, std::string json_file, uint32_t chain_id, bool dry) {
+    // Check datadir does not exist
+    if (fs::exists(data_dir.get_base_path())) {
+        throw std::runtime_error("Provided data directory already exist");
+    }
+
+    // Ensure data directory tree is built
+    data_dir.create_tree();
+
+    // Retrieve source data either from provided json file
+    // or from embedded sources
+    std::string source_data;
+    if (!json_file.empty()) {
+        std::ifstream ifs(json_file);
+        source_data = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    } else {
+        switch (chain_id) {
+            case 1:
+                assert(sizeof_genesis_mainnet_data() != 0);
+                source_data.assign(genesis_mainnet_data(), sizeof_genesis_mainnet_data());
+                break;
+            case 4:
+                assert(sizeof_genesis_rinkeby_data() != 0);
+                source_data.assign(genesis_rinkeby_data(), sizeof_genesis_rinkeby_data());
+                break;
+            case 5:
+                assert(sizeof_genesis_goerli_data() != 0);
+                source_data.assign(genesis_goerli_data(), sizeof_genesis_goerli_data());
+                break;
+            default:
+                // TODO Configs for ETC and Ropsten
+                throw std::runtime_error("Unknown chain id: " + std::to_string(chain_id));
+        }
+    }
+
+    // Parse Json data
+    // N.B. = instead of {} initialization due to https://github.com/nlohmann/json/issues/2204
+    auto genesis_json = nlohmann::json::parse(source_data, nullptr, /* allow_exceptions = */ true);
+
+    // Sanity checks over collected data
+    std::string err{};
+    if (!genesis_json.contains("difficulty")) err.append("* Missing difficulty member\n");
+    if (!genesis_json.contains("nonce")) err.append("* Missing nonce member\n;");
+    if (!genesis_json.contains("gasLimit")) err.append("* Missing gasLimit member\n;");
+    if (!genesis_json.contains("timestamp")) err.append("* Missing timestamp member\n;");
+    if (!genesis_json.contains("extraData")) err.append("* Missing extraData member\n;");
+    if (!genesis_json.contains("config")) {
+        err.append("* Missing config member\n;");
+    } else {
+        if (!genesis_json["config"].is_object()) {
+            err.append("* Member config is not object");
+        } else {
+            if (genesis_json["config"].contains("ethash") &&
+                (!genesis_json.contains("mixhash") || !genesis_json["mixhash"].is_string())) {
+                err.append("* Missing mixhash member for ethash PoW chain");
+            }
+            const auto chain_config = ChainConfig::from_json(genesis_json["config"]);
+            if (!chain_config.has_value()) {
+                err.append("* Incomplete / Wrong genesis [config] member");
+            }
+        }
+    }
+    if (genesis_json.contains("alloc") && !genesis_json["alloc"].is_object()) {
+        err.append("* alloc member is not object");
+    }
+
+    if (!err.empty()) {
+        throw std::runtime_error("Parsing of json data failed. Examine following errors:\n" + err);
+    }
+
+    // Prime database
+    db::EnvConfig config{data_dir.get_chaindata_path().string(), /*create*/ true};
+    auto env{db::open_env(config)};
+    auto txn{env.start_write()};
+    db::table::create_all(txn);
+
+    // Initialize state_buffer for allocations (if any)
+    // and get root_hash
+    db::Buffer state_buffer(txn);
+
+    // Allocate accounts
+    if (genesis_json.contains("alloc")) {
+        auto expected_allocations{genesis_json["alloc"].size()};
+
+        for (auto& item : genesis_json["alloc"].items()) {
+            if (!item.value().is_object() || !item.value().contains("balance") ||
+                !item.value()["balance"].is_string()) {
+                throw std::invalid_argument("alloc address " + item.key() + " has badly formatted allocation");
+            }
+
+            auto address_bytes{from_hex(item.key())};
+            if (address_bytes == std::nullopt || address_bytes.value().length() != kAddressLength) {
+                throw std::invalid_argument("alloc address " + item.key() + " is not valid. Either not hex or not " +
+                                            std::to_string(kAddressLength) + " bytes");
+            }
+
+            evmc::address account_address = to_address(*address_bytes);
+            auto balance_str{item.value()["balance"].get<std::string>()};
+            Account account{0, intx::from_string<intx::uint256>(balance_str)};
+            state_buffer.update_account(account_address, std::nullopt, account);
+        }
+
+        auto applied_allocations{static_cast<size_t>(state_buffer.account_changes().at(0).size())};
+        if (applied_allocations != expected_allocations) {
+            // Maybe some account alloc has been inserted twice ?
+            std::cout << "Allocations expected " << expected_allocations << " applied " << applied_allocations
+                      << std::endl;
+            throw std::logic_error("Allocations mismatch. Check uniqueness of accounts");
+        }
+
+        state_buffer.write_to_db();
+    }
+
+    // Fill Header
+    BlockHeader header;
+
+    auto extra_data = from_hex(genesis_json["extraData"].get<std::string>());
+    if (extra_data.has_value()) {
+        header.extra_data = *extra_data;
+    }
+
+    if (genesis_json.contains("mixhash")) {
+        auto mixhash = from_hex(genesis_json["mixhash"].get<std::string>());
+        if (!mixhash.has_value() || mixhash->size() != kHashLength) {
+            throw std::invalid_argument("mixhash is not an hex hash");
+        }
+        std::memcpy(header.mix_hash.bytes, mixhash->data(), mixhash->size());
+    }
+
+    header.ommers_hash = kEmptyListHash;
+    header.state_root = state_buffer.state_root_hash();
+    header.transactions_root = kEmptyRoot;
+    header.receipts_root = kEmptyRoot;
+
+    auto difficulty_str{genesis_json["difficulty"].get<std::string>()};
+    header.difficulty = intx::from_string<intx::uint256>(difficulty_str);
+    header.gas_limit = std::stoull(genesis_json["gasLimit"].get<std::string>().c_str(), nullptr, 0);
+    header.timestamp = std::stoull(genesis_json["timestamp"].get<std::string>().c_str(), nullptr, 0);
+
+    auto nonce = std::stoull(genesis_json["nonce"].get<std::string>().c_str(), nullptr, 0);
+    std::memcpy(&header.nonce[0], &nonce, 8);
+
+    // Write header
+    auto block_hash{header.hash()};
+    auto block_key{db::block_key(0)};
+    const uint8_t genesis_null_receipts[] = {0xf6};  // <- cbor encoded
+
+    Bytes rlp_header, rlp_body;
+    rlp::encode(rlp_header, header);
+    rlp::encode(rlp_body, BlockBody{});
+
+    Bytes key(8 + kHashLength, '\0');
+    std::memcpy(&key[8], block_hash.bytes, kHashLength);
+    db::open_cursor(txn, db::table::kHeaders).upsert(db::to_slice(key), db::to_slice(rlp_header));
+    db::open_cursor(txn, db::table::kCanonicalHashes)
+        .upsert(db::to_slice(key), db::to_slice(full_view(block_hash.bytes)));
+
+    // Write body
+    db::open_cursor(txn, db::table::kBlockBodies).upsert(db::to_slice(key), db::to_slice(rlp_body));
+    db::open_cursor(txn, db::table::kDifficulty)
+        .upsert(db::to_slice(key), db::to_slice(intx::as_bytes(header.difficulty)));
+    db::open_cursor(txn, db::table::kBlockReceipts)
+        .upsert(db::to_slice(key).safe_middle(0, 8), db::to_slice(Bytes(genesis_null_receipts, 1)));
+    db::open_cursor(txn, db::table::kHeadHeader)
+        .upsert(mdbx::slice{db::table::kLastHeaderKey}, db::to_slice(full_view(block_hash.bytes)));
+    db::open_cursor(txn, db::table::kHeaderNumbers)
+        .upsert(db::to_slice(full_view(block_hash.bytes)), db::to_slice(key.substr(0, 8)));
+
+    // Write Chain Config
+    auto config_data{genesis_json["config"].dump()};
+    db::open_cursor(txn, db::table::kConfig)
+        .upsert(db::to_slice(full_view(block_hash.bytes)), mdbx::slice{config_data.c_str()});
+
+    if (!dry) {
+        txn.commit();
+    } else {
+        txn.abort();
+    }
+}
+
 int main(int argc, char* argv[]) {
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
-
-    StageSetOptions stage_set_opts{};  // Options for stage set
 
     CLI::App app_main("Silkworm db tool");
     app_main.require_subcommand(1);  // At least 1 subcommand is required
@@ -824,6 +996,17 @@ int main(int argc, char* argv[]) {
     auto cmd_stageset_height_opt =
         cmd_stageset->add_option("--height", "Name of the stage to set")->required()->check(CLI::Range(0u, UINT32_MAX));
 
+    // Initialize with genesis tool
+    auto cmd_initgenesis = app_main.add_subcommand("init-genesis", "Initialize a new db with genesis block");
+    cmd_initgenesis->require_option(1);
+    auto cmd_initgenesis_json_opt =
+        cmd_initgenesis->add_option("--json", "Full path to genesis json file")->check(CLI::ExistingFile);
+
+    std::map<std::string, uint32_t> genesis_map{{"mainnet", 1}, {"rinkeby", 4}, {"goerli", 5}};
+    auto cmd_initgenesis_chain_opt = cmd_initgenesis->add_option("--chain", "Name of the chain to initialize")
+                                         ->excludes(cmd_initgenesis_json_opt)
+                                         ->transform(CLI::Transformer(genesis_map, CLI::ignore_case));
+
     /*
      * Parse arguments and validate
      */
@@ -840,16 +1023,19 @@ int main(int argc, char* argv[]) {
             data_dir = DataDirectory(p);
         }
 
-        if (!fs::exists(data_dir.get_chaindata_path()) || !fs::is_directory(data_dir.get_chaindata_path()) ||
-            fs::is_empty(data_dir.get_chaindata_path())) {
-            std::cerr << "\n Directory " << data_dir.get_chaindata_path() << " does not exist or is empty" << std::endl;
-            return -1;
-        }
-        auto mdbx_path{db::get_datafile_path(data_dir.get_chaindata_path())};
-        if (!fs::exists(mdbx_path) || !fs::is_regular_file(mdbx_path)) {
-            std::cerr << "\n Directory " << data_dir.get_chaindata_path() << " does not contain " << db::kDbDataFileName
-                      << std::endl;
-            return -1;
+        if (!*cmd_initgenesis) {
+            if (!fs::exists(data_dir.get_chaindata_path()) || !fs::is_directory(data_dir.get_chaindata_path()) ||
+                fs::is_empty(data_dir.get_chaindata_path())) {
+                std::cerr << "\n Directory " << data_dir.get_chaindata_path() << " does not exist or is empty"
+                          << std::endl;
+                return -1;
+            }
+            auto mdbx_path{db::get_datafile_path(data_dir.get_chaindata_path())};
+            if (!fs::exists(mdbx_path) || !fs::is_regular_file(mdbx_path)) {
+                std::cerr << "\n Directory " << data_dir.get_chaindata_path() << " does not contain "
+                          << db::kDbDataFileName << std::endl;
+                return -1;
+            }
         }
 
         db::EnvConfig src_config{data_dir.get_chaindata_path().string()};
@@ -883,6 +1069,13 @@ int main(int argc, char* argv[]) {
         } else if (*cmd_stageset) {
             do_stage_set(src_config, cmd_stageset_name_opt->as<std::string>(), cmd_stageset_height_opt->as<uint32_t>(),
                          *app_dry_opt);
+        } else if (*cmd_initgenesis) {
+            do_init_genesis(data_dir, cmd_initgenesis_json_opt->as<std::string>(),
+                            *cmd_initgenesis_chain_opt ? cmd_initgenesis_chain_opt->as<uint32_t>() : 0u, *app_dry_opt);
+            if (*app_dry_opt) {
+                std::cout << "\nGenesis initialization succeeded. Due to --dry flag no data is persisted\n" << std::endl;
+                fs::remove_all(data_dir.get_base_path());
+            }
         };
 
         return 0;

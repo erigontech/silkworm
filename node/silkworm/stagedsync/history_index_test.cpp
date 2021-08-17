18e7b2e7f26c84f21b7e36dc5e067bdc1f73d0cf
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2021-08-17 10:26:24 +0200

    fixed linux weird thing i hope, really.

--- node/silkworm/stagedsync/history_index_test.cpp
+++ node/silkworm/stagedsync/history_index_test.cpp
@@ -158,130 +158,27 @@ TEST_CASE("Stage History indexes") {
     // Checks on storage's bitmaps
     CHECK(bitmap_storage_contract.cardinality() == 2);
     CHECK(bitmap_storage_contract.toString() == "{2,3}");
-}
 
-TEST_CASE("Unwind History indexes") {
-    using namespace silkworm;
-
-    TemporaryDirectory tmp_dir;
-    DataDirectory data_dir{tmp_dir.path()};
-
-    // Initialize temporary Database
-    db::EnvConfig db_config{data_dir.get_chaindata_path().string(), /*create*/ true};
-    db_config.inmemory = true;
-    auto env{db::open_env(db_config)};
-    stagedsync::TransactionManager txn{env};
-    db::table::create_all(*txn);
-
-    // ---------------------------------------
-    // Prepare
-    // ---------------------------------------
-
-    uint64_t block_number{1};
-    auto miner{0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c_address};
-
-    Block block{};
-    block.header.number = block_number;
-    block.header.beneficiary = miner;
-    block.header.gas_limit = 100'000;
-    block.header.gas_used = 63'820;
-
-    // This contract initially sets its 0th storage to 0x2a
-    // and its 1st storage to 0x01c9.
-    // When called, it updates its 0th storage to the input provided.
-    Bytes contract_code{*from_hex("600035600055")};
-    Bytes deployment_code{*from_hex("602a6000556101c960015560068060166000396000f3") + contract_code};
-
-    block.transactions.resize(1);
-    block.transactions[0].data = deployment_code;
-    block.transactions[0].gas_limit = block.header.gas_limit;
-    block.transactions[0].max_priority_fee_per_gas = 0;  // EIP-1559
-    block.transactions[0].max_fee_per_gas = 20 * kGiga;
-
-    auto sender{0xb685342b8c54347aad148e1f22eff3eb3eb29391_address};
-    block.transactions[0].from = sender;
-
-    db::Buffer buffer{*txn};
-    Account sender_account{};
-    sender_account.balance = kEther;
-    buffer.update_account(sender, std::nullopt, sender_account);
-
-    std::vector<Receipt> receipts;
-
-    // ---------------------------------------
-    // Execute first block
-    // ---------------------------------------
-    CHECK(execute_block(block, buffer, kMainnetConfig, receipts) == ValidationResult::kOk);
-    auto contract_address{create_address(sender, /*nonce=*/0)};
-
-    // ---------------------------------------
-    // Execute second block
-    // ---------------------------------------
-
-    std::string new_val{"000000000000000000000000000000000000000000000000000000000000003e"};
-
-    block_number = 2;
-    block.header.number = block_number;
-    block.header.gas_used = 26'201;
-
-    block.transactions[0].nonce = 1;
-    block.transactions[0].value = 1000;
-
-    block.transactions[0].to = contract_address;
-    block.transactions[0].data = *from_hex(new_val);
-    block.transactions[0].max_priority_fee_per_gas = 20 * kGiga;
-
-    CHECK(execute_block(block, buffer, kMainnetConfig, receipts) == ValidationResult::kOk);
-
-    // ---------------------------------------
-    // Execute third block
-    // ---------------------------------------
-
-    new_val = "000000000000000000000000000000000000000000000000000000000000003b";
-
-    block_number = 3;
-    block.header.number = block_number;
-    block.header.gas_used = 26'201;
-
-    block.transactions[0].nonce = 2;
-    block.transactions[0].value = 1000;
-
-    block.transactions[0].to = contract_address;
-    block.transactions[0].data = *from_hex(new_val);
-    block.transactions[0].max_priority_fee_per_gas = 20 * kGiga;
-
-    CHECK(execute_block(block, buffer, kMainnetConfig, receipts) == ValidationResult::kOk);
-    buffer.write_to_db();
-    db::stages::set_stage_progress(*txn, db::stages::kExecutionKey, 3);
-
-    CHECK(stagedsync::stage_account_history(txn, data_dir.get_etl_path()) == stagedsync::StageResult::kSuccess);
-    CHECK(stagedsync::stage_storage_history(txn, data_dir.get_etl_path()) == stagedsync::StageResult::kSuccess);
     CHECK(stagedsync::unwind_account_history(txn, data_dir.get_etl_path(), 2) == stagedsync::StageResult::kSuccess);
     CHECK(stagedsync::unwind_storage_history(txn, data_dir.get_etl_path(), 2) == stagedsync::StageResult::kSuccess);
 
-    auto account_history_table{db::open_cursor(*txn, db::table::kAccountHistory)};
-    auto storage_history_table{db::open_cursor(*txn, db::table::kStorageHistory)};
+    account_history_table = db::open_cursor(*txn, db::table::kAccountHistory);
+    storage_history_table = db::open_cursor(*txn, db::table::kStorageHistory);
     // Account retrieving from Databse
-    auto bitmap_address_sender_bytes{account_history_table.lower_bound(db::to_slice(sender)).value};
-    auto bitmap_address_contract_bytes{account_history_table.lower_bound(db::to_slice(contract_address)).value};
+    bitmap_address_sender_bytes = account_history_table.lower_bound(db::to_slice(sender)).value;
+    bitmap_address_contract_bytes = account_history_table.lower_bound(db::to_slice(contract_address)).value;
     // Bitmaps computation of accounts
-    auto bitmap_address_sender{roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_address_sender_bytes).data()), bitmap_address_sender_bytes.size())};
-    auto bitmap_address_contract{roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_address_contract_bytes).data()), bitmap_address_contract_bytes.size())};
+    bitmap_address_sender = roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_address_sender_bytes).data()), bitmap_address_sender_bytes.size());
+    bitmap_address_contract = roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_address_contract_bytes).data()), bitmap_address_contract_bytes.size());
     // Checks on account's bitmaps
     CHECK(bitmap_address_sender.cardinality() == 2);
     CHECK(bitmap_address_contract.cardinality() == 2);
     CHECK(bitmap_address_sender.toString() == "{1,2}");
     CHECK(bitmap_address_contract.toString() == "{1,2}");
-    // The location is the first so it's at 0
-    evmc::bytes32 location{0x0000000000000000000000000000000000000000000000000000000000000000_bytes32};
-    // Composite: Address + Location
-    Bytes composite(kAddressLength + kHashLength, '\0');
-    std::memcpy(&composite[0], contract_address.bytes, kAddressLength);
-    std::memcpy(&composite[kHashLength], location.bytes, kHashLength);
     // Storage retrieving from Databse
-    auto bitmap_storage_contract_bytes{storage_history_table.lower_bound(db::to_slice(composite)).value};
+    bitmap_storage_contract_bytes = storage_history_table.lower_bound(db::to_slice(composite)).value;
     // Bitmaps computing for storage
-    auto bitmap_storage_contract{roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_storage_contract_bytes).data()), bitmap_storage_contract_bytes.size())};
+    bitmap_storage_contract = roaring::Roaring64Map::readSafe(byte_ptr_cast(db::from_slice(bitmap_storage_contract_bytes).data()), bitmap_storage_contract_bytes.size());
     // Checks on storage's bitmaps
     CHECK(bitmap_storage_contract.cardinality() == 1);
     CHECK(bitmap_storage_contract.toString() == "{2}");

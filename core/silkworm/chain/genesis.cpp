e583b06923bde4b2d9be44ec99b36e724bc8e270
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-08-21 15:32:59 +0200

    Remove unneeded include and commented code

--- core/silkworm/chain/genesis.cpp
+++ core/silkworm/chain/genesis.cpp
@@ -18,7 +18,6 @@
 
 #include <cassert>
 #include <fstream>
-#include <iostream>
 #include <stdexcept>
 
 extern const char* genesis_mainnet_data(void);
@@ -33,7 +32,6 @@ extern size_t sizeof_genesis_rinkeby_data(void);
 namespace silkworm {
 
 std::string read_genesis_data(unsigned int chain_id) {
-
     std::string ret{};
     switch (chain_id) {
         case 1:
@@ -49,73 +47,9 @@ std::string read_genesis_data(unsigned int chain_id) {
             ret.assign(genesis_goerli_data(), sizeof_genesis_goerli_data());
             break;
         default:
-            ret = "{"; // <- Won't be lately parsed as valid json value
+            ret = "{";  // <- Won't be lately parsed as valid json value
     };
 
     return ret;
 }
-
-//nlohmann::json silkworm::read_genesis_json(unsigned int chain_id) {
-//    std::string source;
-//    nlohmann::json ret;
-//
-//    switch (chain_id) {
-//        case 1:
-//            assert(sizeof_genesis_mainnet_data() != 0);
-//            source.assign(genesis_mainnet_data(), sizeof_genesis_mainnet_data());
-//            break;
-//        case 4:
-//            assert(sizeof_genesis_rinkeby_data() != 0);
-//            source.assign(genesis_rinkeby_data(), sizeof_genesis_rinkeby_data());
-//            break;
-//        case 5:
-//            assert(sizeof_genesis_goerli_data() != 0);
-//            source.assign(genesis_goerli_data(), sizeof_genesis_goerli_data());
-//            break;
-//        default:
-//            ret["Error"] = nlohmann::json::array();
-//            ret["Error"].push_back(std::string(__FUNCTION__) + " : Provided chain id is unknown");
-//            return ret;
-//    };
-//
-//    auto genesis_json = nlohmann::json::parse(source, nullptr, /* allow_exceptions = */ false);
-//    if (genesis_json == nlohmann::json::value_t::discarded) {
-//        throw std::runtime_error("Unable to parse json for chain " + std::to_string(chain_id).append(" function : ") +
-//                                 std::string(__FUNCTION__));
-//    }
-//    return genesis_json;
-//}
-//
-//nlohmann::json read_genesis_json(std::filesystem::path source) {
-//    namespace fs = std::filesystem;
-//    nlohmann::json ret;
-//
-//    // Check source is valid input
-//    if (!source.has_filename() || !fs::exists(source) || !fs::is_regular_file(source) || !fs::file_size(source)) {
-//        ret["Error"] = nlohmann::json::array();
-//        ret["Error"].push_back(std::string(__FUNCTION__) + " : Provided json source does not exist or is empty file");
-//        return ret;
-//    }
-//
-//    try {
-//        // Read input file
-//        std::string source_data;
-//        std::ifstream ifs(source.string());
-//        source_data = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-//        auto source_json = nlohmann::json::parse(source_data, nullptr, /* allow_exceptions = */ false);
-//        if (source_json == nlohmann::json::value_t::discarded) {
-//            ret["Error"] = nlohmann::json::array();
-//            ret["Error"].push_back(std::string(__FUNCTION__) + " : Badly formatted json file");
-//        } else {
-//            ret = source_json;
-//        }
-//
-//    } catch (const std::exception& ex) {
-//        ret["Error"] = nlohmann::json::array();
-//        ret["Error"].push_back(std::string(__FUNCTION__) + " : " + ex.what());
-//    }
-//
-//    return ret;
-//}
-
 }  // namespace silkworm

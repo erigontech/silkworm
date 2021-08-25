84fdb83b13964ee5ef2da3a80a8cd0242cdb92b4
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2021-08-25 21:02:59 +0200

    added etl_load benchmark

--- cmd/benchmarks.cpp
+++ cmd/benchmarks.cpp
@@ -20,6 +20,8 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <set>
+#include <random>
 
 #include <CLI/CLI.hpp>
 #include <boost/bind.hpp>
@@ -34,17 +36,17 @@
 #include <silkworm/db/stages.hpp>
 #include <silkworm/state/in_memory_state.hpp>
 #include <silkworm/trie/hash_builder.hpp>
+#include <silkworm/common/stopwatch.hpp>
+#include <silkworm/etl/buffer.hpp>
+#include <silkworm/etl/collector.hpp>
+#include <silkworm/common/temp_dir.hpp>
+#include <silkworm/db/tables.hpp>
+#include <silkworm/common/log.hpp>
+
 
 namespace fs = std::filesystem;
 using namespace silkworm;
-
-#include <iostream>
-#include <random>
-
-#include <silkworm/common/stopwatch.hpp>
-#include <silkworm/etl/buffer.hpp>
-
-using namespace silkworm;
+using namespace etl;
 
 std::random_device rd;
 std::default_random_engine engine{rd()};
@@ -203,13 +205,114 @@ void etl_buffer_ordered() {
     }
 }
 
+static std::vector<Entry> generate_entry_set(size_t size) 
+{
+    std::vector<Entry> entries;
+    std::set<Bytes>    keys;
+
+    while (entries.size() < size) {
+        etl::Entry entry{random_bytes(), random_bytes()};
+
+        if (keys.count(entry.key)) {
+            // we want unique keys
+            continue;
+        }
+            
+        keys.insert(entry.key);
+        entries.push_back(std::move(entry));
+    }
+    return entries;
+}
+
+static void etl_benchmark(const std::string& label, LoadFunc load_func, const std::vector<Entry> &entries) 
+{
+    auto do_bench = [&]() {
+        StopWatch sw;
+        sw.start();
+
+        TemporaryDirectory db_tmp_dir;
+        TemporaryDirectory etl_tmp_dir;
+        // Initialize random seed
+        srand(time(NULL));
+
+        // Initialize temporary Database
+        db::EnvConfig db_config{db_tmp_dir.path(), /*create*/ true};
+        db_config.inmemory = true;
+
+        auto env{db::open_env(db_config)};
+        auto txn{env.start_write()};
+
+        auto collector{Collector(etl_tmp_dir.path(), 100 * 16)};  // 100 entries per file (16 bytes per entry)
+        db::table::create_all(txn);
+
+        // Collection
+        for (auto&& entry : entries) {
+            collector.collect(entry);
+        }
+        // Check whether temporary files were generated
+        //assert(std::distance(fs::directory_iterator{etl_tmp_dir.path()}, fs::directory_iterator{}) == 10);
+
+        // Load data
+        auto to{db::open_cursor(txn, db::table::kHeaderNumbers)};
+        collector.load(to, load_func);
+
+        auto feed_time = sw.lap();
+        sw.reset();
+        return sw.format(feed_time.second);
+    };
+    auto res1 = do_bench();
+    std::cout << label << " done in " << res1 << std::endl;
+}
+
+static void bench_collector_load(const std::string &load_desc, 
+                                 LoadFunc load_func, 
+                                 std::vector<Entry> entries) // copy by design
+{
+    // bench collector with entries in random order
+    // -------------------------------------------
+    etl_benchmark(load_desc + "random", load_func, entries);
+
+    // bench collector with entries sorted in increasing order
+    // ------------------------------------------------------
+    std::sort(entries.begin(), entries.end());
+    etl_benchmark(load_desc + "sorted increasing", load_func, entries);
+
+    // bench collector with entries sorted in decreasing order
+    // ------------------------------------------------------
+    std::sort(entries.begin(), entries.end(), [](const Entry &a, const Entry &b) -> bool { return b < a; });
+    etl_benchmark(load_desc + "sorted decreasing", load_func, entries);
+}
+
+void etl_load() {
+    SILKWORM_LOG_VERBOSITY(LogLevel::None);
+
+    //size_t kDataSetSize{1_Gibi};
+    size_t kDataSetSize{4096};
+    std::vector<Entry> entries { generate_entry_set(kDataSetSize) };
+    
+    // first test with default load
+    // ----------------------------
+    bench_collector_load("default load: ", nullptr, entries);
+
+    // first test with custom load
+    // ---------------------------
+    auto load_fn = [](Entry entry, mdbx::cursor& table, MDBX_put_flags_t flags) {
+        (void)flags;
+        entry.key.at(0) = 1;
+        table.upsert(db::to_slice(entry.key), db::to_slice(entry.value));
+    };
+
+    bench_collector_load("custom load: ", load_fn, entries);
+}
+
 int main(int argc, char* argv[]) {
 
     CLI::App app_main("Silkworm db tool");
     app_main.require_subcommand(1);  // At least 1 subcommand is required
     // List tables and gives info about storage
-    auto benchmark_etl_buffer_unordered = app_main.add_subcommand("buffer_unordered", "List db and tables info");
-    auto benchmark_etl_buffer_ordered = app_main.add_subcommand("buffer_ordered", "Print free pages info");
+    auto benchmark_etl_buffer_unordered = app_main.add_subcommand("buffer_unordered", "Benchmark etl::Buffer");
+    auto benchmark_etl_buffer_ordered = app_main.add_subcommand("buffer_ordered", "Benchmark etl::Buffer");
+    auto benchmark_etl_load = app_main.add_subcommand("etl_load", "Benchmark collector::load");
     /*
      * Parse arguments and validate
      */
@@ -220,6 +323,8 @@ int main(int argc, char* argv[]) {
         etl_buffer_unordered();
     } else if (*benchmark_etl_buffer_ordered) {
         etl_buffer_ordered();
+    } else if(*benchmark_etl_load) {
+        etl_load();
     }
     return 0;
 }

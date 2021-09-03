e9d49f28c18b2e53e37f09c00740566cc87efd33
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2021-09-03 18:14:39 +0200

    Fix stage_hashstate for storage (#414)
    * Pass etl::Entry to LoadFunc by const ref rather than by copy
    
    * Move upsert_storage_value to db/util
    
    * Fix stage_hashstate for storage
    
    * kStoragePrefixLength -> kPlainStoragePrefixLength

--- node/silkworm/db/util.cpp
+++ node/silkworm/db/util.cpp
@@ -50,7 +50,7 @@ Bytes block_key(uint64_t block_number, const uint8_t (&hash)[kHashLength]) {
 }
 
 Bytes storage_change_key(uint64_t block_number, const evmc::address& address, uint64_t incarnation) {
-    Bytes res(8 + kStoragePrefixLength, '\0');
+    Bytes res(8 + kPlainStoragePrefixLength, '\0');
     endian::store_big_u64(&res[0], block_number);
     std::memcpy(&res[8], address.bytes, kAddressLength);
     endian::store_big_u64(&res[8 + kAddressLength], incarnation);
@@ -81,7 +81,7 @@ Bytes log_key(uint64_t block_number, uint32_t transaction_id) {
 
 std::optional<ByteView> find_value_suffix(mdbx::cursor& table, ByteView key, ByteView value_prefix) {
     auto prefix_slice{to_slice(value_prefix)};
-    auto data{table.lower_bound_multivalue(to_slice(key), prefix_slice, /*throw_notfound*/ false)};
+    auto data{table.lower_bound_multivalue(to_slice(key), prefix_slice, /*throw_notfound=*/false)};
     if (!data || !data.value.starts_with(prefix_slice)) {
         return std::nullopt;
     }
@@ -89,6 +89,17 @@ std::optional<ByteView> find_value_suffix(mdbx::cursor& table, ByteView key, Byt
     ByteView res{from_slice(data.value)};
     res.remove_prefix(value_prefix.length());
     return res;
+}
+
+void upsert_storage_value(mdbx::cursor& state_cursor, ByteView storage_prefix, ByteView location, ByteView value) {
+    if (find_value_suffix(state_cursor, storage_prefix, location)) {
+        state_cursor.erase();
+    }
+    if (!value.empty()) {
+        Bytes data{location};
+        data.append(value);
+        state_cursor.upsert(to_slice(storage_prefix), to_slice(data));
+    }
 }
 
 namespace detail {

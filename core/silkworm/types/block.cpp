1cca7aee3007c1ae092276f5c88932df225f88c1
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2021-09-05 19:03:53 +0200

    gcc happy

--- core/silkworm/types/block.cpp
+++ core/silkworm/types/block.cpp
@@ -37,7 +37,7 @@ ethash::hash256 BlockHeader::boundary() const {
 
     if (difficulty > 1u) {
         auto result{intx::bswap(dividend / difficulty)};
-        if (difficulty & (difficulty - 1) == 0) {
+        if ((difficulty & (difficulty - 1)) == 0) {
             result += 1;
         }
         std::memcpy(ret.bytes, intx::as_bytes(result), 32);

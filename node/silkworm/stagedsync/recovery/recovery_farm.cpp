3b74c75a87455db8c89c3d47c065db7a5e6fa3b7
Eugene <eugene@linux.com>
2021-09-13 15:57:52 +0200

    senders_recovery: (#429)
    *Don't error if transaction.type == 0x1

--- node/silkworm/stagedsync/recovery/recovery_farm.cpp
+++ node/silkworm/stagedsync/recovery/recovery_farm.cpp
@@ -317,15 +317,25 @@ StageResult RecoveryFarm::transform_and_fill_batch(const ChainConfig& config, ui
     const evmc_revision rev{config.revision(block_num)};
     const bool has_homestead{rev >= EVMC_HOMESTEAD};
     const bool has_spurious_dragon{rev >= EVMC_SPURIOUS_DRAGON};
-    const bool hash_london{rev >= EVMC_LONDON};
+    const bool has_berlin{rev >= EVMC_BERLIN};
+    const bool has_london{rev >= EVMC_LONDON};
 
     uint32_t tx_id{0};
     for (const auto& transaction : transactions) {
 
-        if (transaction.type != Transaction::Type::kLegacy && !hash_london) {
-            SILKWORM_LOG(LogLevel::Error) << "Non legacy transaction type for transaction #" << tx_id << " in block #"
+        switch (transaction.type) {
+            case Transaction::Type::kEip2930:
+                if (!has_berlin) {
+                    SILKWORM_LOG(LogLevel::Error) << "Non legacy transaction type for transaction #" << tx_id << " in block #"
+                                          << block_num << " before Berlin" << std::endl;
+                    return StageResult::kInvalidTransaction;
+                }
+            case Transaction::Type::kEip1559:
+                if (!has_london) {
+                    SILKWORM_LOG(LogLevel::Error) << "Non legacy transaction type for transaction #" << tx_id << " in block #"
                                           << block_num << " before London" << std::endl;
-            return StageResult::kInvalidTransaction;
+                    return StageResult::kInvalidTransaction;
+                }
         }
 
         if (!silkworm::ecdsa::is_valid_signature(transaction.r, transaction.s, has_homestead)) {
@@ -373,7 +383,7 @@ bool RecoveryFarm::dispatch_batch() {
     }
 
     // Locate first available worker
-    while (true) {
+    while (!should_stop()) {
         auto it = std::find_if(workers_.begin(), workers_.end(), [](const worker_pair& w) {
             return w.first->get_status() == RecoveryWorker::Status::Idle;
         });
@@ -447,7 +457,7 @@ StageResult RecoveryFarm::fill_canonical_headers(BlockNum from, BlockNum to) noe
         while (data.done) {
             reached_block_num = endian::load_big_u64(static_cast<uint8_t*>(data.key.iov_base));
             if (reached_block_num != expected_block_num) {
-                SILKWORM_LOG(LogLevel::Error) << "Bad header hash sequence ! Expected " << expected_block_num << " got "
+                SILKWORM_LOG(LogLevel::Error) << "Bad block number sequence ! Expected " << expected_block_num << " got "
                                               << reached_block_num << std::endl;
                 return StageResult::kBadChainSequence;
             }

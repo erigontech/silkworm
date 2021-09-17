29af280fd006fd9286f1e1e6399ab97f43ab3877
Giulio Rebuffo <giulio.rebuffo@gmail.com>
2021-09-17 16:37:09 +0200

    comments solved + clique rewards added

--- cmake-build-debug/CRoaring/benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/giuliorebuffo/forking/silkworm/CRoaring/benchmarks
-# Build directory: /Users/giuliorebuffo/forking/silkworm/cmake-build-debug/CRoaring/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/CRoaring/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/giuliorebuffo/forking/silkworm/CRoaring/src
-# Build directory: /Users/giuliorebuffo/forking/silkworm/cmake-build-debug/CRoaring/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

4bf63163dbb13d191882b42afc62f754c868ae1e
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-09-20 13:14:54 +0200

    Make clear we're searching for signer in a reduced range instead of using double negations which are hard to follow

--- core/silkworm/consensus/clique/clique_snapshot.cpp
+++ core/silkworm/consensus/clique/clique_snapshot.cpp
@@ -50,9 +50,9 @@ ValidationResult CliqueSnapshot::add_header(const BlockHeader& header, const evm
     // Conditions are either :
     // 1 - The signer has not recently voted
     // 2 - The signer has recently voted BUT is the least recent AND the queue would be popped
-    auto pop_least_recent{recents_.size() > signers_.size() / 2};
-    auto is_least_recent{pop_least_recent && recents_.back() == signer};
-    if (!is_least_recent && std::find(recents_.begin(), recents_.end(), signer) != recents_.end()) {
+    auto pop_least_recent{static_cast<int64_t>(recents_.size() > signers_.size() / 2)};  // Last element would be popped
+    auto last_iterator{std::prev(recents_.end(), pop_least_recent)};
+    if (std::find(recents_.begin(), last_iterator, signer) != last_iterator) {
         return ValidationResult::kRecentlySigned;
     }
 

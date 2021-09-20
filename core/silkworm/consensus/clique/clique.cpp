b04bf5ff734d55c0a51e1690b19abb1671c10363
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-09-20 16:18:54 +0200

    Clang format

--- core/silkworm/consensus/clique/clique.cpp
+++ core/silkworm/consensus/clique/clique.cpp
@@ -14,13 +14,13 @@
    limitations under the License.
 */
 
-#include <silkworm/trie/vector_root.hpp>
+#include "clique.hpp"
+
+#include <silkworm/chain/difficulty.hpp>
 #include <silkworm/chain/intrinsic_gas.hpp>
 #include <silkworm/chain/protocol_param.hpp>
-#include <silkworm/chain/difficulty.hpp>
 #include <silkworm/crypto/ecdsa.hpp>
-
-#include "clique.hpp"
+#include <silkworm/trie/vector_root.hpp>
 
 namespace silkworm::consensus {
 
@@ -29,123 +29,118 @@ ValidationResult Clique::pre_validate_block(const Block& block, State& state, co
 }
 
 ValidationResult Clique::validate_block_header(const BlockHeader& header, State& state, const ChainConfig&) {
+    // Checkpoint blocks need to enforce zero beneficiary
+    uint64_t checkpoint{header.number % clique_config_.epoch == 0};
+    if (checkpoint && header.beneficiary) {
+        return ValidationResult::kInvalidCheckpointBeneficiary;
+    }
 
-	// Checkpoint blocks need to enforce zero beneficiary
-	uint64_t checkpoint{header.number % clique_config_.epoch == 0};
-	if (checkpoint && header.beneficiary) {
-		return ValidationResult::kInvalidCheckpointBeneficiary;
-	}
+    // Nonces must be 0x00..0 or 0xff..f, zeroes enforced on checkpoints
+    if (header.nonce != kNonceAuthVote && header.nonce != kNonceDropVote) {
+        return ValidationResult::kInvalidVote;
+    }
 
-	// Nonces must be 0x00..0 or 0xff..f, zeroes enforced on checkpoints
-	if (header.nonce != kNonceAuthVote && header.nonce !=  kNonceDropVote) {
-		return ValidationResult::kInvalidVote;
-	}
+    if (checkpoint && header.nonce != kNonceDropVote) {
+        return ValidationResult::kInvalidVote;
+    }
 
-	if (checkpoint && header.nonce != kNonceDropVote) {
-		return ValidationResult::kInvalidVote;
-	}
+    // Check that the extra-data contains both the vanity and signature
+    if (header.extra_data.size() < (kHashLength + kSignatureLength + 1)) {
+        return ValidationResult::kMissingVanity;
+    }
 
-	// Check that the extra-data contains both the vanity and signature
-	if (header.extra_data.size() < (kHashLength + kSignatureLength + 1)) {
-		return ValidationResult::kMissingVanity;
-	}
+    // Ensure that the extra-data contains a signer list on checkpoint, but none otherwise
+    uint64_t signers_length{header.extra_data.size() - kHashLength - kSignatureLength - 1};
+    if (!checkpoint && signers_length != 0) {
+        return ValidationResult::kUnauthorizedSigner;
+    }
+    if (checkpoint && (signers_length % kAddressLength) != 0) {
+        return ValidationResult::kMissingSigner;
+    }
+    // Ensure that the mix digest is zero as we don't have fork protection currently
+    if (header.mix_hash) {
+        return ValidationResult::kInvalidMixHash;
+    }
+    // Ensure that the block doesn't contain any uncles which are meaningless in PoA
+    if (header.ommers_hash != kEmptyListHash) {
+        return ValidationResult::kWrongOmmersHash;
+    }
+    // Ensure that the block's difficulty is meaningful (may not be correct at this point)
+    if (header.number > 0) {
+        auto parent{state.read_header(header.number - 1, header.parent_hash)};
+        if (parent == std::nullopt) {
+            return ValidationResult::kUnknownParent;
+        }
+        if (parent->timestamp > header.timestamp - clique_config_.period) {
+            return ValidationResult::kInvalidTimestamp;
+        }
+        if (header.difficulty != kDiffInTurn && header.difficulty != kDiffNoTurn) {
+            return ValidationResult::kWrongDifficulty;
+        }
+    }
 
-	// Ensure that the extra-data contains a signer list on checkpoint, but none otherwise
-	uint64_t signers_length{header.extra_data.size() - kHashLength - kSignatureLength - 1};
-	if (!checkpoint && signers_length != 0) {
-		return ValidationResult::kUnauthorizedSigner;
-	}
-	if (checkpoint && (signers_length % kAddressLength) != 0) {
-		return ValidationResult::kMissingSigner;
-	}
-	// Ensure that the mix digest is zero as we don't have fork protection currently
-	if (header.mix_hash) {
-		return ValidationResult::kInvalidMixHash;
-	}
-	// Ensure that the block doesn't contain any uncles which are meaningless in PoA
-	if (header.ommers_hash != kEmptyListHash) {
-		return ValidationResult::kWrongOmmersHash;
-	}
-	// Ensure that the block's difficulty is meaningful (may not be correct at this point)
-	if (header.number > 0) {
-		auto parent{state.read_header(header.number - 1, header.parent_hash)};
-		if (parent == std::nullopt) {
-			return ValidationResult::kUnknownParent;
-		}
-		if (parent->timestamp > header.timestamp - clique_config_.period) {
-			return ValidationResult::kInvalidTimestamp;
-		}
-		if (header.difficulty != kDiffInTurn && header.difficulty != kDiffNoTurn) {
-			return ValidationResult::kWrongDifficulty;
-		}
-	}
-	
-	// All basic checks passed, verify cascading fields
+    // All basic checks passed, verify cascading fields
     if (header.number == 0) {
         std::vector<evmc::address> signers;
-        size_t signers_count{(header.extra_data.size() - kHashLength - kSignatureLength -1) / kAddressLength};
+        size_t signers_count{(header.extra_data.size() - kHashLength - kSignatureLength - 1) / kAddressLength};
         for (size_t i = 0; i < signers_count; i++) {
             evmc::address signer;
-            std::memcpy(signer.bytes, 
-                        &header.extra_data[kHashLength + (i * kAddressLength)],
-                        kAddressLength);
+            std::memcpy(signer.bytes, &header.extra_data[kHashLength + (i * kAddressLength)], kAddressLength);
             signers.push_back(signer);
         }
         last_snapshot_ = CliqueSnapshot{header.number, header.hash(), signers, {}};
         state.write_snapshot(header.number, header.hash(), last_snapshot_);
         return ValidationResult::kOk;
     } else if (!last_snapshot_.get_hash()) {
-		// If last snapshot is not set, find it from the database and initialize it
-		auto current_header{header};
-		std::vector<BlockHeader> pending_blocks;
+        // If last snapshot is not set, find it from the database and initialize it
+        auto current_header{header};
+        std::vector<BlockHeader> pending_blocks;
 
-		while (current_header.number % clique_config_.epoch != 0) {
-			pending_blocks.push_back(current_header);
-			auto parent{state.read_header(current_header.number - 1, current_header.parent_hash)};
-			if (parent == std::nullopt) {
-				return ValidationResult::kUnknownParent;
-			}
-			current_header = *parent;
-		}
-		std::reverse(pending_blocks.begin(), pending_blocks.end());
-		last_snapshot_ = *state.read_snapshot(current_header.number, current_header.hash());
-		for (const auto& h: pending_blocks) {
-			auto signer{get_signer_from_clique_header(h)};
-			
-			if (signer == std::nullopt) {
-				return ValidationResult::kInvalidVote;
-			}
+        while (current_header.number % clique_config_.epoch != 0) {
+            pending_blocks.push_back(current_header);
+            auto parent{state.read_header(current_header.number - 1, current_header.parent_hash)};
+            if (parent == std::nullopt) {
+                return ValidationResult::kUnknownParent;
+            }
+            current_header = *parent;
+        }
+        std::reverse(pending_blocks.begin(), pending_blocks.end());
+        last_snapshot_ = *state.read_snapshot(current_header.number, current_header.hash());
+        for (const auto& h : pending_blocks) {
+            auto signer{get_signer_from_clique_header(h)};
 
-			auto err{last_snapshot_.add_header(h, *signer, clique_config_)};
-			if (err != ValidationResult::kOk) {
-				return err;
-			}
-		}
-		return ValidationResult::kOk;
-	} else {			
-		auto signer{get_signer_from_clique_header(header)};
+            if (signer == std::nullopt) {
+                return ValidationResult::kInvalidVote;
+            }
 
-		if (signer == std::nullopt) {
-			return ValidationResult::kInvalidVote;
-		}
-		
+            auto err{last_snapshot_.add_header(h, *signer, clique_config_)};
+            if (err != ValidationResult::kOk) {
+                return err;
+            }
+        }
+        return ValidationResult::kOk;
+    } else {
+        auto signer{get_signer_from_clique_header(header)};
+
+        if (signer == std::nullopt) {
+            return ValidationResult::kInvalidVote;
+        }
+
         auto err{last_snapshot_.add_header(header, *signer, clique_config_)};
         if (err != ValidationResult::kOk) {
             return err;
         }
-		if (checkpoint) {
-        	state.write_snapshot(last_snapshot_.get_block_number(), last_snapshot_.get_hash(), last_snapshot_);
-		}
-		return last_snapshot_.verify_seal(header, *signer);
+        if (checkpoint) {
+            state.write_snapshot(last_snapshot_.get_block_number(), last_snapshot_.get_hash(), last_snapshot_);
+        }
+        return last_snapshot_.verify_seal(header, *signer);
     }
 }
 
 // There are no rewards in Clique POA consensus
 void Clique::apply_rewards(IntraBlockState&, const Block&, const evmc_revision&) {}
 
-evmc::address Clique::get_beneficiary(const BlockHeader& header) {
-    return *get_signer_from_clique_header(header);
-}
+evmc::address Clique::get_beneficiary(const BlockHeader& header) { return *get_signer_from_clique_header(header); }
 
 // taking the header not by reference is intentional
 std::optional<evmc::address> Clique::get_signer_from_clique_header(BlockHeader header) {
@@ -167,7 +162,7 @@ std::optional<evmc::address> Clique::get_signer_from_clique_header(BlockHeader h
     }
     // Run Ecrecover and get public key
     auto recovered{ecdsa::recover(full_view(header_hash), signature, v)};
-    if (!recovered.has_value() || recovered->at(0) !=4u) {
+    if (!recovered.has_value() || recovered->at(0) != 4u) {
         return std::nullopt;
     }
     auto hash{keccak256(recovered->substr(1))};
@@ -178,5 +173,4 @@ std::optional<evmc::address> Clique::get_signer_from_clique_header(BlockHeader h
     return signer;
 }
 
-
-}
+}  // namespace silkworm::consensus

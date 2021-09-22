58b5aceedc7beb7cd77ce79ee1a69739abeae009
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-09-22 09:19:31 +0200

    Update validation.hpp
    Remove strong numbering of members

--- core/silkworm/consensus/validation.hpp
+++ core/silkworm/consensus/validation.hpp
@@ -21,58 +21,58 @@ namespace silkworm {
 
 // Classification of invalid transactions and blocks.
 enum class [[nodiscard]] ValidationResult{
-    kOk = 0,
+    kOk,
 
     // See [YP] Section 4.3.2 "Holistic Validity", Eq (31)
-    kWrongStateRoot = 1,         // wrong Hr
-    kWrongOmmersHash = 2,        // wrong Ho
-    kWrongTransactionsRoot = 3,  // wrong Ht
-    kWrongReceiptsRoot = 4,      // wrong He
-    kWrongLogsBloom = 5,         // wrong Hb
+    kWrongStateRoot,         // wrong Hr
+    kWrongOmmersHash,        // wrong Ho
+    kWrongTransactionsRoot,  // wrong Ht
+    kWrongReceiptsRoot,      // wrong He
+    kWrongLogsBloom,         // wrong Hb
 
     // See [YP] Section 4.3.4 "Block Header Validity", Eq (50)
-    kUnknownParent = 6,       // P(H) = ∅ ∨ Hi ≠ P(H)Hi + 1
-    kWrongDifficulty = 7,     // Hd ≠ D(H)
-    kGasAboveLimit = 8,       // Hg > Hl
-    kInvalidGasLimit = 9,     // |Hl-P(H)Hl|≥P(H)Hl/1024 ∨ Hl<5000
-    kInvalidTimestamp = 10,   // Hs ≤ P(H)Hs
-    kExtraDataTooLong = 11,   // ‖Hx‖ > 32
-    kWrongDaoExtraData = 12,  // see EIP-779
-    kWrongBaseFee = 13,       // see EIP-1559
-    kInvalidSeal = 14,        // Nonce or mix_hash (invalid Proof of Work)
-    kInvalidMixHash = 15,     // Invalid mix_hash (Clique, EIP-225)
+    kUnknownParent,      // P(H) = ∅ ∨ Hi ≠ P(H)Hi + 1
+    kWrongDifficulty,    // Hd ≠ D(H)
+    kGasAboveLimit,      // Hg > Hl
+    kInvalidGasLimit,    // |Hl-P(H)Hl|≥P(H)Hl/1024 ∨ Hl<5000
+    kInvalidTimestamp,   // Hs ≤ P(H)Hs
+    kExtraDataTooLong,   // ‖Hx‖ > 32
+    kWrongDaoExtraData,  // see EIP-779
+    kWrongBaseFee,       // see EIP-1559
+    kInvalidSeal,        // Nonce or mix_hash (invalid Proof of Work)
+    kInvalidMixHash,     // Invalid mix_hash (Clique, EIP-225)
 
     // See [YP] Section 6.2 "Execution", Eq (58)
-    kMissingSender = 16,          // S(T) = ∅
-    kSenderNoEOA = 17,            // EIP-3607: σ[S(T)]c ≠ KEC( () )
-    kWrongNonce = 18,             // Tn ≠ σ[S(T)]n
-    kIntrinsicGas = 19,           // g0 > Tg
-    kInsufficientFunds = 20,      // v0 > σ[S(T)]b
-    kBlockGasLimitExceeded = 21,  // Tg > BHl - l(BR)u
+    kMissingSender,          // S(T) = ∅
+    kSenderNoEOA,            // EIP-3607: σ[S(T)]c ≠ KEC( () )
+    kWrongNonce,             // Tn ≠ σ[S(T)]n
+    kIntrinsicGas,           // g0 > Tg
+    kInsufficientFunds,      // v0 > σ[S(T)]b
+    kBlockGasLimitExceeded,  // Tg > BHl - l(BR)u
 
     // Various other transaction validation
-    kMaxFeeLessThanBase = 22,            // max_fee_per_gas < base_fee_per_gas (EIP-1559)
-    kMaxPriorityFeeGreaterThanMax = 23,  // max_priority_fee_per_gas > max_fee_per_gas (EIP-1559)
-    kInvalidSignature = 24,              // EIP-2
-    kWrongChainId = 25,                  // EIP-155
-    kUnsupportedTransactionType = 26,    // EIP-2718
+    kMaxFeeLessThanBase,            // max_fee_per_gas < base_fee_per_gas (EIP-1559)
+    kMaxPriorityFeeGreaterThanMax,  // max_priority_fee_per_gas > max_fee_per_gas (EIP-1559)
+    kInvalidSignature,              // EIP-2
+    kWrongChainId,                  // EIP-155
+    kUnsupportedTransactionType,    // EIP-2718
 
     // See [YP] Section 11.1 "Ommer Validation", Eq (157)
-    kTooManyOmmers = 27,       // ‖BU‖ > 2
-    kInvalidOmmerHeader = 28,  // ¬V(U)
-    kNotAnOmmer = 29,          // ¬k(U, P(BH)H, 6)
-    kDuplicateOmmer = 30,      // not well covered by the YP actually
+    kTooManyOmmers,       // ‖BU‖ > 2
+    kInvalidOmmerHeader,  // ¬V(U)
+    kNotAnOmmer,          // ¬k(U, P(BH)H, 6)
+    kDuplicateOmmer,      // not well covered by the YP actually
 
     // See [YP] Section 11.2 "Transaction Validation", Eq (160)
-    kWrongBlockGas = 31,  // BHg ≠ l(BR)u
+    kWrongBlockGas,  // BHg ≠ l(BR)u
 
     // Clique (EIP-225)
-    kUnauthorizedSigner = 32,  // Handling an unauthorized voting signer
-    kMissingSigner = 33,       // Missing Signer in extra_data
-    kRecentlySigned = 34,      // Signer has already recently signed
-    kInvalidVote = 35,         // Non-Existing vote option
-    kInvalidCheckpointBeneficiary = 36,
-    kMissingVanity = 37,  // ‖Hx‖ < 32+65
+    kUnauthorizedSigner,  // Handling an unauthorized voting signer
+    kMissingSigner,       // Missing Signer in extra_data
+    kRecentlySigned,      // Signer has already recently signed
+    kInvalidVote,         // Non-Existing vote option
+    kInvalidCheckpointBeneficiary,
+    kMissingVanity,  // ‖Hx‖ < 32+65
 };
 
 }  // namespace silkworm

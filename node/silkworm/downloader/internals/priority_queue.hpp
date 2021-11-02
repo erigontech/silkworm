d1bedc3d209bd6136f8521f79da79d434b86aadd
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-11-02 17:40:46 +0100

    Update priority_queue.hpp
    Format

--- node/silkworm/downloader/internals/priority_queue.hpp
+++ node/silkworm/downloader/internals/priority_queue.hpp
@@ -1,17 +1,17 @@
 /*
-Copyright 2021 The Silkworm Authors
+    Copyright 2021 The Silkworm Authors
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 #ifndef SILKWORM_PRIORITY_QUEUE_HPP
@@ -33,7 +33,7 @@ class set_based_priority_queue {
     void pop() { elements_.erase(elements_.begin()); }
     void push(const T& element) { elements_.insert(element); }
     void erase(const T& element) { elements_.erase(element); }
-    size_t size() const { return elements_.size(); }
+    [[nodiscard]] size_t size() const { return elements_.size(); }
     bool contains(const T& element) { return elements_.template find(element) != elements_.end(); }
 };
 

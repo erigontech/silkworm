1b252e1888dba3e89b9de6973c246a089ee27414
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-11-28 19:23:53 +0100

    Update config_test.cpp
    Signed ids

--- core/silkworm/chain/config_test.cpp
+++ core/silkworm/chain/config_test.cpp
@@ -21,12 +21,12 @@
 namespace silkworm {
 
 TEST_CASE("Config lookup") {
-    CHECK(lookup_chain_config(0) == nullptr);
-    CHECK(lookup_chain_config(1) == &kMainnetConfig);
-    CHECK(lookup_chain_config(3) == &kRopstenConfig);
-    CHECK(lookup_chain_config(4) == &kRinkebyConfig);
-    CHECK(lookup_chain_config(5) == &kGoerliConfig);
-    CHECK(lookup_chain_config(12345) == nullptr);
+    CHECK(lookup_chain_config(0u) == nullptr);
+    CHECK(lookup_chain_config(1u) == &kMainnetConfig);
+    CHECK(lookup_chain_config(3u) == &kRopstenConfig);
+    CHECK(lookup_chain_config(4u) == &kRinkebyConfig);
+    CHECK(lookup_chain_config(5u) == &kGoerliConfig);
+    CHECK(lookup_chain_config(12345u) == nullptr);
     CHECK(lookup_chain_config("mainnet") == &kMainnetConfig);
     CHECK(lookup_chain_config("ropsten") == &kRopstenConfig);
     CHECK(lookup_chain_config("Rinkeby") == &kRinkebyConfig);

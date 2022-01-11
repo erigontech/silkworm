2239c54dc4697050189590bba1472a484caf256c
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-01-11 11:35:48 +0100

    Update node/silkworm/stagedsync/stage_blockhashes.cpp
    Co-authored-by: Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>

--- node/silkworm/stagedsync/stage_blockhashes.cpp
+++ node/silkworm/stagedsync/stage_blockhashes.cpp
@@ -118,7 +118,7 @@ StageResult BlockHashes::unwind(db::RWTxn& txn, BlockNum to) {
     std::vector<Bytes> collected_keys;
     if (source_data) {
         db::cursor_for_each(source, [&collected_keys](::mdbx::cursor&, ::mdbx::cursor::move_result& _data) -> bool {
-            collected_keys.emplace_back(static_cast<uint8_t*>(_data.value.iov_base), _data.value.iov_len);
+            collected_keys.emplace_back(db::from_slice(_data.value));
             return true;
         });
     }

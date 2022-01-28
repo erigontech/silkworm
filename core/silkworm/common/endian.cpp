bc7425afde0be06d67eada1137103e39978591d6
greg <greg7mdp@gmail.com>
2022-01-28 07:47:22 -0500

    Update functions comments to be correct, and add early return for zero values.

--- core/silkworm/common/endian.cpp
+++ core/silkworm/common/endian.cpp
@@ -27,12 +27,18 @@
 namespace silkworm::endian {
 
 ByteView to_big_compact(const uint64_t value) {
+    if (!value) {
+        return {};
+    }
     SILKWORM_THREAD_LOCAL uint8_t full_be[sizeof(uint64_t)];
     store_big_u64(&full_be[0], value);
     return zeroless_view(full_be);
 }
 
 ByteView to_big_compact(const intx::uint256& value) {
+    if (!value) {
+        return {};
+    }
     SILKWORM_THREAD_LOCAL uint8_t full_be[sizeof(intx::uint256)];
     intx::be::store(full_be, value);
     return zeroless_view(full_be);

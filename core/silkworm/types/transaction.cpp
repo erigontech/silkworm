cba00825d4f2d03e7999d774d6ba3ce0bf4b178b
yperbasis <andrey.ashikhmin@gmail.com>
2022-02-13 12:58:38 +0100

    Clear access_list in legacy_decode

--- core/silkworm/types/transaction.cpp
+++ core/silkworm/types/transaction.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2020-2021 The Silkworm Authors
+   Copyright 2020-2022 The Silkworm Authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -261,6 +261,8 @@ namespace rlp {
         if (DecodingResult err{decode(from, to.s)}; err != DecodingResult::kOk) {
             return err;
         }
+
+        to.access_list.clear();
 
         return DecodingResult::kOk;
     }

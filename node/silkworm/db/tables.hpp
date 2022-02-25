72f2c32759444433d3aa5463a1caac823a8ac039
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-02-25 16:53:41 +0100

    Add more tables comments

--- node/silkworm/db/tables.hpp
+++ node/silkworm/db/tables.hpp
@@ -49,7 +49,6 @@ inline constexpr db::MapConfig kAccountChangeSet{"AccountChangeSet", mdbx::key_m
 inline constexpr db::MapConfig kAccountHistory{"AccountHistory"};
 inline constexpr db::MapConfig kBlockBodies{"BlockBody"};
 
-
 //! \details Stores the binding of *canonical* block number with header hash
 //! \struct
 //! \verbatim
@@ -58,20 +57,46 @@ inline constexpr db::MapConfig kBlockBodies{"BlockBody"};
 //! \endverbatim
 inline constexpr db::MapConfig kCanonicalHashes{"CanonicalHeader"};
 
-//! \struct block_num_u64 (BE) + hash -> header (RLP)
+//! \details Stores the headers downloaded from peers
+//! \struct
+//! \verbatim
+//!   key   : block_num_u64 (BE) + header hash
+//!   value : header RLP encoded
+//! \endverbatim
 inline constexpr db::MapConfig kHeaders{"Header"};
 
-//! \struct block_num_u64 (BE) + hash -> total_difficulty (RLP)
+//! \details Stores the total difficulty accrued at each block height
+//! \struct
+//! \verbatim
+//!   key   : block_num_u64 (BE) + header hash
+//!   value : total difficulty (RLP encoded
+//! \endverbatim
 inline constexpr db::MapConfig kDifficulty{"HeadersTotalDifficulty"};
+
 
 inline constexpr db::MapConfig kBlockReceipts{"Receipt"};
 inline constexpr db::MapConfig kBloomBitsIndex{"BloomBitsIndex"};
 inline constexpr db::MapConfig kBloomBits{"BloomBits"};
 inline constexpr db::MapConfig kBodiesSnapshotInfo{"BodiesSnapshotInfo"};
+
+//! \details Stores the mapping of block number to the set (sorted) of all accounts touched by call traces.
+//! \struct
+//! \verbatim
+//!   key   : block_num_u64 (BE)
+//!   value : account address + two bits (one for "from" + another for "to")
+//! \endverbatim
+inline constexpr db::MapConfig kCallTraceSet{"CallTraceSet", mdbx::key_mode::usual, mdbx::value_mode::multi};
 inline constexpr db::MapConfig kCallFromIndex{"CallFromIndex"};
 inline constexpr db::MapConfig kCallToIndex{"CallToIndex"};
-inline constexpr db::MapConfig kCallTraceSet{"CallTraceSet", mdbx::key_mode::usual, mdbx::value_mode::multi};
+
+//! \details Stores contract's code
+//! \struct
+//! \verbatim
+//!   key   : contract code hash
+//!   value : contract code
+//! \endverbatim
 inline constexpr db::MapConfig kCode{"Code"};
+
 inline constexpr db::MapConfig kConfig{"Config"};
 inline constexpr db::MapConfig kDatabaseInfo{"DbInfo"};
 inline constexpr db::MapConfig kBlockTransactions{"BlockTransaction"};

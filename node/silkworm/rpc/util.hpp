8ee9d5f6dce49eed27b08a84a7c42eaee7c1196f
canepat <16927169+canepat@users.noreply.github.com>
2022-03-04 10:48:33 +0100

    BackEnd KV standalone server and test client (#596)

--- node/silkworm/rpc/util.hpp
+++ node/silkworm/rpc/util.hpp
@@ -22,7 +22,39 @@
 #include <grpcpp/grpcpp.h>
 #include <grpc/support/log.h>
 
+#include <silkworm/common/base.hpp>
+#include <silkworm/common/endian.hpp>
 #include <silkworm/common/log.hpp>
+#include <types/types.pb.h>
+
+namespace grpc {
+// operator== overloading for grpc::Status is *NOT* present in gRPC library
+inline bool operator==(const Status& lhs, const Status& rhs) {
+    return lhs.error_code() == rhs.error_code() &&
+        lhs.error_message() == rhs.error_message() &&
+        lhs.error_details() == rhs.error_details();
+}
+
+// operator<< overloading for grpc::Status is *NOT* present in gRPC library
+inline std::ostream& operator<<(std::ostream& out, const Status& status) {
+    out << "status=" << (status.ok() ? "OK" : "KO");
+    if (!status.ok()) {
+        out << " error_code=" << status.error_code()
+            << " error_message=" << status.error_message()
+            << " error_details=" << status.error_details();
+    }
+    return out;
+}
+} // namespace grpc
+
+// operator== overloading is *NOT* present in gRPC generated sources
+namespace types {
+inline bool operator==(const H160& lhs, const H160& rhs) {
+    return lhs.hi().hi() == rhs.hi().hi() &&
+        lhs.hi().lo() == rhs.hi().lo() &&
+        lhs.lo() == rhs.lo();
+}
+} // namespace types
 
 // The default gRPC logging function
 void gpr_default_log(gpr_log_func_args* args);
@@ -57,6 +89,17 @@ using GrpcNoLogGuard = GrpcLogGuard<gpr_no_log>;
 
 //! Utility class to map gRPC logging to Silkworm logging for an instance lifetime.
 using Grpc2SilkwormLogGuard = GrpcLogGuard<gpr_silkworm_log>;
+
+inline evmc::address address_from_H160(const types::H160& h160) {
+    uint64_t hi_hi = h160.hi().hi();
+    uint64_t hi_lo = h160.hi().lo();
+    uint32_t lo = h160.lo();
+    evmc::address address{};
+    silkworm::endian::store_big_u64(address.bytes +  0, hi_hi);
+    silkworm::endian::store_big_u64(address.bytes +  8, hi_lo);
+    silkworm::endian::store_big_u32(address.bytes + 16, lo);
+    return address;
+}
 
 } // namespace silkworm::rpc
 

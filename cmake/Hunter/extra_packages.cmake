5e092ad511e1d8c10903de70439df5ae52078049
yperbasis <andrey.ashikhmin@gmail.com>
2022-04-07 18:36:53 +0200

    Move benchmark to silkpre

--- cmake/Hunter/extra_packages.cmake
+++ cmake/Hunter/extra_packages.cmake
@@ -1,5 +1,5 @@
 #[[
-   Copyright 2020-2021 The Silkworm Authors
+   Copyright 2020-2022 The Silkworm Authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 ]]
 
 hunter_add_package(abseil)
-hunter_add_package(benchmark)
 hunter_add_package(Boost COMPONENTS thread)
 hunter_add_package(CLI11)
 hunter_add_package(gRPC)

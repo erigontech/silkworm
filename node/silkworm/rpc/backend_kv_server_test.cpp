a79242956c2ae4bf2d9633edbcdbda7838dc7327
canepat <16927169+canepat@users.noreply.github.com>
2022-04-12 10:50:17 +0200

    Reinsert delay in max TTL unit test

--- node/silkworm/rpc/backend_kv_server_test.cpp
+++ node/silkworm/rpc/backend_kv_server_test.cpp
@@ -1283,7 +1283,7 @@ TEST_CASE("BackEndKvServer E2E: bidirectional max TTL duration", "[silkworm][nod
         tx_reader_writer->Read(&response);
         CHECK(response.k() == "AA");
         CHECK(response.v() == "00");
-        //std::this_thread::sleep_for(std::chrono::milliseconds{kCustomMaxTimeToLive});
+        std::this_thread::sleep_for(std::chrono::milliseconds{kCustomMaxTimeToLive});
         remote::Cursor next_dup2;
         next_dup2.set_op(remote::Op::NEXT_DUP);
         next_dup2.set_cursor(cursor_id);

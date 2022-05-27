7e27ecac0293799387e834723a418698cf9b1be1
canepat <16927169+canepat@users.noreply.github.com>
2022-05-27 17:44:20 +0200

    Add unit tests

--- node/silkworm/rpc/client/call_test.cpp
+++ node/silkworm/rpc/client/call_test.cpp
@@ -16,11 +16,25 @@
 
 #include "call.hpp"
 
+#include <ostream>
+
 #include <catch2/catch.hpp>
 
 #include <remote/ethbackend.grpc.pb.h>
+#include <remote/kv.grpc.pb.h>
 
 namespace silkworm::rpc {
+
+// Factory function creating one null output stream (all characters are discarded)
+inline std::ostream& null_stream() {
+    static struct null_buf : public std::streambuf {
+        int overflow(int c) override { return c; }
+    } null_buf;
+    static struct null_strm : public std::ostream {
+        null_strm() : std::ostream(&null_buf) {}
+    } null_strm;
+    return null_strm;
+}
 
 // Exclude gRPC tests from sanitizer builds due to data race warnings
 #ifndef SILKWORM_SANITIZE
@@ -36,6 +50,8 @@ TEST_CASE("AsyncCall", "[silkworm][rpc][client][call]") {
         FakeCall call{&queue};
         CHECK(call.peer().empty());
         CHECK(call.start_time() <= std::chrono::steady_clock::now());
+        CHECK(call.end_time() <= std::chrono::steady_clock::now());
+        CHECK(call.latency() == call.end_time() - call.start_time());
         CHECK(call.status().ok());
         CHECK_NOTHROW(call.cancel());
     }
@@ -52,8 +68,20 @@ TEST_CASE("AsyncUnaryCall", "[silkworm][rpc][client][call]") {
       public:
         explicit FakeUnaryCall(grpc::CompletionQueue* queue, remote::ETHBACKEND::StubInterface* stub, CompletionFunc func = {})
             : AsyncUnaryCall(queue, stub, func) {}
+
+        bool finished() const { return finished_; }
+
+        void trigger_finish(bool ok) {
+            finish_processor_(ok);
+        }
+
       protected:
-        void handle_finish(bool /*ok*/) override {}
+        void handle_finish(bool /*ok*/) override {
+            finished_ = true;
+        }
+
+      private:
+        bool finished_{false};
     };
 
     grpc::CompletionQueue queue;
@@ -75,7 +103,35 @@ TEST_CASE("AsyncUnaryCall", "[silkworm][rpc][client][call]") {
         CHECK(call.status().ok());
         CHECK_NOTHROW(call.cancel());
     }
+
+    SECTION("AsyncUnaryCall::handle_finish") {
+        FakeUnaryCall call{&queue, stub_ptr.get()};
+        CHECK(!call.finished());
+        CHECK_NOTHROW(call.trigger_finish(true));
+        CHECK(call.finished());
+    }
 }
+
+TEST_CASE("AsyncServerStreamingCall", "[silkworm][rpc][client][call]") {
+    class FakeServerStreamingCall : public AsyncServerStreamingCall<
+        remote::StateChangeRequest,
+        remote::StateChangeBatch,
+        remote::KV::StubInterface,
+        &remote::KV::StubInterface::PrepareAsyncStateChanges> {
+      public:
+        explicit FakeServerStreamingCall(grpc::CompletionQueue* queue, remote::KV::StubInterface* stub)
+            : AsyncServerStreamingCall(queue, stub) {}
+
+        void handle_read() override {}
+
+        void handle_finish() override {}
+    };
+
+    SECTION("print ServerStreamingStats") {
+        CHECK_NOTHROW(null_stream() <<  FakeServerStreamingCall::stats());
+    }
+}
+
 #endif // SILKWORM_SANITIZE
 
 } // namespace silkworm::rpc

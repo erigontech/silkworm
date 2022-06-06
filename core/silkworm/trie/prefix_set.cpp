afc06c2b464a752f3ac27fe070cfcb9da7b61e2d
yperbasis <andrey.ashikhmin@gmail.com>
2022-06-06 16:52:58 +0200

    Small code simplification

--- core/silkworm/trie/prefix_set.cpp
+++ core/silkworm/trie/prefix_set.cpp
@@ -47,17 +47,16 @@ bool PrefixSet::contains(ByteView prefix) {
         --index_;
     }
 
-    for (;; ++index_) {
+    for (; index_ < keys_.size(); ++index_) {
         if (keys_[index_].starts_with(prefix)) {
             return true;
         }
         if (keys_[index_] > prefix) {
             return false;
         }
-        if (index_ == keys_.size() - 1) {
-            return false;
-        }
     }
+
+    return false;
 }
 
 }  // namespace silkworm::trie

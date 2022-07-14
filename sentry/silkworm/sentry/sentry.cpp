f6f2bca87be3d275019d762d67f4b560c6a7ef5d
battlmonstr <battlmonstr@users.noreply.github.com>
2022-07-14 17:45:16 +0200

    sentry: TCP server

--- sentry/silkworm/sentry/sentry.cpp
+++ sentry/silkworm/sentry/sentry.cpp
@@ -15,15 +15,29 @@ limitations under the License.
 */
 
 #include "sentry.hpp"
-#include <boost/asio/signal_set.hpp>
+#include <future>
+
+#include <silkworm/concurrency/coroutine.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/use_future.hpp>
+#include <grpc/grpc.h>
+
 #include <silkworm/common/directories.hpp>
+#include <silkworm/common/log.hpp>
+#include <silkworm/concurrency/cancellation_signal.hpp>
+#include <silkworm/rpc/server/server_context_pool.hpp>
+
+#include "rlpx/server.hpp"
 #include "rpc/server.hpp"
 #include "node_key_config.hpp"
 
 namespace silkworm::sentry {
 
 using namespace std;
+using namespace boost::asio::experimental::awaitable_operators;
 
 class SentryImpl final {
   public:
@@ -40,8 +54,14 @@ class SentryImpl final {
     void setup_shutdown_on_signals(boost::asio::io_context&);
 
     Settings settings_;
+    silkworm::rpc::ServerContextPool context_pool_;
+
+    rlpx::Server rlpx_server_;
+    optional<future<std::variant<std::monostate, std::monostate>>> rlpx_server_task_;
     rpc::Server rpc_server_;
+
     optional<unique_ptr<boost::asio::signal_set>> shutdown_signals_;
+    optional<unique_ptr<concurrency::CancellationSignal>> stop_signal_;
 };
 
 static silkworm::rpc::ServerConfig make_server_config(const Settings& settings) {
@@ -52,9 +72,17 @@ static silkworm::rpc::ServerConfig make_server_config(const Settings& settings) 
     return config;
 }
 
+class DummyServerCompletionQueue : public grpc::ServerCompletionQueue {
+};
+
 SentryImpl::SentryImpl(Settings settings)
     : settings_(std::move(settings)),
+      context_pool_(settings_.num_contexts),
+      rlpx_server_("0.0.0.0", settings_.port),
       rpc_server_(make_server_config(settings_)) {
+    for (size_t i = 0; i < settings_.num_contexts; i++) {
+        context_pool_.add_context(make_unique<DummyServerCompletionQueue>(), settings_.wait_mode);
+    }
 }
 
 void SentryImpl::start() {
@@ -63,15 +91,36 @@ void SentryImpl::start() {
     NodeKey node_key = node_key_get_or_generate(settings_.node_key, data_dir);
 
     rpc_server_.build_and_start();
-    setup_shutdown_on_signals(rpc_server_.next_io_context());
+
+    auto& rlpx_io_context = context_pool_.next_io_context();
+    stop_signal_ = { std::make_unique<concurrency::CancellationSignal>(rlpx_io_context) };
+    rlpx_server_task_ = boost::asio::co_spawn(
+            rlpx_io_context,
+            rlpx_server_.start(rlpx_io_context) || stop_signal_.value()->await(),
+            boost::asio::use_future);
+
+    setup_shutdown_on_signals(context_pool_.next_io_context());
+
+    context_pool_.start();
 }
 
 void SentryImpl::stop() {
     rpc_server_.shutdown();
+
+    if (stop_signal_) {
+        stop_signal_.value()->emit();
+    }
 }
 
 void SentryImpl::join() {
     rpc_server_.join();
+
+    if (rlpx_server_task_) {
+        rlpx_server_task_->wait();
+    }
+
+    context_pool_.stop();
+    context_pool_.join();
 }
 
 void SentryImpl::setup_shutdown_on_signals(boost::asio::io_context& io_context) {

af7e0c8fe2f992e1b9434e2db30fd4451224b03e
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-08-04 20:54:52 +0200

    abseil cmake args

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -14,6 +14,16 @@
    limitations under the License.
 ]]
 
+include(hunter_cmake_args)
+
+hunter_cmake_args(
+  abseil
+  CMAKE_ARGS
+        ABSL_PROPAGATE_CXX_STD=ON
+        ABSL_ENABLE_INSTALL=OFF
+        ABSL_RUN_TESTS=OFF
+)
+
 hunter_config(
   Microsoft.GSL
   VERSION 4.0.0

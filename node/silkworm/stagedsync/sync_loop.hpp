5aaa669197a1ddfbe1bf1ecdad9c982bb9b6b201
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-08-18 18:13:46 +0200

    Make current_stage_ atomic
    Is accessed by log_timer also in another thread

--- node/silkworm/stagedsync/sync_loop.hpp
+++ node/silkworm/stagedsync/sync_loop.hpp
@@ -36,9 +36,9 @@ class SyncLoop final : public Worker {
     silkworm::NodeSettings* node_settings_;  // As being passed by CLI arguments and/or already initialized data
     mdbx::env* chaindata_env_;               // The actual opened environment
     std::vector<std::unique_ptr<stagedsync::IStage>> stages_{};  // Collection of stages
-    size_t current_stage_{0};                                    // Index of current stage
+    std::atomic<size_t> current_stage_{0};                       // Index of current stage
     void work() final;                                           // The loop itself
-    void load_stages();                                          // Fills the vector of stages
+    void load_stages();                                          // Fills the vector with stages
 
     //! \brief Runs a full sync cycle
     [[nodiscard]] StageResult run_cycle(db::RWTxn& cycle_txn, Timer& log_timer);

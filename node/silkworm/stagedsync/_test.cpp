cab521a8ff091fa6904a73f9de209e9863358fef
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2022-08-26 15:58:06 +0200

    More tests

--- node/silkworm/stagedsync/_test.cpp
+++ node/silkworm/stagedsync/_test.cpp
@@ -64,57 +64,77 @@ TEST_CASE("Sync Stages") {
     node_settings.chain_config = db::read_chain_config(*txn);
 
     SECTION("BlockHashes") {
-        std::vector<evmc::bytes32> block_hashes = {
-            0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb_bytes32,
-            0xb5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510_bytes32,
-            0x0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2_bytes32};
 
-        auto canonical_table{db::open_cursor(*txn, db::table::kCanonicalHashes)};
-        BlockNum block_num{1};
-        for (const auto& hash : block_hashes) {
-            Bytes block_key{db::block_key(block_num++)};
-            canonical_table.insert(db::to_slice(block_key), db::to_slice(hash));
+        SECTION("Forward/Unwind/Prune args validation") {
+
+            stagedsync::BlockHashes stage(&node_settings);
+
+            // (previous_progress == headers_progress == 0)
+            REQUIRE(stage.forward(txn) == stagedsync::StageResult::kSuccess);
+            REQUIRE(stage.unwind(txn, 0) == stagedsync::StageResult::kSuccess);
+
+
+            // (previous_progress > headers_progress)
+            stage.update_progress(txn, 10);
+            REQUIRE(stage.forward(txn) == stagedsync::StageResult::kInvalidProgress);
+
+
         }
-        db::stages::write_stage_progress(*txn, db::stages::kHeadersKey, 3);
-        REQUIRE_NOTHROW(txn.commit(true));
-        stagedsync::BlockHashes stage(&node_settings);
 
-        // Forward
-        auto stage_result{stage.forward(txn)};
-        REQUIRE(stage_result == stagedsync::StageResult::kSuccess);
-        txn.commit(true);
+        SECTION("Forward and Unwind") {
+            std::vector<evmc::bytes32> block_hashes = {
+                0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb_bytes32,
+                0xb5553de315e0edf504d9150af82dafa5c4667fa618ed0a6f19c69b41166c5510_bytes32,
+                0x0b42b6393c1f53060fe3ddbfcd7aadcca894465a5a438f69c87d790b2299b9b2_bytes32};
 
-        {
-            // Verify written data is consistent
-            auto target_table{db::open_cursor(*txn, db::table::kHeaderNumbers)};
-            REQUIRE(txn->get_map_stat(target_table.map()).ms_entries == block_hashes.size() + 1);  // Block 0 is genesis
+            auto canonical_table{db::open_cursor(*txn, db::table::kCanonicalHashes)};
+            BlockNum block_num{1};
+            for (const auto& hash : block_hashes) {
+                Bytes block_key{db::block_key(block_num++)};
+                canonical_table.insert(db::to_slice(block_key), db::to_slice(hash));
+            }
+            db::stages::write_stage_progress(*txn, db::stages::kHeadersKey, 3);
+            REQUIRE_NOTHROW(txn.commit(true));
+            stagedsync::BlockHashes stage(&node_settings);
 
-            std::vector<std::pair<evmc::bytes32, BlockNum>> written_data{};
-            db::WalkFunc walk_func = [&written_data](::mdbx::cursor&, ::mdbx::cursor::move_result& data) -> bool {
-                auto written_block_num{endian::load_big_u64(static_cast<uint8_t*>(data.value.data()))};
-                auto written_hash{to_bytes32(db::from_slice(data.key))};
-                written_data.emplace_back(written_hash, written_block_num);
-                return true;
-            };
-            (void)db::cursor_for_each(target_table, walk_func);
+            // Forward
+            auto stage_result{stage.forward(txn)};
+            REQUIRE(stage_result == stagedsync::StageResult::kSuccess);
+            txn.commit(true);
 
-            REQUIRE(written_data.size() == block_hashes.size() + 1);
-            for (const auto& [written_hash, written_block_num] : written_data) {
-                REQUIRE(written_block_num < block_hashes.size() + 1);
-                if (written_block_num) {
-                    REQUIRE(written_hash == block_hashes.at(written_block_num - 1));
+            {
+                // Verify written data is consistent
+                auto target_table{db::open_cursor(*txn, db::table::kHeaderNumbers)};
+                REQUIRE(txn->get_map_stat(target_table.map()).ms_entries == block_hashes.size() + 1);  // Block 0 is genesis
+
+                std::vector<std::pair<evmc::bytes32, BlockNum>> written_data{};
+                db::WalkFunc walk_func = [&written_data](::mdbx::cursor&, ::mdbx::cursor::move_result& data) -> bool {
+                    auto written_block_num{endian::load_big_u64(static_cast<uint8_t*>(data.value.data()))};
+                    auto written_hash{to_bytes32(db::from_slice(data.key))};
+                    written_data.emplace_back(written_hash, written_block_num);
+                    return true;
+                };
+                (void)db::cursor_for_each(target_table, walk_func);
+
+                REQUIRE(written_data.size() == block_hashes.size() + 1);
+                for (const auto& [written_hash, written_block_num] : written_data) {
+                    REQUIRE(written_block_num < block_hashes.size() + 1);
+                    if (written_block_num) {
+                        REQUIRE(written_hash == block_hashes.at(written_block_num - 1));
+                    }
                 }
             }
-        }
 
-        // Unwind
-        stage_result = stage.unwind(txn, 1);
-        REQUIRE(stage_result == stagedsync::StageResult::kSuccess);
-        {
-            // Verify written data is consistent
-            auto target_table{db::open_cursor(*txn, db::table::kHeaderNumbers)};
-            REQUIRE(txn->get_map_stat(target_table.map()).ms_entries == 2);
-            REQUIRE(target_table.seek(db::to_slice(block_hashes.back())) == false);
+            // Unwind
+            stage_result = stage.unwind(txn, 1);
+            REQUIRE(stage_result == stagedsync::StageResult::kSuccess);
+            {
+                // Verify written data is consistent
+                auto target_table{db::open_cursor(*txn, db::table::kHeaderNumbers)};
+                REQUIRE(txn->get_map_stat(target_table.map()).ms_entries == 2);
+                REQUIRE(target_table.seek(db::to_slice(block_hashes.back())) == false);
+            }
+
         }
     }
 

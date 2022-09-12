96b9d3db18385f3a4fd085eb9b5a1d5481a9456f
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2022-09-12 17:42:22 +0200

    Temporarily commenting out merge engine creation to do a sync test

--- core/silkworm/consensus/engine.cpp
+++ core/silkworm/consensus/engine.cpp
@@ -93,13 +93,17 @@ static std::unique_ptr<IEngine> pre_merge_engine(const ChainConfig& chain_config
 
 std::unique_ptr<IEngine> engine_factory(const ChainConfig& chain_config) {
     std::unique_ptr<IEngine> engine{pre_merge_engine(chain_config)};
-    if (!engine) return nullptr;
+    return engine;
+    // temporarily commenting out merge engine creation to do a sync test
+    /*
+    if (!engine) {
+        return nullptr;
+    }
 
     if (chain_config.terminal_total_difficulty.has_value()) {
         engine = std::make_unique<MergeEngine>(std::move(engine), chain_config);
     }
-
-    return engine;
+    */
 }
 
 }  // namespace silkworm::consensus

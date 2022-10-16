10b8317822ec0af3160be025b2acd518787d2f7b
canepat <16927169+canepat@users.noreply.github.com>
2022-10-17 00:47:32 +0200

    Disable -Werror for Google Benchmark build to avoid bug in GCC 12.1.0 (2nd try)

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -27,8 +27,9 @@ hunter_cmake_args(
 # Avoid -Werror to overcome GCC 12.1.0 bug breaking Google Benchmark build
 # https://github.com/google/benchmark/issues/1398
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
-hunter_cmake_args(
+hunter_config(
   benchmark
+  VERSION 1.6.1
   CMAKE_ARGS BENCHMARK_ENABLE_WERROR=OFF
 )
 

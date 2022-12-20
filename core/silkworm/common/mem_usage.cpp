b7d667de3c40b403cd9d59a838fede904bd2583b
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2022-12-20 16:52:45 +0100

    Conform to some coding conventions

--- core/silkworm/common/mem_usage.cpp
+++ core/silkworm/common/mem_usage.cpp
@@ -1,17 +1,17 @@
 /*
-Copyright 2022 The Silkworm Authors
+   Copyright 2022 The Silkworm Authors
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+       http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
 #include <memory>
@@ -76,48 +76,3 @@ size_t get_mem_usage(bool resident) {
     return 0;
 #endif
 }
-
-/* previous implementation
-
-#include <regex>
-
-#if defined(_WIN32)
-#include <Psapi.h>
-#endif
-
-#if defined(__linux__)
-#include <fstream>
-#include <regex>
-#endif
-
-
-size_t get_mem_usage() {
-    size_t ret{0};
-#if defined(_WIN32)
-
-    static HANDLE phandle{GetCurrentProcess()};
-    PROCESS_MEMORY_COUNTERS_EX pmc;
-    (void)K32GetProcessMemoryInfo(phandle, (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
-    ret = pmc.WorkingSetSize;
-
-#endif
-
-#if defined(__linux__)
-
-    static const std::regex pattern{R"(^VmRSS:\s*(\d*)\s*kB$)", std::regex_constants::icase};
-    std::smatch matches;
-    std::string line;
-    std::ifstream input("/proc/self/status");
-    while (std::getline(input, line)) {
-        if (std::regex_search(line, matches, pattern, std::regex_constants::match_default)) {
-            std::string int_part = matches[1].str();
-            auto value{std::strtoull(int_part.c_str(), nullptr, 10)};
-            ret = value * 1_Kibi;
-            break;
-        }
-    }
-
-#endif
-    return ret;
-}
-*/

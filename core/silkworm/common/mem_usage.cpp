9c2d9cf5ac4d4398076f79e01de9d704db37acd1
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2022-12-21 12:51:11 +0100

    Avoid unused return value warning

--- core/silkworm/common/mem_usage.cpp
+++ core/silkworm/common/mem_usage.cpp
@@ -49,7 +49,7 @@ size_t get_mem_usage(bool resident) {
     FILE* file = fopen("/proc/self/statm", "r");
     if (file) {
         unsigned long vm = 0, rm = 0;
-        fscanf(file, "%lu %lu", &vm, &rm);  // the first 2 num: vm size, resident set size
+        (void)fscanf(file, "%lu %lu", &vm, &rm);  // the first 2 num: vm size, resident set size
         fclose(file);
         vm_size = vm * static_cast<size_t>(getpagesize());
         rm_size = rm * static_cast<size_t>(getpagesize());

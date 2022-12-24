62b555be15f3a0c9878a100b762e93c46c7a5325
canepat <16927169+canepat@users.noreply.github.com>
2022-12-24 18:53:55 +0100

    Refactor unit test

--- node/silkworm/recsplit/support/murmur_hash3_test.cpp
+++ node/silkworm/recsplit/support/murmur_hash3_test.cpp
@@ -16,35 +16,35 @@
 
 #include "murmur_hash3.hpp"
 
-#include <cstdint>
+#include <cstddef>
 #include <cstring>
-#include <memory>
 
 #include <catch2/catch.hpp>
 
 namespace silkworm::succinct {
 
-using HashFunction = void (*)(const void*, uint64_t, uint32_t, void*);
+TEST_CASE("MurmurHash3_x64_128", "[silkworm][recsplit][support]") {
+    constexpr std::size_t kHashBits{128};
+    constexpr std::size_t kExpectedVerification{0x6384BA69};
 
-static bool verification_test(HashFunction hash, const std::size_t hash_bits, int expected) {
-    const auto hash_bytes = hash_bits / 8u;
+    constexpr std::size_t kHashBytes = kHashBits / 8u;
 
     uint8_t* key{new uint8_t[256]};
-    uint8_t* hashes{new uint8_t[hash_bytes * 256u]};
-    uint8_t* res{new uint8_t[hash_bytes]};
+    uint8_t* hashes{new uint8_t[kHashBytes * 256u]};
+    uint8_t* res{new uint8_t[kHashBytes]};
 
     std::memset(key, 0, 256);
-    std::memset(hashes, 0, hash_bytes * 256);
-    std::memset(res, 0, hash_bytes);
+    std::memset(hashes, 0, kHashBytes * 256);
+    std::memset(res, 0, kHashBytes);
 
     // Hash keys of the form {0}, {0,1}, {0,1,2}... up to N=255 using 256-N as the seed
     for (std::size_t i{0}; i < 256; ++i) {
         key[i] = static_cast<uint8_t>(i);
-        hash(key, i, 256 - i, &hashes[i * hash_bytes]);
+        MurmurHash3_x64_128(key, i, 256 - i, &hashes[i * kHashBytes]);
     }
 
     // Then hash the result array
-    hash(hashes, hash_bytes * 256, 0, res);
+    MurmurHash3_x64_128(hashes, kHashBytes * 256, 0, res);
 
     // The first four bytes of that hash, interpreted as a LE integer, is our verification value
     const int verification = (res[0] << 0) | (res[1] << 8) | (res[2] << 16) | (res[3] << 24);
@@ -53,11 +53,7 @@ static bool verification_test(HashFunction hash, const std::size_t hash_bits, in
     delete[] hashes;
     delete[] res;
 
-    return verification == expected;
-}
-
-TEST_CASE("MurmurHash3_x64_128", "[silkworm][recsplit][support]") {
-    CHECK(verification_test(MurmurHash3_x64_128, 128, 0x6384BA69));
+    CHECK(verification == kExpectedVerification);
 }
 
 TEST_CASE("Murmur3", "[silkworm][recsplit][support]") {

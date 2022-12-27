06b895c3575da82691e04552e873406ebb5837b6
yperbasis <andrey.ashikhmin@gmail.com>
2022-12-27 15:05:47 +0100

    Withdrawal RLP

---
+++ core/silkworm/types/withdrawal.cpp
@@ -0,0 +1,40 @@
+/*
+   Copyright 2022 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "withdrawal.hpp"
+
+#include <silkworm/rlp/encode.hpp>
+
+namespace silkworm::rlp {
+
+static Header header(const Withdrawal& w) {
+    Header h{.list = true};
+    h.payload_length += length(w.index);
+    h.payload_length += length(w.validator_index);
+    h.payload_length += length(w.address);
+    h.payload_length += length(w.amount);
+    return h;
+}
+
+void encode(Bytes& to, const Withdrawal& w) {
+    encode_header(to, header(w));
+    encode(to, w.index);
+    encode(to, w.validator_index);
+    encode(to, w.address);
+    encode(to, w.amount);
+}
+
+}  // namespace silkworm::rlp

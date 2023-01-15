e89444a9577b13413f784b25bf83021ff000d80c
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-01-15 18:50:43 +0100

    removes cout

--- core/silkworm/consensus/clique/engine.cpp
+++ core/silkworm/consensus/clique/engine.cpp
@@ -31,18 +31,13 @@ CliqueEngine::ecrecover(const BlockHeader& header) {
     evmc::address beneficiary = evmc::address{};
 
     evmc::bytes32 seal_hash = header.hash(false, true);
-    std::cout << "erecover: seal-hash: " << silkworm::to_hex(seal_hash.bytes) << "\n";
     Bytes signature = header.extra_data.substr(header.extra_data.length() - kEntraSealSize, kEntraSealSize - 1);
-    std::cout << "erecover: signature: " << silkworm::to_hex(signature) << "\n";
     bool odd_y_parity = header.extra_data[header.extra_data.length() - 1] == 1;
-    std::cout << "erecover: parity: " << odd_y_parity << "\n";
 
     static secp256k1_context* context{secp256k1_context_create(SILKPRE_SECP256K1_CONTEXT_FLAGS)};
     if (!silkpre_recover_address(beneficiary.bytes, seal_hash.bytes, signature.c_str(), odd_y_parity, context)) {
         return header.beneficiary;
     }
-    std::cout << "erecover: beneficiary: " << silkworm::to_hex(beneficiary) << "\n";
-
     return beneficiary;
 }
 

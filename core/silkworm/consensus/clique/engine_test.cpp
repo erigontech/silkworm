72d1ae88c71ee25fa4f6559c4a87c16954e77d8f
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-01-18 23:08:57 +0100

    correct test

--- core/silkworm/consensus/clique/engine_test.cpp
+++ core/silkworm/consensus/clique/engine_test.cpp
@@ -61,14 +61,18 @@ TEST_CASE("get_beneficiary() && extra_data with seal") {
     header.gas_limit = 29999972;
     header.gas_used = 798242;
     header.timestamp = 1643045393;
-    header.mix_hash = 0x2f73f29450aad18c0956ec6350524c2910f3be67ec6e80b7b597240a195788e1_bytes32;
-    header.extra_data = string_view_to_byte_view(
-        "d883010a0d846765746888676f312e31372e33856c696e7578000000000000002ab85c52944f7ced556a\
-                         389a8044be45c006fca6ab41adf927f05f8c66a5debd68218cc4cf4e578581ca7db3c77efd6bbdabf0d435c5cfa68b5e80aa0798fece01");
-
+    std::string bloom_str = "0020000000000080100920000000000008000000000000000000000000100000000002000010100000000410000000000000000000000400000";
+    bloom_str.append("0080010200000000000004000001021000048000000000000000000000400000000080000002000030000040010808000000000");
+    bloom_str.append("0001000000200000000080800000100002000800000100000000002000400000800000000020000000000000900800200000804200400");
+    bloom_str.append("000110010000000000004000000008400000000000000008000000c000000000200000004420000000000421000400002000000000000");
+    bloom_str.append("0000000000000210000083000820090000200000400100000008000000000000000000000000");
+    auto bloom = *from_hex(bloom_str);
+    std::copy(bloom.begin(), bloom.end(), header.logs_bloom.begin());
+    std::string extra_data_str = "d883010a0d846765746888676f312e31372e33856c696e7578000000000000002ab85c52944f7ced556a";
+    extra_data_str.append("389a8044be45c006fca6ab41adf927f05f8c66a5debd68218cc4cf4e578581ca7db3c77efd6bbdabf0d435c5cfa68b5e80aa0798fece01");
+    header.extra_data = *from_hex(extra_data_str);
     auto consensus_engine = engine_factory(ChainConfig{.seal_engine = SealEngineType::kClique});  // Clique consensus engine
-    auto address = consensus_engine->get_beneficiary(header);
-    CHECK(address == 0x0000000000000000000000000000000000000000_address);  // temporary
+    CHECK(consensus_engine->get_beneficiary(header) == 0x6db839fd90ad468df541d3405fea73e6b85ff61f_address);
 }
 
 TEST_CASE("get_beneficiary() && extra_data no seal") {
@@ -86,10 +90,8 @@ TEST_CASE("get_beneficiary() && extra_data no seal") {
     header.mix_hash = 0x2f73f29450aad18c0956ec6350524c2910f3be67ec6e80b7b597240a195788e1_bytes32;
     header.nonce = {};
     header.extra_data = string_view_to_byte_view("d883010a0d84");
-
     auto consensus_engine = engine_factory(ChainConfig{.seal_engine = SealEngineType::kClique});  // Clique consensus engine
-    auto address = consensus_engine->get_beneficiary(header);
-    CHECK(address == 0x0000000000000000000000000000000000000000_address);
+    CHECK(consensus_engine->get_beneficiary(header) == 0x0000000000000000000000000000000000000000_address);
 }
 
 }  // namespace silkworm::consensus

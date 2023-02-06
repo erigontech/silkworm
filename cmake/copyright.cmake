440af34f42e9cf24804c4746695aa2be7df16462
battlmonstr <battlmonstr@users.noreply.github.com>
2023-02-06 14:40:09 +0100

    update copyright year to 2023

--- cmake/copyright.cmake
+++ cmake/copyright.cmake
@@ -1,5 +1,5 @@
 #[[
-   Copyright 2022 The Silkworm Authors
+   Copyright 2023 The Silkworm Authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 SET(COPYRIGHT_HEADER
 "/*
-   Copyright 2022 The Silkworm Authors
+   Copyright 2023 The Silkworm Authors
 
    Licensed under the Apache License, Version 2.0 (the \"License\");
    you may not use this file except in compliance with the License.

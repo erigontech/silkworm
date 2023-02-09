4911f5121b5fcf53fbb68840426a29ccc8406efa
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-02-09 19:33:33 +0100

    Unskip execution engine test

--- node/silkworm/stagedsync/execution_engine_test.cpp
+++ node/silkworm/stagedsync/execution_engine_test.cpp
@@ -41,7 +41,7 @@ class ExecutionEngine_ForTest : public stagedsync::ExecutionEngine {
     using stagedsync::ExecutionEngine::tx_;
 };
 
-TEST_CASE("ExecutionEngine", "[.]") {
+TEST_CASE("ExecutionEngine") {
     test::SetLogVerbosityGuard log_guard(log::Level::kNone);
 
     test::Context context;

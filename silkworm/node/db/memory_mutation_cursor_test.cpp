439c15bca11db991f3a71f7c3e092e47847e2718
canepat <16927169+canepat@users.noreply.github.com>
2023-03-12 23:12:09 +0100

    Fix format

--- silkworm/node/db/memory_mutation_cursor_test.cpp
+++ silkworm/node/db/memory_mutation_cursor_test.cpp
@@ -26,8 +26,7 @@ const MapConfig kTestMap{"TestTable"};
 const MapConfig kTestMultiMap{"TestMultiTable", mdbx::key_mode::usual, mdbx::value_mode::multi};
 
 // Avoid false positive from TSAN on potential cycle lock acquisition
-[[gnu::no_sanitize_thread, clang::no_sanitize("thread")]]
-static mdbx::env_managed create_main_env(const db::EnvConfig& main_db_config) {
+[[gnu::no_sanitize_thread, clang::no_sanitize("thread")]] static mdbx::env_managed create_main_env(const db::EnvConfig& main_db_config) {
     auto main_env = db::open_env(main_db_config);
     RWTxn main_txn{main_env};
     table::check_or_create_chaindata_tables(main_txn);

6056d3b0da74067a4ad5e0f68fcb1c492140d526
yperbasis <andrey.ashikhmin@gmail.com>
2023-03-21 17:02:22 +0100

    Remove unnecessary silkworm::

--- silkworm/core/crypto/sha256_test.cpp
+++ silkworm/core/crypto/sha256_test.cpp
@@ -27,7 +27,7 @@ TEST_CASE("SHA256 of empty string") {
     CHECK(to_hex(hash, 32) == "0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 
     silkworm_sha256(hash, nullptr, 0, /*use_cpu_extensions=*/true);
-    CHECK(silkworm::to_hex(hash, 32) == "0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    CHECK(to_hex(hash, 32) == "0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 }
 
 TEST_CASE("SHA256 sample") {

5a67d41bafd4477fd7864ba70e5a475618a28710
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-03-21 22:06:54 +0100

    .

--- silkworm/silkrpc/types/log.hpp
+++ silkworm/silkrpc/types/log.hpp
@@ -22,6 +22,25 @@
 #include <evmc/evmc.hpp>
 
 #include <silkworm/core/common/base.hpp>
+#include <glaze/glaze.hpp>
+#include <glaze/core/meta.hpp>
+
+template <>
+struct glz::meta<evmc::bytes32> {
+   using T = evmc::bytes32;
+   static constexpr auto value = object(
+      "bytes", &T::bytes, "bytes is a array"
+   );
+};
+
+template <>
+struct glz::meta<evmc::address> {
+   using T = evmc::address;
+   static constexpr auto value = object(
+      "bytes", &T::bytes, "bytes is a array"
+   );
+};
+
 
 namespace silkrpc {
 
@@ -38,9 +57,69 @@ struct Log {
     evmc::bytes32 block_hash;
     uint32_t index{0};
     bool removed{false};
+
+#ifdef notdef
+    struct glaze {
+     using T = Log;
+     static constexpr auto value = glz::object(
+        "address", &T::address,
+        "topics", &T::topics,
+        "data", &T::data,
+        "block_number", &T::block_number,
+        "tx_hash", &T::tx_hash,
+        "tx_index", &T::tx_index,
+        "block_hash", &T::block_hash,
+        "index", &T::index,
+        "removed", &T::removed
+     );
+  };
+#endif
 };
 
 typedef std::vector<Log> Logs;
+
+// INPUT
+struct eth_log_item {
+    //char from_block[16];
+    //char to_block[16];
+    //char address[128];
+    std::string from_block;
+    std::string to_block;
+    std::string address;
+    std::vector<std::vector<std::string>> topics;
+    //std::vector<std::string> topics1;
+    std::string block_hash;
+
+        //"topics", &T::topics1,
+    struct glaze {
+     using T = eth_log_item;
+     static constexpr auto value = glz::object(
+        "address", &T::address,
+        "fromBlock", &T::from_block,
+        "toBlock", &T::to_block,
+        "topics", &T::topics,
+        "blockHash", &T::block_hash
+     );
+   };
+};
+
+struct eth_getLogs_request_json {
+    //char jsonrpc[8];
+    //char method[32];
+    std::string jsonrpc;
+    std::string method;
+    std::vector<eth_log_item> params;
+    uint32_t id;
+    struct glaze {
+     using T = eth_getLogs_request_json;
+     static constexpr auto value = glz::object(
+        "jsonrpc", &T::jsonrpc,
+        "id", &T::id,
+        "method", &T::method,
+        "params", &T::params
+     );
+   };
+};
 
 std::ostream& operator<<(std::ostream& out, const Log& log);
 

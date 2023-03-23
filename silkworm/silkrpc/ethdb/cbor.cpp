39de53b80b49a60e341ef25c1367aec643d8cf79
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-03-23 14:38:53 +0100

    changed

--- silkworm/silkrpc/ethdb/cbor.cpp
+++ silkworm/silkrpc/ethdb/cbor.cpp
@@ -16,32 +16,55 @@
 
 #include "cbor.hpp"
 
+#include <cbor/cbor.h>
 #include <nlohmann/json.hpp>
+#include "listener_cbor_log.h"
 
 #include <silkworm/silkrpc/common/log.hpp>
 #include <silkworm/silkrpc/json/types.hpp>
 
 namespace silkrpc {
 
+
 bool cbor_decode(const silkworm::Bytes& bytes, std::vector<Log>& logs) {
     if (bytes.size() == 0) {
         return false;
     }
+    cbor::input input((void *)bytes.data(), bytes.size());
+    listener_cbor_log listener(logs);
+    cbor::decoder decoder(input, listener);
+    decoder.run();
+    if (!listener.is_processing_terminated_successfully()) {
+        SILKRPC_ERROR << "cbor_decode<std::vector<Log>> unexpected cbor" << "\n";
+        return false;
+    }
+    //for (const auto& ll : logs) {
+    //   std::cout << "Log: " << ll << "\n";
+    //}
+    return true;
+    
+#ifdef notdef
     auto json = nlohmann::json::from_cbor(bytes);
-    SILKRPC_TRACE << "cbor_decode<std::vector<Log>> json: " << json.dump() << "\n";
+    SILKRPC_DEBUG << "cbor_decode<std::vector<Log>> json: " << json.dump() << "\n";
     if (json.is_array()) {
         logs = json.get<std::vector<Log>>();
+        std::cout << "nlohman::cbor\n";
+        for (const auto& ll : logs) {
+           std::cout << "Log: " << ll << "\n";
+        }
         return true;
     } else {
         SILKRPC_ERROR << "cbor_decode<std::vector<Log>> unexpected json: " << json.dump() << "\n";
         return false;
     }
+#endif
 }
 
 bool cbor_decode(const silkworm::Bytes& bytes, std::vector<Receipt>& receipts) {
     if (bytes.size() == 0) {
         return false;
     }
+    std::cout << "cbor_decode receipt: " << bytes.size() << "\n";
     auto json = nlohmann::json::from_cbor(bytes);
     SILKRPC_TRACE << "cbor_decode<std::vector<Receipt>> json: " << json.dump() << "\n";
     if (json.is_array()) {
@@ -52,5 +75,6 @@ bool cbor_decode(const silkworm::Bytes& bytes, std::vector<Receipt>& receipts) {
         return false;
     }
 }
+
 
 } // namespace silkrpc

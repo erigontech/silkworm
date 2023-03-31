997563dd0f5eb455e1eccaf600ea825d12198881
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-03-31 08:51:51 +0200

    lint fix

--- silkworm/silkrpc/commands/rpc_api_table.cpp
+++ silkworm/silkrpc/commands/rpc_api_table.cpp
@@ -24,7 +24,7 @@
 
 #define API_WITH_GLAZE 1
 
-namespace silkrpc::rpc::commands {
+namespace silkworm::rpc::commands {
 
 RpcApiTable::RpcApiTable(const std::string& api_spec) {
     build_handlers(api_spec);
@@ -220,4 +220,4 @@ void RpcApiTable::add_ots_handlers() {
     method_handlers_[http::method::k_ots_getBlockDetailsByHash] = &commands::RpcApi::handle_ots_getBlockDetailsByHash;
 }
 
-}  // namespace silkrpc::rpc::commands
+}  // namespace silkworm::rpc::commands

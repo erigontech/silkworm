084cb701d5741a5e8c6d9c1ccbda517fb114392e
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-04-01 13:04:01 +0200

    lint fix

--- silkworm/silkrpc/json/types.cpp
+++ silkworm/silkrpc/json/types.cpp
@@ -17,8 +17,8 @@
 #include "types.hpp"
 
 #include <cstring>
-#include <utility>
 #include <span>
+#include <utility>
 
 #include <boost/endian/conversion.hpp>
 #include <intx/intx.hpp>
@@ -35,8 +35,8 @@ using evmc::literals::operator""_address;
 void to_hex(std::span<char> hex_bytes, silkworm::ByteView bytes) {
     static const char* kHexDigits{"0123456789abcdef"};
     if (bytes.size() * 2 + 2 > hex_bytes.size()) {
-       throw std::invalid_argument("to_hex: hex_bytes too small");
-    } 
+        throw std::invalid_argument("to_hex: hex_bytes too small");
+    }
     char* dest = hex_bytes.data();
     *dest++ = '0';
     *dest++ = 'x';
@@ -51,7 +51,7 @@ std::size_t to_hex_no_leading_zeros(std::span<char> hex_bytes, silkworm::ByteVie
     size_t len = bytes.length();
     if (len * 2 + 2 > hex_bytes.size()) {
         throw std::invalid_argument("to_hex_no_leading_zeros: hex_bytes too small");
-    } 
+    }
     char* dest = hex_bytes.data();
     *dest++ = '0';
     *dest++ = 'x';
@@ -862,10 +862,10 @@ void make_glaze_json_error(std::string& reply, uint32_t id, const int code, cons
     glz::write_json(glaze_json_error, reply);
 }
 
-//static constexpr auto addressSize = 64;
-//static constexpr auto hashSize = 128;
-//static constexpr auto int64Size = 32;
-//static constexpr auto dataSize = 4096;
+// static constexpr auto addressSize = 64;
+// static constexpr auto hashSize = 128;
+// static constexpr auto int64Size = 32;
+// static constexpr auto dataSize = 4096;
 
 void make_glaze_json_content(std::string& reply, uint32_t id, const Logs& logs) {
     GlazeJsonLog log_json_data{};

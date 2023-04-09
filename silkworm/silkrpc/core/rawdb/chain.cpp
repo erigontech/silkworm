8500c7fd99716feec790a645f29a8f44ee52b6f8
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-04-09 23:32:15 +0200

    .

--- silkworm/silkrpc/core/rawdb/chain.cpp
+++ silkworm/silkrpc/core/rawdb/chain.cpp
@@ -141,8 +141,8 @@ boost::asio::awaitable<std::shared_ptr<silkworm::BlockWithHash>> read_block_NEW(
     SILKRPC_INFO << "header: number=" << block_with_hash_ptr->block.header.number << "\n";
     auto body = co_await read_body(reader, block_hash, block_number);
     SILKRPC_INFO << "body: #txn=" << body.transactions.size() << " #ommers=" << body.ommers.size() << "\n";
-    block_with_hash_ptr->block.transactions = body.transactions,
-    block_with_hash_ptr->block.ommers = body.ommers,
+    block_with_hash_ptr->block.transactions = std::move(body.transactions);
+    block_with_hash_ptr->block.ommers = std::move(body.ommers),
     block_with_hash_ptr->hash = block_hash;
     co_return block_with_hash_ptr;
 }

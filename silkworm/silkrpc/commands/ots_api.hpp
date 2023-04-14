c27bb635776dc05eb0f25511e3a32fe2e6904746
dario <dario.gagliano@outlook.com>
2023-04-14 15:55:19 +0200

    ots_getTransactionBySenderAndNonce implementation

--- silkworm/silkrpc/commands/ots_api.hpp
+++ silkworm/silkrpc/commands/ots_api.hpp
@@ -53,6 +53,7 @@ class OtsRpcApi {
     boost::asio::awaitable<void> handle_ots_getBlockDetails(const nlohmann::json& request, nlohmann::json& reply);
     boost::asio::awaitable<void> handle_ots_getBlockDetailsByHash(const nlohmann::json& request, nlohmann::json& reply);
     boost::asio::awaitable<void> handle_ots_getBlockTransactions(const nlohmann::json& request, nlohmann::json& reply);
+    boost::asio::awaitable<void> handle_ots_getTransactionBySenderAndNonce(const nlohmann::json& request, nlohmann::json& reply);
 
     std::unique_ptr<ethdb::Database>& database_;
     std::shared_ptr<ethdb::kv::StateCache>& state_cache_;

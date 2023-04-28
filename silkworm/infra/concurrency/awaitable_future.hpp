8aea027b76271ef7d964d428ea0b0b276a48401c
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-04-28 13:21:26 +0200

    infra: future/promise compatible w/ different Asio completion tokens (#999)

---
+++ silkworm/infra/concurrency/awaitable_future.hpp
@@ -0,0 +1,99 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#pragma once
+
+#include <stdexcept>
+
+#include <silkworm/infra/concurrency/coroutine.hpp>
+
+#include <boost/asio/any_io_executor.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/experimental/concurrent_channel.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/use_future.hpp>
+
+namespace silkworm::concurrency {
+
+namespace asio = boost::asio;
+
+// An awaitable-friendly future/promise
+
+template <typename T>
+class AwaitablePromise;
+
+template <typename T>
+class AwaitableFuture {
+  public:
+    AwaitableFuture(const AwaitableFuture&) = delete;
+    AwaitableFuture(AwaitableFuture&& orig) : channel_(std::move(orig.channel_)) {}
+
+    asio::awaitable<T> get_async() {
+        return get(asio::use_awaitable);
+    }
+
+    T get() {
+        return get(asio::use_future).get();
+    }
+
+  private:
+    friend class AwaitablePromise<T>;
+
+    using AsyncChannel = asio::experimental::concurrent_channel<void(std::exception_ptr, T)>;
+
+    explicit AwaitableFuture(std::shared_ptr<AsyncChannel> channel) : channel_(channel) {}
+
+    template <typename CompletionToken>
+    auto get(CompletionToken completion_token) {
+        return channel_->async_receive(completion_token);
+    }
+
+    std::shared_ptr<AsyncChannel> channel_;
+};
+
+template <typename T>
+class AwaitablePromise {
+    inline static size_t one_shot_channel = 1;
+    using AsyncChannel = typename AwaitableFuture<T>::AsyncChannel;
+
+  public:
+    explicit AwaitablePromise(asio::any_io_executor&& executor) : channel_(std::make_shared<AsyncChannel>(executor, one_shot_channel)) {}
+    explicit AwaitablePromise(asio::any_io_executor& executor) : channel_(std::make_shared<AsyncChannel>(executor, one_shot_channel)) {}
+    explicit AwaitablePromise(asio::io_context& io_context) : channel_(std::make_shared<AsyncChannel>(io_context, one_shot_channel)) {}
+
+    AwaitablePromise(const AwaitablePromise&) = delete;
+    AwaitablePromise(AwaitablePromise&& orig) : channel_(std::move(orig.channel_)) {}
+
+    void set_value(T value) {
+        bool sent = channel_->try_send(nullptr, std::move(value));
+        if (!sent) throw std::runtime_error("AwaitablePromise::set_value: channel is full");
+    }
+
+    void set_exception(std::exception_ptr ptr) {
+        bool sent = channel_->try_send(ptr, T{});
+        if (!sent) throw std::runtime_error("AwaitablePromise::set_exception: channel is full");
+    }
+
+    AwaitableFuture<T> get_future() { return AwaitableFuture<T>(channel_); }
+
+  private:
+    std::shared_ptr<AsyncChannel> channel_;
+};
+
+}  // namespace silkworm::concurrency
---
+++ silkworm/infra/concurrency/awaitable_future_test.cpp
@@ -0,0 +1,242 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "awaitable_future.hpp"
+
+#include <catch2/catch.hpp>
+
+#include "active_component.hpp"
+
+namespace silkworm {
+
+namespace asio = boost::asio;
+using concurrency::AwaitableFuture;
+using concurrency::AwaitablePromise;
+
+auto create_promise_and_set_value(asio::io_context& io, int value) {
+    concurrency::AwaitablePromise<int> promise{io};
+    promise.set_value(value);
+    return promise.get_future();
+}
+
+TEST_CASE("awaitable future") {
+    asio::io_context io;
+    asio::io_context::work work{io};
+    // IOExecution execution{io};
+
+    SECTION("trivial use") {
+        AwaitablePromise<int> promise{io};
+
+        auto future = promise.get_future();
+
+        promise.set_value(42);
+        auto value = future.get();
+
+        CHECK(value == 42);
+        // The destructor of Executor calls stop() and join()
+    }
+
+    SECTION("variation of the trivial use") {
+        AwaitablePromise<int> promise{io};
+        promise.set_value(42);
+
+        auto future = promise.get_future();
+        auto value = future.get();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("setting exception instead of value") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        promise.set_exception(std::make_exception_ptr(std::exception()));
+
+        CHECK_THROWS(future.get());
+    }
+
+    SECTION("variation of setting exception instead of value") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        try {
+            throw std::exception();
+        } catch (...) {
+            promise.set_exception(std::current_exception());
+        }
+
+        CHECK_THROWS(future.get());
+    }
+
+    SECTION("setting value two times") {
+        AwaitablePromise<int> promise{io};
+
+        promise.set_value(42);
+
+        CHECK_THROWS(promise.set_value(43));
+    }
+
+    SECTION("returning the future from a function") {
+        auto future = create_promise_and_set_value(io, 42);
+
+        auto value = future.get();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("returning the future from a function (variation)") {
+        auto returned_future = [&]() {
+            concurrency::AwaitablePromise<int> promise{io};
+            auto future = promise.get_future();
+            promise.set_value(42);
+            return future;
+        }();
+
+        auto value = returned_future.get();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("writing and reading from different threads") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        int value;
+        std::thread concurrent(
+            [&](AwaitableFuture<int>&& moved_future) {
+                value = moved_future.get();
+                io.stop();
+            },
+            std::move(future));
+
+        promise.set_value(42);
+
+        io.run();
+        concurrent.join();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("writing and reading from different threads") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        int value;
+        std::thread concurrent(
+            [&](AwaitableFuture<int>&& moved_future) {
+                value = moved_future.get();
+                io.stop();
+            },
+            std::move(future));
+
+        asio::co_spawn(
+            io,
+            [&]() -> asio::awaitable<void> {
+                promise.set_value(42);
+                co_return;
+            },
+            asio::detached);
+
+        io.run();
+        concurrent.join();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("using coroutines in read in the same io_context, write before read") {
+        AwaitablePromise<int> promise{io};
+        int value;
+
+        asio::co_spawn(
+            io,
+            [&]() -> asio::awaitable<void> {
+                auto future = promise.get_future();
+                value = co_await future.get_async();
+                io.stop();
+            },
+            asio::detached);
+
+        promise.set_value(42);
+        io.run();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("variation of using coroutines in the same io_context, write before read") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        int value;
+        asio::co_spawn(
+            io,
+            [&]() -> asio::awaitable<void> {
+                value = co_await future.get_async();
+                io.stop();
+            },
+            asio::detached);
+
+        promise.set_value(42);
+        io.run();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("moving AwaitableFuture") {
+        AwaitablePromise<int> promise{io};
+        auto future = promise.get_future();
+
+        int value;
+        auto lambda = [&](AwaitableFuture<int>&& moved_future) -> asio::awaitable<void> {
+            value = co_await moved_future.get_async();
+            io.stop();
+        };
+
+        asio::co_spawn(io, lambda(std::move(future)), asio::detached);
+
+        promise.set_value(42);
+        io.run();
+
+        CHECK(value == 42);
+    }
+
+    SECTION("using coroutine for both read and write, read before write") {
+        AwaitablePromise<int> promise{io};
+        int value;
+
+        asio::co_spawn(
+            io,
+            [&]() -> asio::awaitable<void> {
+                auto future = promise.get_future();
+                value = co_await future.get_async();
+                io.stop();
+            },
+            asio::detached);
+
+        asio::co_spawn(
+            io,
+            [&]() -> asio::awaitable<void> {
+                promise.set_value(42);
+                co_return;
+            },
+            asio::detached);
+
+        io.run();
+
+        CHECK(value == 42);
+    }
+}
+
+}  // namespace silkworm

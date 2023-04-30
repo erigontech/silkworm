7585b1a1414baab13546b255499a480afc7133ec
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-04-30 16:24:18 +0200

    Add sync_wait

---
+++ silkworm/infra/concurrency/sync_wait_test.cpp
@@ -0,0 +1,79 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "sync_wait.hpp"
+
+#include <chrono>
+
+#include <boost/asio.hpp>
+#include <catch2/catch.hpp>
+
+namespace silkworm {
+
+namespace asio = boost::asio;
+
+asio::awaitable<void> dummy_task() {
+    auto executor = co_await asio::this_coro::executor;
+
+    asio::steady_timer timer{executor};
+    timer.expires_after(std::chrono::milliseconds(100));
+
+    co_await timer.async_wait(asio::use_awaitable);
+}
+
+class DummyEngine {
+    asio::io_context& io;
+
+  public:
+    DummyEngine(asio::io_context& io) : io{io} {}
+
+    asio::awaitable<int> do_work() {
+        co_return 42;
+    }
+
+    asio::io_context& get_executor() {
+        return io;
+    }
+};
+
+TEST_CASE("sync wait") {
+    asio::io_context io;
+    asio::io_context::work work{io};
+
+    SECTION("wait for function") {
+        std::thread io_execution([&io]() { io.run(); });
+
+        sync_wait(io, dummy_task());
+
+        io.stop();
+        io_execution.join();
+    }
+
+    SECTION("wait for method") {
+        std::thread io_execution([&io]() { io.run(); });
+
+        DummyEngine engine{io};
+
+        auto value = sync_wait(in(engine), engine.do_work());
+
+        CHECK(value == 42);
+
+        io.stop();
+        io_execution.join();
+    }
+}
+
+}  // namespace silkworm

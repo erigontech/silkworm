9390f4cefb00b7189f4941558cc9ea8ec94fbf85
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-06 14:17:42 +0200

    removes transition_configuration json file

--- silkworm/silkrpc/json/types.cpp
+++ silkworm/silkrpc/json/types.cpp
@@ -537,19 +537,6 @@ void to_json(nlohmann::json& json, const PayloadStatus& payload_status) {
     }
 }
 
-void to_json(nlohmann::json& json, const TransitionConfiguration& transition_configuration) {
-    json["terminalTotalDifficulty"] = to_quantity(transition_configuration.terminal_total_difficulty);
-    json["terminalBlockHash"] = transition_configuration.terminal_block_hash;
-    json["terminalBlockNumber"] = to_quantity(transition_configuration.terminal_block_number);
-}
-
-void from_json(const nlohmann::json& json, TransitionConfiguration& transition_configuration) {
-    transition_configuration = TransitionConfiguration{
-        .terminal_total_difficulty = json.at("terminalTotalDifficulty").get<intx::uint256>(),
-        .terminal_block_hash = json.at("terminalBlockHash").get<evmc::bytes32>(),
-        .terminal_block_number = static_cast<uint64_t>(std::stol(json.at("terminalBlockNumber").get<std::string>(), nullptr, 16))};
-}
-
 void to_json(nlohmann::json& json, const Forks& forks) {
     json["genesis"] = forks.genesis_hash;
     json["heightForks"] = forks.block_numbers;

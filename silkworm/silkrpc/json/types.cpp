2dedd6dbe771f95fd8ab4624ee5b11c2209f5abd
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-13 19:04:32 +0200

    remove from/to json for node_info

--- silkworm/silkrpc/json/types.cpp
+++ silkworm/silkrpc/json/types.cpp
@@ -293,22 +293,6 @@ void to_json(nlohmann::json& json, const Rlp& rlp) {
     json = "0x" + silkworm::to_hex(rlp.buffer);
 }
 
-void to_json(nlohmann::json& json, const NodeInfoPorts& node_info_ports) {
-    json["discovery"] = node_info_ports.discovery;
-    json["listener"] = node_info_ports.listener;
-}
-
-void to_json(nlohmann::json& json, const NodeInfo& node_info) {
-    json["id"] = node_info.id;
-    json["name"] = node_info.name;
-    json["enode"] = node_info.enode;
-    json["enr"] = node_info.enr;
-    json["listenAddr"] = node_info.listener_addr;
-    json["ports"] = node_info.ports;
-    json["ip"] = node_info.enode;
-    json["protocols"] = nlohmann::json::parse(node_info.protocols, nullptr, /* allow_exceptions = */ false);
-}
-
 void to_json(nlohmann::json& json, const PeerInfo& info) {
     json["id"] = info.id;
     json["name"] = info.name;

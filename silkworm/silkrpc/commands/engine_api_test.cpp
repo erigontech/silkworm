3ad9119bf6053de5f50726d086bb960088e0cf44
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-20 23:19:55 +0200

    rename get_state into create_state

--- silkworm/silkrpc/commands/engine_api_test.cpp
+++ silkworm/silkrpc/commands/engine_api_test.cpp
@@ -72,8 +72,8 @@ namespace {
             co_return nullptr;
         }
 
-        boost::asio::awaitable<std::shared_ptr<silkworm::State>> get_state(boost::asio::io_context& /* io_context */, const core::rawdb::DatabaseReader& /* db_reader */,
-                                                                           uint64_t /* block_number */) override {
+        boost::asio::awaitable<std::shared_ptr<silkworm::State>> create_state(boost::asio::io_context& /* io_context */, const core::rawdb::DatabaseReader& /* db_reader */,
+                                                                              uint64_t /* block_number */) override {
             co_return nullptr;
         }
 

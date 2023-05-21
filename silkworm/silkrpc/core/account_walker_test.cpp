a20814cd70cc761899920ecf00769fa05d140758
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-21 14:21:35 +0200

    remove awaitable for create_state

--- silkworm/silkrpc/core/account_walker_test.cpp
+++ silkworm/silkrpc/core/account_walker_test.cpp
@@ -169,9 +169,9 @@ class DummyTransaction : public ethdb::Transaction {
         co_return cursor;
     }
 
-    boost::asio::awaitable<std::shared_ptr<silkworm::State>> create_state(boost::asio::io_context& /* io_context */, const core::rawdb::DatabaseReader& /* db_reader */,
-                                                                          uint64_t /* block_number */) override {
-        co_return nullptr;
+    std::shared_ptr<silkworm::State> create_state(boost::asio::io_context& /* io_context */, const core::rawdb::DatabaseReader& /* db_reader */,
+                                                  uint64_t /* block_number */) override {
+        return nullptr;
     }
 
     boost::asio::awaitable<void> close() override {

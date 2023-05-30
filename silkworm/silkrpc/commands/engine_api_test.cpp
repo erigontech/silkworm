cdec088664baf50ceb69e08d14b8d796dd85f235
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-30 17:24:29 +0200

    mod create_state interface

--- silkworm/silkrpc/commands/engine_api_test.cpp
+++ silkworm/silkrpc/commands/engine_api_test.cpp
@@ -74,9 +74,8 @@ namespace {
             co_return nullptr;
         }
 
-        std::shared_ptr<silkworm::State> create_state(boost::asio::io_context& /* io_context */, const core::rawdb::DatabaseReader& /* db_reader */,
-                                                      uint64_t /* block_number */) override {
-            return nullptr;
+        awaitable<std::shared_ptr<silkworm::State>> create_state(const core::rawdb::DatabaseReader& /* db_reader */, uint64_t /* block_number */) override {
+            co_return nullptr;
         }
 
         awaitable<void> close() override { co_return; }

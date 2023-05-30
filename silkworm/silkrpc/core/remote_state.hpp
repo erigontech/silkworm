ba0e6f48bcea591c0f72187e4c2f8c452a066433
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-05-30 17:28:55 +0200

    remove io_context, add io_executor

--- silkworm/silkrpc/core/remote_state.hpp
+++ silkworm/silkrpc/core/remote_state.hpp
@@ -69,8 +69,8 @@ class AsyncRemoteState {
 
 class RemoteState : public silkworm::State {
   public:
-    explicit RemoteState(boost::asio::io_context& io_context, const core::rawdb::DatabaseReader& db_reader, uint64_t block_number)
-        : io_context_(io_context), async_state_{db_reader, block_number} {}
+    explicit RemoteState(boost::asio::any_io_executor& executor, const core::rawdb::DatabaseReader& db_reader, uint64_t block_number)
+        : executor_(executor), async_state_{db_reader, block_number} {}
 
     std::optional<silkworm::Account> read_account(const evmc::address& address) const noexcept override;
 
@@ -121,9 +121,10 @@ class RemoteState : public silkworm::State {
         const evmc::bytes32& /*current*/) override {}
 
     void unwind_state_changes(uint64_t /*block_number*/) override {}
+    boost::asio::awaitable<void> xxx(const evmc::address& address) const noexcept;
 
   private:
-    boost::asio::io_context& io_context_;
+    boost::asio::any_io_executor executor_;
     AsyncRemoteState async_state_;
 };
 

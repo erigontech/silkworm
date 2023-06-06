12129b25c41a1762e2cf7803965d9a3bed616f6b
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-06-07 00:09:47 +0200

    add reset method

--- silkworm/silkrpc/core/local_state.cpp
+++ silkworm/silkrpc/core/local_state.cpp
@@ -84,6 +84,11 @@ uint64_t LocalState::current_canonical_block() const {
     return 0;
 }
 
+void LocalState::reset() {
+    rotxn_.reset(nullptr);
+    rotxn_ = nullptr;
+}
+
 std::optional<evmc::bytes32> LocalState::canonical_hash(uint64_t block_number) const {
     if (!rotxn_) {
         rotxn_ = std::make_unique<db::ROTxn>(*chaindata_env_);

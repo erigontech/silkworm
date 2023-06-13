b3cc26cf22dac99498daef5a8846f05a9c3a2eff
battlmonstr <battlmonstr@users.noreply.github.com>
2023-06-13 11:32:12 +0200

    fix

--- cmake/run_unit_tests.sh
+++ cmake/run_unit_tests.sh
@@ -5,11 +5,6 @@ then
     ulimit -s unlimited
 fi
 
-if test `uname -s` = Darwin
-then
-    ulimit -n 1024
-fi
-
 script_dir=`dirname "$0"`
 
 cmake "-DSILKWORM_BUILD_DIR=$1" "-DSILKWORM_CLANG_COVERAGE=$2" -P "$script_dir/run_unit_tests.cmake"

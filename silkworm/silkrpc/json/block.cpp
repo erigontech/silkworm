bd41ec7c2dc129ef379835a0a4db195e6ea6a654
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-06-20 20:20:44 +0200

    add Block in separete module

---
+++ silkworm/silkrpc/json/block.cpp
@@ -0,0 +1,87 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include <cstring>
+#include <utility>
+
+#include <silkworm/core/common/util.hpp>
+#include <silkworm/infra/common/log.hpp>
+#include <silkworm/silkrpc/common/util.hpp>
+
+#include "filter.hpp"
+#include "types.hpp"
+
+namespace silkworm::rpc {
+
+void to_json(nlohmann::json& json, const Block& b) {
+    const auto block_number = to_quantity(b.block.header.number);
+    json["number"] = block_number;
+    json["hash"] = b.hash;
+    json["parentHash"] = b.block.header.parent_hash;
+    json["nonce"] = "0x" + silkworm::to_hex({b.block.header.nonce.data(), b.block.header.nonce.size()});
+    json["sha3Uncles"] = b.block.header.ommers_hash;
+    json["logsBloom"] = "0x" + silkworm::to_hex(full_view(b.block.header.logs_bloom));
+    json["transactionsRoot"] = b.block.header.transactions_root;
+    if (b.block.header.withdrawals_root) {
+        json["withdrawalsRoot"] = *(b.block.header.withdrawals_root);
+    }
+    json["stateRoot"] = b.block.header.state_root;
+    json["receiptsRoot"] = b.block.header.receipts_root;
+    json["miner"] = b.block.header.beneficiary;
+    json["difficulty"] = to_quantity(silkworm::endian::to_big_compact(b.block.header.difficulty));
+    json["totalDifficulty"] = to_quantity(silkworm::endian::to_big_compact(b.total_difficulty));
+    json["extraData"] = "0x" + silkworm::to_hex(b.block.header.extra_data);
+    json["mixHash"] = b.block.header.prev_randao;
+    json["size"] = to_quantity(b.get_block_size());
+    json["gasLimit"] = to_quantity(b.block.header.gas_limit);
+    json["gasUsed"] = to_quantity(b.block.header.gas_used);
+    if (b.block.header.base_fee_per_gas.has_value()) {
+        json["baseFeePerGas"] = to_quantity(b.block.header.base_fee_per_gas.value_or(0));
+    }
+    json["timestamp"] = to_quantity(b.block.header.timestamp);
+    if (b.full_tx) {
+        json["transactions"] = b.block.transactions;
+        for (std::size_t i{0}; i < json["transactions"].size(); i++) {
+            auto& json_txn = json["transactions"][i];
+            json_txn["transactionIndex"] = to_quantity(i);
+            json_txn["blockHash"] = b.hash;
+            json_txn["blockNumber"] = block_number;
+            json_txn["gasPrice"] = to_quantity(b.block.transactions[i].effective_gas_price(b.block.header.base_fee_per_gas.value_or(0)));
+        }
+    } else {
+        std::vector<evmc::bytes32> transaction_hashes;
+        transaction_hashes.reserve(b.block.transactions.size());
+        for (std::size_t i{0}; i < b.block.transactions.size(); i++) {
+            auto ethash_hash{hash_of_transaction(b.block.transactions[i])};
+            auto bytes32_hash = silkworm::to_bytes32({ethash_hash.bytes, silkworm::kHashLength});
+            transaction_hashes.emplace(transaction_hashes.end(), bytes32_hash);
+            SILK_DEBUG << "transaction_hashes[" << i << "]: " << silkworm::to_hex({transaction_hashes[i].bytes, silkworm::kHashLength});
+        }
+        json["transactions"] = transaction_hashes;
+    }
+    std::vector<evmc::bytes32> ommer_hashes;
+    ommer_hashes.reserve(b.block.ommers.size());
+    for (std::size_t i{0}; i < b.block.ommers.size(); i++) {
+        ommer_hashes.emplace(ommer_hashes.end(), b.block.ommers[i].hash());
+        SILK_DEBUG << "ommer_hashes[" << i << "]: " << silkworm::to_hex({ommer_hashes[i].bytes, silkworm::kHashLength});
+    }
+    json["uncles"] = ommer_hashes;
+    if (b.block.withdrawals) {
+        json["withdrawals"] = *(b.block.withdrawals);
+    }
+}
+
+}  // namespace silkworm::rpc

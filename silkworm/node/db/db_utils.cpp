e461868e6289007fd11be2a034b806f8a4ede431
canepat <16927169+canepat@users.noreply.github.com>
2023-06-25 21:00:06 +0200

    Update db_utils.cpp

--- silkworm/node/db/db_utils.cpp
+++ silkworm/node/db/db_utils.cpp
@@ -22,29 +22,8 @@
 
 namespace silkworm {
 
-// Read all headers up to limit, in reverse order from last, processing each via a user defined callback
-// alternative implementation: use cursor_for_count(cursor, WalkFuncRef, size_t max_count, CursorMoveDirection)
-/*
-void read_headers_in_reverse_order(db::ROTxn& txn, size_t limit, std::function<void(BlockHeader&&)> callback) {
-    db::PooledCursor header_table(txn, db::table::kHeaders);
-
-    bool throw_notfound = false;
-    size_t read = 0;
-    auto data = header_table.to_last(throw_notfound);
-    while (data && read < limit) {
-        // read header
-        BlockHeader header;
-        ByteView data_view = db::from_slice(data.value);
-        success_or_throw(rlp::decode(data_view, header));
-        read++;
-        // consume header
-        callback(std::move(header));
-        // move backward
-        data = header_table.to_previous(throw_notfound);
-    }
-}  // note: maybe we can simplify/replace the implementation with db::cursor_for_count plus lambda
-*/
-// An alternative implementation of read_headers_in_reverse_order that uses DataModel and is snapshot aware
+// Read all headers up to limit in reverse order from last, processing each one via a user defined callback
+// This implementation uses DataModel and is snapshot aware
 void for_last_n_headers(const db::DataModel& data_model, size_t n, std::function<void(BlockHeader&&)> callback) {
     auto highest_block_num = data_model.highest_block_number();
 

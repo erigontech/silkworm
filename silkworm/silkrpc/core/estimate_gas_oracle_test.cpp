c03604599bf5ae93cd805434dadcb8d05d22edbc
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-06-27 16:11:28 +0200

    use success/error_message methods

--- silkworm/silkrpc/core/estimate_gas_oracle_test.cpp
+++ silkworm/silkrpc/core/estimate_gas_oracle_test.cpp
@@ -102,7 +102,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call empty, always fails but success in last step") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         EXPECT_CALL(estimate_gas_oracle, try_execution(_, _, _))
             .Times(16)
             .WillOnce(Return(expect_result_fail))
@@ -137,7 +137,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call empty, alternatively fails and succeeds") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         EXPECT_CALL(estimate_gas_oracle, try_execution(_, _, _))
             .Times(14)
             .WillOnce(Return(expect_result_fail))
@@ -162,7 +162,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call empty, alternatively succeeds and fails") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         EXPECT_CALL(estimate_gas_oracle, try_execution(_, _, _))
             .Times(14)
             .WillOnce(Return(expect_result_ok))
@@ -188,7 +188,7 @@ TEST_CASE("estimate gas") {
     SECTION("Call with gas, always fails but succes last step") {
         call.gas = kTxGas * 4;
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         EXPECT_CALL(estimate_gas_oracle, try_execution(_, _, _))
             .Times(17)
             .WillOnce(Return(expect_result_fail))
@@ -228,7 +228,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call with gas_price, gas not capped") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         call.gas = kTxGas * 2;
         call.gas_price = intx::uint256{10'000};
 
@@ -258,7 +258,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call with gas_price, gas capped") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         call.gas = kTxGas * 2;
         call.gas_price = intx::uint256{40'000};
 
@@ -285,7 +285,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call with gas_price and value, gas not capped") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         call.gas = kTxGas * 2;
         call.gas_price = intx::uint256{10'000};
         call.value = intx::uint256{500'000'000};
@@ -316,7 +316,7 @@ TEST_CASE("estimate gas") {
 
     SECTION("Call with gas_price and value, gas capped") {
         ExecutionResult expect_result_ok{.error_code = evmc_status_code::EVMC_SUCCESS, .pre_check_error = std::nullopt};
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         call.gas = kTxGas * 2;
         call.gas_price = intx::uint256{20'000};
         call.value = intx::uint256{500'000'000};
@@ -364,7 +364,7 @@ TEST_CASE("estimate gas") {
     }
 
     SECTION("Call with too high value, exception") {
-        ExecutionResult expect_result_fail{.error_code = pre_check_error, .pre_check_error = "intrisic gas"};
+        ExecutionResult expect_result_fail{.error_code = std::nullopt, .pre_check_error = "intrisic gas"};
         call.value = intx::uint256{2'000'000'000};
 
         try {

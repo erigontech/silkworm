3eeddb3a6f47cfb9afd5287362b57e88675f8261
canepat <16927169+canepat@users.noreply.github.com>
2023-07-24 11:44:33 +0200

    cmd: fix symbol loading using boost DLL api: change library type to SHARED

--- cmd/api/execute.cpp
+++ cmd/api/execute.cpp
@@ -24,19 +24,12 @@
 
 #include <silkworm/api/silkworm_api.h>
 
-const char* kSilkwormApiPath = "../../silkworm/api/libsilkworm_api.so";
+const char* kSilkwormApiLibPath = "../../silkworm/api/libsilkworm_api.dylib";
 const char* kSilkwormExecuteBlocksSymbol = "silkworm_execute_blocks";
 
-//! Function pointer for silkworm_execute_blocks C API
-using SilkwormExecuteBlocksFunc =
-    SilkwormStatusCode (*)(MDBX_txn* txn,
-                           uint64_t chain_id,
-                           uint64_t start_block,
-                           uint64_t max_block,
-                           uint64_t batch_size,
-                           bool write_receipts,
-                           uint64_t* last_executed_block,
-                           int* mdbx_error_code);
+//! Function signature for silkworm_execute_blocks C API
+using SilkwormExecuteBlocksSig =
+    SilkwormStatusCode(MDBX_txn*,uint64_t,uint64_t,uint64_t,uint64_t,bool,uint64_t*,int*);
 
 int main(int /*argc*/, char* /*argv*/[]) {
     CLI::App app{"Execute blocks"};
@@ -46,15 +39,14 @@ int main(int /*argc*/, char* /*argv*/[]) {
         std::cout << "Execute blocks starting [pid=" << std::to_string(pid) << "]\n";
         // parse_command_line(argc, argv, app, settings);
 
-        boost::dll::shared_library silkworm_api_lib{kSilkwormApiPath};
-
-        const SilkwormExecuteBlocksFunc silkworm_execute_blocks{
-            silkworm_api_lib.get<SilkwormExecuteBlocksFunc>(kSilkwormExecuteBlocksSymbol)};
+        // Import the silkworm_execute_blocks symbol from silkworm API library
+        const auto silkworm_execute_blocks{
+            boost::dll::import_symbol<SilkwormExecuteBlocksSig>(kSilkwormApiLibPath, kSilkwormExecuteBlocksSymbol)};
 
         uint64_t last_executed_block{std::numeric_limits<uint64_t>::max()};
         int mdbx_error_code{0};
         const auto status_code{
-            silkworm_execute_blocks(NULL, 1, 0, 0, 1, false, &last_executed_block, &mdbx_error_code)};
+            silkworm_execute_blocks(nullptr, 1, 0, 0, 1, false, &last_executed_block, &mdbx_error_code)};
         std::cout << "Execute blocks status code: " << std::to_string(status_code) << "\n";
 
         std::cout << "Execute blocks exiting [pid=" << std::to_string(pid) << "]\n";

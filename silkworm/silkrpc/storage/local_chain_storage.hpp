63d05def8581ebf06bca8be603e64fdeda4f7605
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-07-29 16:10:45 +0200

    change signature

--- silkworm/silkrpc/storage/local_chain_storage.hpp
+++ silkworm/silkrpc/storage/local_chain_storage.hpp
@@ -69,7 +69,7 @@ class LocalChainStorage : public ChainStorage {
 
     [[nodiscard]] Task<std::optional<intx::uint256>> read_total_difficulty(const Hash& block_hash, BlockNum block_number) const override;
 
-    Task<uint64_t> read_block_number_by_transaction_hash(const evmc::bytes32& transaction_hash) const override;
+    Task<bool> read_block_number_by_transaction_hash(const evmc::bytes32& transaction_hash, uint64_t& bn) const override;
 
   private:
     db::DataModel data_model_;

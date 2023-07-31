4dccb55f65cfae9318316cb248b13a9f7c07d053
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-07-31 23:12:01 +0200

    temporary modified

--- silkworm/core/common/util_test.cpp
+++ silkworm/core/common/util_test.cpp
@@ -39,8 +39,7 @@ TEST_CASE("Hex") {
     parsed_bytes = from_hex("0x0");
     CHECK((parsed_bytes.has_value() == true && parsed_bytes.value() == expected_bytes));
 
-    expected_bytes = {0xFF, 0xFF};
-    expected_bytes = {0x0a};
+    expected_bytes = 0x0a;
     parsed_bytes = from_hex("0xa");
     CHECK((parsed_bytes.has_value() == true && parsed_bytes.value() == expected_bytes));
 

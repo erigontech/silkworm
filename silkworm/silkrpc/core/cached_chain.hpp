c7f56398a9e038dd391bbbd1752306cb2c23c6d8
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-08-04 14:45:23 +0200

    modify return type read_block_by_transaction_hash

--- silkworm/silkrpc/core/cached_chain.hpp
+++ silkworm/silkrpc/core/cached_chain.hpp
@@ -34,7 +34,7 @@ using boost::asio::awaitable;
 awaitable<std::shared_ptr<BlockWithHash>> read_block_by_number(BlockCache& cache, const ChainStorage& storage, BlockNum block_number);
 awaitable<std::shared_ptr<BlockWithHash>> read_block_by_hash(BlockCache& cache, const ChainStorage& storage, const evmc::bytes32& block_hash);
 awaitable<std::shared_ptr<BlockWithHash>> read_block_by_number_or_hash(BlockCache& cache, const ChainStorage& storage, const rawdb::DatabaseReader& reader, const BlockNumberOrHash& bnoh);
-awaitable<BlockWithHash> read_block_by_transaction_hash(BlockCache& cache, const ChainStorage& storage, const evmc::bytes32& transaction_hash);
+awaitable<std::shared_ptr<BlockWithHash>> read_block_by_transaction_hash(BlockCache& cache, const ChainStorage& storage, const evmc::bytes32& transaction_hash);
 awaitable<std::optional<TransactionWithBlock>> read_transaction_by_hash(BlockCache& cache, const ChainStorage& storage, const evmc::bytes32& transaction_hash);
 
 }  // namespace silkworm::rpc::core

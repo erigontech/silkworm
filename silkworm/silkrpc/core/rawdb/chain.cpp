2fc5be0ff4cec88158eb52c516f5d4e1d006f139
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-08-17 20:36:01 +0200

    fix after review

--- silkworm/silkrpc/core/rawdb/chain.cpp
+++ silkworm/silkrpc/core/rawdb/chain.cpp
@@ -37,8 +37,8 @@
 namespace silkworm::rpc::core::rawdb {
 
 /* Local Routines */
-static boost::asio::awaitable<silkworm::BlockHeader> read_header_by_hash(const DatabaseReader& reader, const evmc::bytes32& block_hash);
-static boost::asio::awaitable<silkworm::BlockHeader> read_header(const DatabaseReader& reader, const evmc::bytes32& block_hash, uint64_t block_number);
+static Task<silkworm::BlockHeader> read_header_by_hash(const DatabaseReader& reader, const evmc::bytes32& block_hash);
+static Task<silkworm::BlockHeader> read_header(const DatabaseReader& reader, const evmc::bytes32& block_hash, uint64_t block_number);
 
 Task<uint64_t> read_header_number(const DatabaseReader& reader, const evmc::bytes32& block_hash) {
     const silkworm::ByteView block_hash_bytes{block_hash.bytes, silkworm::kHashLength};

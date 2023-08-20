cf17d39073667aaa87a488e0979f2269f523b96f
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-08-20 21:37:23 +0200

    extract json CallBundle into separate files

--- silkworm/silkrpc/json/types.cpp
+++ silkworm/silkrpc/json/types.cpp
@@ -265,20 +265,6 @@ void to_json(nlohmann::json& json, const PeerInfo& info) {
     json["protocols"] = nullptr;
 }
 
-void to_json(nlohmann::json& json, const struct CallBundleTxInfo& tx_info) {
-    json["gasUsed"] = tx_info.gas_used;
-    json["txHash"] = silkworm::to_bytes32({tx_info.hash.bytes, silkworm::kHashLength});
-    if (!tx_info.error_message.empty())
-        json["error"] = tx_info.error_message;
-    else
-        json["value"] = silkworm::to_bytes32({tx_info.value.bytes, silkworm::kHashLength});
-}
-
-void to_json(nlohmann::json& json, const struct CallBundleInfo& bundle_info) {
-    json["bundleHash"] = silkworm::to_bytes32({bundle_info.bundle_hash.bytes, silkworm::kHashLength});
-    json["results"] = bundle_info.txs_info;
-}
-
 void to_json(nlohmann::json& json, const AccessListResult& access_list_result) {
     json["accessList"] = access_list_result.access_list;
     if (access_list_result.error) {

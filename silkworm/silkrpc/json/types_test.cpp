67316cc19f55d1c7b1b58e6f075907a18d771bc5
lupin012 <58134934+lupin012@users.noreply.github.com>
2023-08-21 09:02:49 +0200

    Rpcdaemon: Extract CallBundle to_json() into separate file  (#1457)
    * extract json CallBundle into separate files
    
    * extract json CallBundle into separate files

--- silkworm/silkrpc/json/types_test.cpp
+++ silkworm/silkrpc/json/types_test.cpp
@@ -494,46 +494,6 @@ TEST_CASE("serialize block body with ommers", "[silkrpc][to_json]") {
     })"_json);
 }
 
-TEST_CASE("serialize empty call_bundle", "[silkrpc][to_json]") {
-    struct CallBundleInfo bundle_info {};
-
-    nlohmann::json j = bundle_info;
-    CHECK(j == R"({
-        "bundleHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
-        "results":[]
-    })"_json);
-}
-
-TEST_CASE("serialize call_bundle with error", "[silkrpc][to_json]") {
-    struct CallBundleInfo bundle_info {};
-    struct CallBundleTxInfo tx_info {};
-    tx_info.gas_used = 0x234;
-    tx_info.error_message = "operation reverted";
-    bundle_info.txs_info.push_back(tx_info);
-
-    nlohmann::json j = bundle_info;
-    CHECK(j == R"({
-        "bundleHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
-        "results":[{"error": "operation reverted", "gasUsed": 564,
-                    "txHash": "0x0000000000000000000000000000000000000000000000000000000000000000"}]
-    })"_json);
-}
-
-TEST_CASE("serialize call_bundle with value", "[silkrpc][to_json]") {
-    struct CallBundleInfo bundle_info {};
-    struct CallBundleTxInfo tx_info {};
-    tx_info.gas_used = 0x234;
-    tx_info.value = 0x1230000000000000000000000000000000000000000000000000000000000321_bytes32;
-    bundle_info.txs_info.push_back(tx_info);
-
-    nlohmann::json j = bundle_info;
-    CHECK(j == R"({
-        "bundleHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
-        "results":[{"value": "0x1230000000000000000000000000000000000000000000000000000000000321", "gasUsed": 564,
-                    "txHash": "0x0000000000000000000000000000000000000000000000000000000000000000"}]
-    })"_json);
-}
-
 TEST_CASE("serialize filled SyncingData", "[silkrpc][to_json]") {
     SyncingData syncing_data{};
     StageData stage_data;

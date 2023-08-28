7ab39951d716706d7b0ddf9cff8c8bd57735ec2a
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-08-28 19:35:20 +0200

    Using conanfile.py; turning mimalloc override to false on windows

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -45,7 +45,7 @@ set(CONAN_CMAKE_SILENT_OUTPUT ON)
 include("${CMAKE_SOURCE_DIR}/third_party/cmake-conan/conan.cmake")
 
 # provide a static conanfile.txt instead of generating it with conan_cmake_configure()
-file(COPY "${CMAKE_SOURCE_DIR}/conanfile.txt" DESTINATION "${CONAN_BINARY_DIR}")
+file(COPY "${CMAKE_SOURCE_DIR}/conanfile.py" DESTINATION "${CONAN_BINARY_DIR}")
 
 if(NOT DEFINED CONAN_PROFILE)
   guess_conan_profile()

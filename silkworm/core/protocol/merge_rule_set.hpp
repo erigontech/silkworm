ecde07eeca2ddfa567fcc4db651eb4a1a07ae1a0
GitHub <noreply@github.com>
2023-09-04 17:38:45 +0000

    make fmt

--- silkworm/core/protocol/merge_rule_set.hpp
+++ silkworm/core/protocol/merge_rule_set.hpp
@@ -41,7 +41,7 @@ class MergeRuleSet : public BaseRuleSet {
 
     evmc::address get_beneficiary(const BlockHeader& header) override;
 
-     BlockReward compute_reward(const Block& block) override;
+    BlockReward compute_reward(const Block& block) override;
 
   protected:
     intx::uint256 difficulty(const BlockHeader& header, const BlockHeader& parent) override;

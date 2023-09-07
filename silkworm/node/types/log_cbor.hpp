703469f892c5be123fcf040fbb240e8c297005d7
canepat <16927169+canepat@users.noreply.github.com>
2023-09-07 22:41:29 +0200

    node: skip non-address and non-topic data in LogIndex stage (#1502)

--- silkworm/node/types/log_cbor.hpp
+++ silkworm/node/types/log_cbor.hpp
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <silkworm/core/common/base.hpp>
+#include <silkworm/core/types/hash.hpp>
 #include <silkworm/core/types/log.hpp>
 
 namespace silkworm {
@@ -23,5 +25,20 @@ namespace silkworm {
 // Erigon-compatible CBOR encoding for storage.
 // See core/types/log.go
 Bytes cbor_encode(const std::vector<Log>& v);
+
+//! LogCborConsumer is the interface to implement for parsing a CBOR-encoded sequence of Logs
+struct LogCborConsumer {
+    virtual ~LogCborConsumer() = default;
+
+    virtual void on_num_logs(std::size_t num_logs) = 0;
+    virtual void on_address(std::span<const uint8_t, kAddressLength> address_bytes) = 0;
+    virtual void on_num_topics(std::size_t num_topics) = 0;
+    virtual void on_topic(HashAsSpan topic_bytes) = 0;
+    virtual void on_data(std::span<const uint8_t> data_bytes) = 0;
+};
+
+void cbor_decode(ByteView data, LogCborConsumer& consumer);
+
+[[nodiscard]] bool cbor_decode(ByteView data, std::vector<Log>& logs);
 
 }  // namespace silkworm

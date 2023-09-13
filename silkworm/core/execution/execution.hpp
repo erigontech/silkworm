f8a2eb6e5a3bcb79546d5cde88dbfe70c3b908c9
canepat <16927169+canepat@users.noreply.github.com>
2023-09-14 00:58:29 +0200

    api: implement block execution flags for state changes and call traces (#1519)
    (cherry picked from commit 12f063542c62dcdb3bbb16cfc4e30ffba06eaaff)

--- silkworm/core/execution/execution.hpp
+++ silkworm/core/execution/execution.hpp
@@ -19,41 +19,15 @@
 #include <vector>
 
 #include <silkworm/core/chain/config.hpp>
+#include <silkworm/core/execution/call_tracer.hpp>
 #include <silkworm/core/execution/processor.hpp>
 #include <silkworm/core/protocol/rule_set.hpp>
 #include <silkworm/core/state/state.hpp>
 #include <silkworm/core/types/block.hpp>
+#include <silkworm/core/types/call_traces.hpp>
 #include <silkworm/core/types/receipt.hpp>
 
 namespace silkworm {
-
-/**
- * @brief Execute a given block, write resulting changes into the state and return the transaction receipts.
- * @attention This \code execute_block overload requires both EVM state_pool and analysis_cache to better performance
- * @precondition validate_block_header & pre_validate_block_body must return kOk; transaction senders must be already populated.
- * @warning This method does not verify state root; pre-Byzantium receipt root isn't validated either.
- * @param block The block to execute.
- * @param analysis_cache The cache containing the EVM code analysis indexed by code hash.
- * @param state_pool The pool of EVM execution states to use.
- * @param state The chain state at the beginning of the block.
- * @param chain_config The configuration parameters for the chain.
- * @param receipts The transaction receipts produced by block execution.
- */
-[[nodiscard]] inline ValidationResult execute_block(const Block& block,
-                                                    AnalysisCache& analysis_cache,
-                                                    ObjectPool<evmone::ExecutionState>& state_pool,
-                                                    State& state,
-                                                    const ChainConfig& chain_config,
-                                                    std::vector<Receipt>& receipts) noexcept {
-    const auto rule_set{protocol::rule_set_factory(chain_config)};
-    if (!rule_set) {
-        return ValidationResult::kUnknownProtocolRuleSet;
-    }
-    ExecutionProcessor processor{block, *rule_set, state, chain_config};
-    processor.evm().analysis_cache = &analysis_cache;
-    processor.evm().state_pool = &state_pool;
-    return processor.execute_and_write_block(receipts);
-}
 
 /**
  * @brief Execute a given block, write resulting changes into the state and return the transaction receipts.

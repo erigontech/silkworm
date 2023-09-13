91529aab76b1858fdfff2ba18847bb6143bb7dd0
battlmonstr <battlmonstr@users.noreply.github.com>
2023-09-13 21:25:56 +0200

    infra: timeout debugging macro

--- silkworm/infra/concurrency/timeout.hpp
+++ silkworm/infra/concurrency/timeout.hpp
@@ -23,7 +23,10 @@
 
 namespace silkworm::concurrency {
 
-Task<void> timeout(std::chrono::milliseconds duration);
+Task<void> timeout(
+    std::chrono::milliseconds duration,
+    const char* source_file_path = nullptr,
+    int source_file_line = 0);
 
 class TimeoutExpiredError : public std::runtime_error {
   public:
@@ -31,3 +34,5 @@ class TimeoutExpiredError : public std::runtime_error {
 };
 
 }  // namespace silkworm::concurrency
+
+#define SILK_CONCURRENCY_TIMEOUT(duration) ::silkworm::concurrency::timeout(duration, __FILE__, __LINE__)

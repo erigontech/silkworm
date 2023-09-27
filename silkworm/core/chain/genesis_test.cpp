b96e45c451e3a2a76bfc7e3ffb2e781dc3d13c5e
yperbasis <andrey.ashikhmin@gmail.com>
2023-09-27 11:45:34 +0200

    try removing const

--- silkworm/core/chain/genesis_test.cpp
+++ silkworm/core/chain/genesis_test.cpp
@@ -37,14 +37,14 @@ void test_genesis_config(const ChainConfig& x) {
 }
 
 TEST_CASE("unknown genesis 0") {
-    const nlohmann::json genesis_json{nlohmann::json::parse("{", nullptr, /* allow_exceptions = */ false)};
+    nlohmann::json genesis_json{nlohmann::json::parse("{", nullptr, /* allow_exceptions = */ false)};
     CHECK(genesis_json.is_discarded());
 }
 
 TEST_CASE("unknown genesis") {
     const std::string_view genesis_data{read_genesis_data(1'000u)};
     CHECK(genesis_data == "{");
-    const nlohmann::json genesis_json{nlohmann::json::parse(genesis_data, nullptr, /* allow_exceptions = */ false)};
+    nlohmann::json genesis_json{nlohmann::json::parse(genesis_data, nullptr, /* allow_exceptions = */ false)};
     CHECK(genesis_json.is_discarded());
 }
 

9a5daaf4cf3779409ad67ed5672eb19513946225
canepat <16927169+canepat@users.noreply.github.com>
2023-10-18 17:56:11 +0200

    cmd: fix BitTorrent command-line options node: add BitTorrent client flag to emit warnings for error alerts node: cleanup BitTorrent client settings

--- silkworm/node/bittorrent/settings.hpp
+++ silkworm/node/bittorrent/settings.hpp
@@ -25,18 +25,6 @@ namespace silkworm {
 //! The settings for handling BitTorrent protocol
 struct BitTorrentSettings {
     inline const static std::filesystem::path kDefaultTorrentRepoPath{".torrent"};
-    constexpr static std::chrono::seconds kDefaultWaitBetweenAlertPolls{1};
-    constexpr static int kDefaultNumberOfPollsBetweenStats{30};
-    constexpr static std::chrono::seconds kDefaultResumeDataSaveInterval{60};
-    constexpr static bool kDefaultVerifyOnStartup{false};
-    constexpr static bool kDefaultSeeding{false};
-
-    constexpr static int kDefaultDownloadRateLimit{64 * 1024 * 1024};  // 64MiB
-    constexpr static int kDefaultUploadRateLimit{4 * 1024 * 1024};     // 4MiB
-    constexpr static int kDefaultActiveDownloads{6};
-    constexpr static int kDefaultMaxOutRequestQueue{6000};
-    constexpr static bool kDefaultAnnounceToAllTiers{true};
-    constexpr static int kDefaultAsyncIOThreads{32};
 
     /* BitTorrentClient configuration settings */
     //! Directory path where torrent files will be stored
@@ -45,28 +33,31 @@ struct BitTorrentSettings {
     //! Path for magnet links
     std::optional<std::string> magnets_file_path;
 
-    //! Time interval between two alert polling loopsErigon forks 242
-    std::chrono::seconds wait_between_alert_polls{kDefaultWaitBetweenAlertPolls};
+    //! Time interval between two alert polling loops
+    std::chrono::seconds wait_between_alert_polls{1};
 
     //! The number of alert polls between two contiguous stats requests
-    int number_of_polls_between_stats{kDefaultNumberOfPollsBetweenStats};
+    int number_of_polls_between_stats{30};
 
     //! Time interval between two resume data savings
-    std::chrono::seconds resume_data_save_interval{kDefaultResumeDataSaveInterval};
+    std::chrono::seconds resume_data_save_interval{60};
 
     //! Flag indicating if snapshots will be verified on startup
-    bool verify_on_startup{kDefaultVerifyOnStartup};
+    bool verify_on_startup{false};
 
     //! Flag indicating if the client should seed torrents when done or not
-    bool seeding{kDefaultSeeding};
+    bool seeding{false};
+
+    //! Flag indicating if BitTorrent failure/error alerts should be treated as warnings
+    bool warn_on_error_alerts{false};
 
     /* BitTorrent protocol settings */
-    int download_rate_limit{kDefaultDownloadRateLimit};
-    int upload_rate_limit{kDefaultUploadRateLimit};
-    int active_downloads{kDefaultActiveDownloads};
-    int max_out_request_queue{kDefaultMaxOutRequestQueue};
-    bool announce_to_all_tiers{kDefaultAnnounceToAllTiers};
-    int aio_threads{kDefaultAsyncIOThreads};
+    int download_rate_limit{64};  // 64MiB
+    int upload_rate_limit{4};     // 4MiB
+    int active_downloads{6};
+    int max_out_request_queue{6000};
+    bool announce_to_all_tiers{true};
+    int aio_threads{32};
 };
 
 }  // namespace silkworm

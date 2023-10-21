93aa16e4f9d4b3b8a4a60cc448bb0aa33a459600
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-10-21 15:02:42 +0200

    avoid UB on golomb-rice's max_index on multithreading index build

--- silkworm/node/recsplit/rec_split.hpp
+++ silkworm/node/recsplit/rec_split.hpp
@@ -650,9 +650,13 @@ class RecSplit {
 
     static inline std::size_t skip_nodes(std::size_t m) { return (memo[m] >> 16) & 0x7FF; }
 
-    static constexpr uint64_t golomb_param(const std::size_t m, const std::array<uint32_t, kMaxBucketSize>& memo) {
-        if (m > golomb_param_max_index_) golomb_param_max_index_ = m;
+    inline uint64_t golomb_param(const std::size_t m, const std::array<uint32_t, kMaxBucketSize>& memo) const {
         return memo[m] >> 27;
+    }
+
+    inline uint64_t golomb_param_with_max_updating(const std::size_t m, const std::array<uint32_t, kMaxBucketSize>& memo) {
+        if (m > golomb_param_max_index_) golomb_param_max_index_ = m;
+        return golomb_param(m, memo);
     }
 
     // Generates the precomputed table of 32-bit values holding the Golomb-Rice code
@@ -805,7 +809,7 @@ class RecSplit {
                 }
             }
             salt -= kStartSeed[level];
-            const auto log2golomb = golomb_param(m, memo);
+            const auto log2golomb = golomb_param_with_max_updating(m, memo);
             gr_builder_.append_fixed(salt, log2golomb);
             unary.push_back(static_cast<uint32_t>(salt >> log2golomb));
         } else {
@@ -840,7 +844,7 @@ class RecSplit {
             std::copy(buffer_offsets_.data(), buffer_offsets_.data() + m, offsets.data() + start);
 
             salt -= kStartSeed[level];
-            const auto log2golomb = golomb_param(m, memo);
+            const auto log2golomb = golomb_param_with_max_updating(m, memo);
             gr_builder_.append_fixed(salt, log2golomb);
             unary.push_back(static_cast<uint32_t>(salt >> log2golomb));
 
@@ -907,7 +911,7 @@ class RecSplit {
     static const std::size_t kUpperAggregationBound;
 
     //! The max index used in Golomb parameter array
-    static inline uint16_t golomb_param_max_index_{0};
+    uint16_t golomb_param_max_index_{0};
 
     //! For each bucket size, the Golomb-Rice parameter (upper 8 bits) and the number of bits to
     //! skip in the fixed part of the tree (lower 24 bits).

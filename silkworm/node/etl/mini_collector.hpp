6d5ee79b2b1c9e8244e850514b9fec12ffecde0b
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2023-11-13 19:48:14 +0100

    Add implementation of the mini-collector

---
+++ silkworm/node/etl/mini_collector.hpp
@@ -0,0 +1,154 @@
+/*
+Copyright 2023 The Silkworm Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <mutex>
+
+#include <silkworm/infra/concurrency/signal_handler.hpp>
+#include <silkworm/node/common/settings.hpp>
+#include <silkworm/node/db/mdbx.hpp>
+#include <silkworm/node/etl/util.hpp>
+
+/*
+ * This is a memory-only reduced version of ETL Collector, with compatible interface
+ * It can be used to prototype code that will use the full ETL collector or to do performance comparisons
+ * between a memory-only impl. and a file-based impl.
+ */
+namespace silkworm::etl {
+
+// Function pointer to process Load on before Load data into tables
+using MiniLoadFunc = std::function<void(const Bytes& key, const Bytes& value,
+                                        db::RWCursorDupSort&, MDBX_put_flags_t)>;
+
+inline const Bytes& key(const std::pair<Bytes, Bytes>& entry) { return entry.first; }
+inline const Bytes& value(const std::pair<Bytes, Bytes>& entry) { return entry.second; }
+
+// Collects data Extracted from db
+class MiniCollector {
+  public:
+    // Not copyable nor movable
+    MiniCollector(const MiniCollector&) = delete;
+    MiniCollector& operator=(const MiniCollector&) = delete;
+
+    explicit MiniCollector() = default;
+
+    explicit MiniCollector([[maybe_unused]] const NodeSettings* node_settings) {
+        // entries_.reserve(node_settings->etl_buffer_size)
+    }
+    explicit MiniCollector(const std::filesystem::path&, [[maybe_unused]] size_t optimal_size) {
+        // entries_.reserve(optimal_size)
+    }
+    explicit MiniCollector([[maybe_unused]] size_t optimal_size) {
+        // entries_.reserve(optimal_size)
+    }
+
+    void collect(const Bytes& key, const Bytes& value) {
+        ++size_;
+        bytes_size_ += key.size() + value.size();
+        entries_.emplace(key, value);
+    }
+
+    void collect(Bytes&& key, Bytes&& value) {
+        ++size_;
+        bytes_size_ += key.size() + value.size();
+        entries_.emplace(std::move(key), std::move(value));
+    }
+
+    //! \brief Loads and optionally transforms collected entries into db
+    //! \param [in] target : a cursor opened on target table and owned by caller (can be empty)
+    //! \param [in] load_func : Pointer to function transforming collected entries. If NULL no transform is executed
+    //! \param [in] flags : Optional put flags for append or upsert (default)
+    //! items
+    void load(db::RWCursorDupSort& target, const MiniLoadFunc& load_func = {},
+              MDBX_put_flags_t flags = MDBX_put_flags_t::MDBX_UPSERT) {
+        using namespace std::chrono_literals;
+        [[maybe_unused]] static const auto kLogInterval{5s};               // Updates processing key (for log purposes) every this time
+        [[maybe_unused]] auto log_time{std::chrono::steady_clock::now()};  // To check if an update of key is needed
+
+        set_loading_key({});
+
+        if (empty()) return;
+
+        sort_entries();
+
+        for (const auto& entry : entries_) {
+            if (const auto now{std::chrono::steady_clock::now()}; log_time <= now) {
+                if (SignalHandler::signalled()) {
+                    throw std::runtime_error("Operation cancelled");
+                }
+                set_loading_key(entry.first);
+                log_time = now + kLogInterval;
+            }
+
+            if (load_func) {
+                load_func(entry.first, entry.second, target, flags);
+            } else {
+                mdbx::slice k{db::to_slice(key(entry))};
+                if (value(entry).empty()) {
+                    target.erase(k);
+                } else {
+                    mdbx::slice v{db::to_slice(value(entry))};
+                    mdbx::error::success_or_throw(target.put(k, &v, flags));
+                }
+            }
+        }
+
+        clear();
+    }
+
+    //! \brief Returns the number of actually collected items
+    [[nodiscard]] size_t size() const { return size_; }
+
+    //! \brief Returns the number of actually collected bytes
+    [[nodiscard]] size_t bytes_size() const { return bytes_size_; }
+
+    //! \brief Returns whether this instance is empty (i.e. no items)
+    [[nodiscard]] bool empty() const { return size_ == 0; }
+
+    //! \brief Clears contents of collector and reset
+    void clear() {
+        entries_.clear();
+        size_ = 0;
+        bytes_size_ = 0;
+    }
+
+    //! \brief Returns the hex representation of current load key (for progress tracking)
+    [[nodiscard]] std::string get_load_key() const {
+        std::unique_lock l{mutex_};
+        return loading_key_;
+    }
+
+  private:
+    size_t size_{0};        // Count of total collected items
+    size_t bytes_size_{0};  // Count of total collected bytes
+
+    std::map<Bytes,  // key
+             Bytes>  // value
+        entries_;
+
+    void sort_entries() {}  // does nothing, std::map is always sorted
+
+    // for progress tracking only
+    void set_loading_key(ByteView key) {
+        std::unique_lock l{mutex_};
+        loading_key_ = to_hex(key, true);
+    }
+    mutable std::mutex mutex_{};  // To sync loading_key_
+    std::string loading_key_{};   // Actual load key (for log purposes)
+};
+
+}  // namespace silkworm::etl

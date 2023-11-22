cc2e7a001a6802b76af410a454f55e5ded3459fe
yperbasis <andrey.ashikhmin@gmail.com>
2023-11-22 21:33:24 +0100

    node: constexpr snapshot configs

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -42,7 +42,5 @@ list(FILTER SRC EXCLUDE REGEX "silkworm/interfaces/")
 list(FILTER SRC EXCLUDE REGEX "silkworm/core/chain/genesis_[a-z]+.cpp\$")
 list(FILTER SRC EXCLUDE REGEX "silkworm/core/chain/dao.hpp$")
 list(FILTER SRC EXCLUDE REGEX "silkworm/node/common/preverified_hashes_[a-z]+.cpp\$")
-list(FILTER SRC EXCLUDE REGEX "silkworm/node/snapshot/config/[a-z_]+.cpp\$")
-list(FILTER SRC EXCLUDE REGEX "silkworm/node/snapshot/toml.hpp$$")
 
 execute_process(COMMAND ${CLANG_FORMAT} -style=file -i ${SRC})
---
+++ silkworm/node/snapshot/config/goerli.hpp
@@ -0,0 +1,65 @@
+/* Generated from goerli.toml using Silkworm embed_toml */
+
+#pragma once
+
+#include <array>
+
+#include <silkworm/node/snapshot/entry.hpp>
+
+namespace silkworm::snapshot {
+
+inline constexpr std::array<Entry, 51> kGoerliSnapshots{
+    Entry{"v1-000000-000500-bodies.seg"sv, "4b060b529c2fc07293740c6194f9e6b97754392d"sv},
+    Entry{"v1-000000-000500-headers.seg"sv, "43e2e49d76d4052e72583ea142fa5f405bd011bb"sv},
+    Entry{"v1-000000-000500-transactions.seg"sv, "86bbaa339e87391aab1bb605c451db8361f50a3a"sv},
+    Entry{"v1-000500-001000-bodies.seg"sv, "4f236ccf757f1398a6fdc8675806edb07b040809"sv},
+    Entry{"v1-000500-001000-headers.seg"sv, "f0495df390346a4864b35376dc740766bfa372d6"sv},
+    Entry{"v1-000500-001000-transactions.seg"sv, "8baafd535e7a50329b61cb0486ead7f189baa288"sv},
+    Entry{"v1-001000-001500-bodies.seg"sv, "47a7c9918eedccffe1b85d9f04e18dca0d9a8031"sv},
+    Entry{"v1-001000-001500-headers.seg"sv, "301f9ec6a1b17f4ad6c578c60c3fdd58c075177a"sv},
+    Entry{"v1-001000-001500-transactions.seg"sv, "a69df4a1560ca984f20d8ec38f16f9a5b90a68f6"sv},
+    Entry{"v1-001500-002000-bodies.seg"sv, "a9c1ae4f3fef416819b5b4d156b610d5bc29f15f"sv},
+    Entry{"v1-001500-002000-headers.seg"sv, "183abb6620c6363ec42b17d942cb0f51877419ea"sv},
+    Entry{"v1-001500-002000-transactions.seg"sv, "f144951734fecca8ed232d897974d83d90e17085"sv},
+    Entry{"v1-002000-002500-bodies.seg"sv, "7bfde5065a79a0cc843e8f4e2993e8a2bf066f77"sv},
+    Entry{"v1-002000-002500-headers.seg"sv, "edc38f3fdf092c8a532ee6fcb1a46978f567565e"sv},
+    Entry{"v1-002000-002500-transactions.seg"sv, "cf70b5d7fee5be88c9eeec09582ab29ff5467040"sv},
+    Entry{"v1-002500-003000-bodies.seg"sv, "e2e8a8b6dc5ef6b86d198bcee76ebd3cd8a73c47"sv},
+    Entry{"v1-002500-003000-headers.seg"sv, "db195984c6a33c5a38475a5fab1bb4b6cb2c11f4"sv},
+    Entry{"v1-002500-003000-transactions.seg"sv, "a92c63f05adb68eeb6956bc3cbc0fa1dedfb1748"sv},
+    Entry{"v1-003000-003500-bodies.seg"sv, "499798b2a67f91a31a8aa73a2da81ce5ef88fe37"sv},
+    Entry{"v1-003000-003500-headers.seg"sv, "415966ac96a160604835502a62b95361dcf5593a"sv},
+    Entry{"v1-003000-003500-transactions.seg"sv, "7cab4a79f1c4e273dd4e8daaba65863f67a04fde"sv},
+    Entry{"v1-003500-004000-bodies.seg"sv, "6666cdc238d2d915c1448f2cca2bd14d725a8c86"sv},
+    Entry{"v1-003500-004000-headers.seg"sv, "06df29f175cc5fb07a86c65d3888296307fc19f6"sv},
+    Entry{"v1-003500-004000-transactions.seg"sv, "a4e06dd230598972ac8de1c183b5caef064e7d75"sv},
+    Entry{"v1-004000-004500-bodies.seg"sv, "808b81038f05ccd8d2a86210167b96c89079ea0c"sv},
+    Entry{"v1-004000-004500-headers.seg"sv, "0c8b83e63e4f708ca1121c1868455435f74b0754"sv},
+    Entry{"v1-004000-004500-transactions.seg"sv, "dbdf63fdc71140816a6e998beac3123092c3aa10"sv},
+    Entry{"v1-004500-005000-bodies.seg"sv, "fe16ee31e73c02609d81f5229913c74c880d0860"sv},
+    Entry{"v1-004500-005000-headers.seg"sv, "cafca4eed33c635628f62e29c0bdfa5eb2722628"sv},
+    Entry{"v1-004500-005000-transactions.seg"sv, "eb7cf83f4a0182405b944b3e33e71e1182e66ce7"sv},
+    Entry{"v1-005000-005500-bodies.seg"sv, "4c23133d51b2b1fb7077e21fdfc70ead17dc29ab"sv},
+    Entry{"v1-005000-005500-headers.seg"sv, "fdd2e595fb8239d2d992d5b4f10b9864af7099a0"sv},
+    Entry{"v1-005000-005500-transactions.seg"sv, "59131e74b9bcc766a8e1e54c40e2dfde5a65883c"sv},
+    Entry{"v1-005500-006000-bodies.seg"sv, "1ee18411a71cf19ad87a04117c5e0fb6e0fc003d"sv},
+    Entry{"v1-005500-006000-headers.seg"sv, "26b71d05fb00ddb600fc485eafede63945cdbdb0"sv},
+    Entry{"v1-005500-006000-transactions.seg"sv, "ed9667972a39eea33b76ca4acbc9876421bf99c2"sv},
+    Entry{"v1-006000-006500-bodies.seg"sv, "7211c3469cbeef3026ef0d4d69cbd6742cd0bf77"sv},
+    Entry{"v1-006000-006500-headers.seg"sv, "d0566be1f2fca9fd37c6aab509ddc1e314755613"sv},
+    Entry{"v1-006000-006500-transactions.seg"sv, "662aa714c39468850f31d300b2f6f53602310367"sv},
+    Entry{"v1-006500-007000-bodies.seg"sv, "284d5e119dc41fdf30097aba5e451c905483fdb9"sv},
+    Entry{"v1-006500-007000-headers.seg"sv, "0b1320ee5575306a508388d7289d21bd0e58106f"sv},
+    Entry{"v1-006500-007000-transactions.seg"sv, "ec7e317ce7bc3f5d8361dc3fa4912b2820e30717"sv},
+    Entry{"v1-007000-007500-bodies.seg"sv, "559c8db03982d8883ecacdb4b3f92cc5445087a0"sv},
+    Entry{"v1-007000-007500-headers.seg"sv, "8ec2325839e6e1bcb9f9d76c806d0c024cfa7274"sv},
+    Entry{"v1-007000-007500-transactions.seg"sv, "f6e62ea8fe50d7db500d14efb1ad63a483771df3"sv},
+    Entry{"v1-007500-008000-bodies.seg"sv, "80351d546845fee4bf990d60b28ffec7f31b5af8"sv},
+    Entry{"v1-007500-008000-headers.seg"sv, "e8c8ba714f1856505b5b13f9a351a623eefc5588"sv},
+    Entry{"v1-007500-008000-transactions.seg"sv, "bdb149e5b5d908c3b076955ce98a925ba3b01bf1"sv},
+    Entry{"v1-008000-008500-bodies.seg"sv, "0dbdcb84fbe3ea457017a51aadfd0402b7b6eeea"sv},
+    Entry{"v1-008000-008500-headers.seg"sv, "80f25553b63f8ddc29eeed7564baa5289145769f"sv},
+    Entry{"v1-008000-008500-transactions.seg"sv, "ac98d05bf8674af6fc6a23dc28541970f7996d26"sv},
+};
+
+}

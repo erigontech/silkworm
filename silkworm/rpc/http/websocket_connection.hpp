c8142253a860645666e35ab2f4dd3e7185d799d5
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-01-07 13:44:07 +0100

    add websocket basic handle

---
+++ silkworm/rpc/http/websocket_connection.hpp
@@ -0,0 +1,84 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+//
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#pragma once
+
+#include <array>
+#include <string>
+
+#include <silkworm/infra/concurrency/task.hpp>
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/websocket.hpp>
+
+#include <silkworm/rpc/commands/rpc_api_table.hpp>
+#include <silkworm/rpc/common/constants.hpp>
+#include <silkworm/rpc/http/reply.hpp>
+#include <silkworm/rpc/http/request.hpp>
+#include <silkworm/rpc/http/request_handler.hpp>
+#include <silkworm/rpc/http/request_parser.hpp>
+
+namespace silkworm::rpc::http {
+
+//! Represents a single connection from a client via websocket.
+class WebSocketConnection : public std::enable_shared_from_this<WebSocketConnection> {
+  public:
+    WebSocketConnection(const WebSocketConnection&) = delete;
+    WebSocketConnection& operator=(const WebSocketConnection&) = delete;
+
+    //! Construct a connection running within the given execution context.
+    WebSocketConnection(boost::beast::websocket::stream<boost::beast::tcp_stream>&& ws,
+                        RequestHandler&& request_handler);
+
+    ~WebSocketConnection();
+
+    template <class Body, class Allocator>
+    Task<void> do_accept(boost::beast::http::request<Body, boost::beast::http::basic_fields<Allocator>> req);
+    Task<void> do_read();
+
+  private:
+    //! Reset connection data
+    void clean();
+
+    //! Perform an asynchronous write operation.
+    // Task<void> do_write();
+
+    boost::beast::websocket::stream<boost::beast::tcp_stream>& ws_;
+
+    //! The handler used to process the incoming request.
+    RequestHandler& request_handler_;
+
+    //! Buffer for incoming data.
+    boost::beast::flat_buffer buffer_;
+
+    //! The incoming request.
+    Request request_;
+
+    //! The reply to be sent back to the client.
+    Reply reply_;
+};
+
+}  // namespace silkworm::rpc::http

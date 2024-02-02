4e1329e877dbeb1030030e273bf2366e00d33bde
GitHub <noreply@github.com>
2024-02-02 13:59:16 +0000

    make fmt

--- silkworm/rpc/http/request_handler.cpp
+++ silkworm/rpc/http/request_handler.cpp
@@ -76,7 +76,7 @@ Task<void> RequestHandler::handle(const std::string& content) {
     SILK_TRACE << "handle HTTP request t=" << clock_time::since(start) << "ns";
 }
 
-bool RequestHandler::is_valid_jsonrpc(const nlohmann::json&  request_json ) {
+bool RequestHandler::is_valid_jsonrpc(const nlohmann::json& request_json) {
     auto validation_result = json_rpc_validator_.validate(request_json);
     return validation_result.is_valid;
 }

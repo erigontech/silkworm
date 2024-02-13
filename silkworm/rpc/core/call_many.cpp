d63de467a8b526d38fadc3f1689d04a3aefcf8ce
canepat <16927169+canepat@users.noreply.github.com>
2024-02-13 09:31:52 +0100

    move override state

--- silkworm/rpc/core/call_many.cpp
+++ silkworm/rpc/core/call_many.cpp
@@ -49,8 +49,7 @@ CallManyResult CallExecutor::executes_all_bundles(const silkworm::ChainConfig& c
     const auto& block = block_with_hash->block;
     const auto& block_transactions = block.transactions;
     auto state = transaction_.create_state(this_executor, tx_database, storage, block.header.number);
-    auto override_state = std::make_shared<state::OverrideState>(state::OverrideState{*state, accounts_overrides});
-    EVMExecutor executor{config, workers_, override_state};
+    EVMExecutor executor{config, workers_, std::make_shared<state::OverrideState>(*state, accounts_overrides)};
 
     std::uint64_t timeout = opt_timeout.value_or(5000);
     const auto start_time = clock_time::now();

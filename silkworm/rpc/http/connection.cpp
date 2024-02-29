34bab99aa54ee8b959ab6a932759b64f5c077608
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-02-29 22:26:25 +0100

    fix clang-tidy warning after PR 1858

--- silkworm/rpc/http/connection.cpp
+++ silkworm/rpc/http/connection.cpp
@@ -328,11 +328,9 @@ bool Connection::is_origin_allowed(const std::vector<std::string>& allowed_origi
         return true;
     }
 
-    for (const auto& curr_origin : allowed_origins) {
-        if (curr_origin == origin) {
-            return true;
-        }
-    }
+    if (std::ranges::any_of(allowed_origins.begin(), allowed_origins.end(), [origin](const std::string& curr_origin) {
+            return origin == curr_origin;
+        })) return true;
     return false;
 }
 

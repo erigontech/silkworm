18ffd0f6ac5e3e49c925975f401e2f8c53fe742d
Jacek Glen <jacek.glen@gmail.com>
2024-03-01 15:49:30 +0100

    capi: rework main execution loop

--- silkworm/capi/silkworm.cpp
+++ silkworm/capi/silkworm.cpp
@@ -67,7 +67,7 @@ struct ExecutionProgress {
     size_t processed_blocks{0};
     size_t processed_transactions{0};
     size_t processed_gas{0};
-    float gas_state_perc{0.0};
+    float batch_state_perc{0.0};
 };
 
 //! Kind of match to perform between Erigon and Silkworm libmdbx versions
@@ -161,6 +161,8 @@ static log::Args log_args_for_exec_progress(ExecutionProgress& progress, uint64_
     progress.processed_blocks = 0;
     progress.processed_transactions = 0;
     progress.processed_gas = 0;
+    std::stringstream batch_progress_perc;
+    batch_progress_perc << std::fixed << std::setprecision(2) << progress.batch_state_perc * 100 << "%";
     return {
         "number",
         std::to_string(current_block),
@@ -170,8 +172,8 @@ static log::Args log_args_for_exec_progress(ExecutionProgress& progress, uint64_
         float_to_string(speed_transactions),
         "Mgas/s",
         float_to_string(speed_mgas),
-        "gasState",
-        float_to_string(progress.gas_state_perc)};
+        "batchProgress",
+        batch_progress_perc.str()};
 }
 
 //! A signal handler guard using RAII pattern to acquire/release signal handling
@@ -497,107 +499,94 @@ int silkworm_execute_blocks(SilkwormHandle handle, MDBX_txn* mdbx_txn, uint64_t 
 
         const size_t max_batch_size{batch_size};
 
-        // Transform batch size limit into gas units (Ggas = Giga gas)
-        const size_t gas_max_batch_size{batch_size * 2_Kibi};  // 256MB -> 512Ggas roughly
-
         ExecutionProgress progress{.start_time = std::chrono::steady_clock::now()};
         auto signal_check_time{progress.start_time};
-        auto log_time{progress.start_time};
-
-        size_t gas_batch_size{0};
+        auto log_time{progress.start_time + 20s};
 
         std::optional<Block> block;
+        BlockNum block_number{start_block};
 
         const auto protocol_rule_set{protocol::rule_set_factory(*chain_config)};
         if (!protocol_rule_set) {
             return SILKWORM_UNKNOWN_CHAIN_ID;
         }
 
-        for (BlockNum block_number{start_block}; block_number <= max_block; ++block_number) {
-            block_buffer.pop_back(&block);
-            if (!block || !block.has_value()) {
-                block_buffer.terminate_and_release_all();
-                block_provider_thread.join();
-                return SILKWORM_BLOCK_NOT_FOUND;
-            }
-
-            ExecutionProcessor processor{*block, *protocol_rule_set, state_buffer, *chain_config};
-            processor.evm().analysis_cache = &analysis_cache;
-            processor.evm().state_pool = &state_pool;
-            CallTraces traces;
-            CallTracer tracer{traces};
-            if (write_call_traces) {
-                processor.evm().add_tracer(tracer);
-            }
-
-            std::vector<Receipt> receipts;
-            const auto result{processor.execute_and_write_block(receipts)};
-            if (result != ValidationResult::kOk) {
-                block_buffer.terminate_and_release_all();
-                block_provider_thread.join();
-                return SILKWORM_INVALID_BLOCK;
-            }
-
-            if (write_receipts) {
-                state_buffer.insert_receipts(block->header.number, receipts);
-            }
-            if (write_call_traces) {
-                state_buffer.insert_call_traces(block->header.number, traces);
-            }
-
-            if (last_executed_block) {
-                *last_executed_block = block->header.number;
-            }
-
-            ++progress.processed_blocks;
-            progress.processed_transactions += block->transactions.size();
-            progress.processed_gas += block->header.gas_used;
-            gas_batch_size += block->header.gas_used;
-
-            // Always flush history for single processed block (no batching)
-            state_buffer.write_history_to_db(write_change_sets);
-
-            // Flush state buffer if we've reached the target batch size
-            if (state_buffer.current_batch_state_size() >= max_batch_size) {
-                log::Info{"[4/12 Execution] Flushing state",  // NOLINT(*-unused-raii)
-                          log_args_for_exec_flush(state_buffer, max_batch_size, block->header.number)};
-                state_buffer.write_state_to_db();
-                db::stages::write_stage_progress(txn, db::stages::kExecutionKey, block->header.number);
-                gas_batch_size = 0;
-                StopWatch sw{/*auto_start=*/true};
-                txn.commit_and_renew();
-                const auto [elapsed, _]{sw.stop()};
-                log::Info("[4/12 Execution] Commit state+history",  // NOLINT(*-unused-raii)
-                          log_args_for_exec_commit(sw.since_start(elapsed), db_path));
-            }
-
-            const auto now{std::chrono::steady_clock::now()};
-            if (signal_check_time <= now) {
-                if (SignalHandler::signalled()) {
+        while (block_number <= max_block) {
+            while (block_number <= max_block && state_buffer.current_batch_state_size() < max_batch_size) {
+                block_buffer.pop_back(&block);
+                if (!block || !block.has_value()) {
                     block_buffer.terminate_and_release_all();
                     block_provider_thread.join();
-                    txn.abort();
-                    return SILKWORM_TERMINATION_SIGNAL;
+                    return SILKWORM_BLOCK_NOT_FOUND;
                 }
-                signal_check_time = now + 5s;
+
+                state_buffer.begin_block(block->header.number);
+                ExecutionProcessor processor{*block, *protocol_rule_set, state_buffer, *chain_config};
+                processor.evm().analysis_cache = &analysis_cache;
+                processor.evm().state_pool = &state_pool;
+                CallTraces traces;
+                CallTracer tracer{traces};
+                if (write_call_traces) {
+                    processor.evm().add_tracer(tracer);
+                }
+
+                std::vector<Receipt> receipts;
+                const auto result{processor.execute_and_write_block(receipts)};
+                if (result != ValidationResult::kOk) {
+                    block_buffer.terminate_and_release_all();
+                    block_provider_thread.join();
+                    return SILKWORM_INVALID_BLOCK;
+                }
+
+                if (write_receipts) {
+                    state_buffer.insert_receipts(block->header.number, receipts);
+                }
+                if (write_call_traces) {
+                    state_buffer.insert_call_traces(block->header.number, traces);
+                }
+
+                if (last_executed_block) {
+                    *last_executed_block = block->header.number;
+                }
+
+                ++progress.processed_blocks;
+                progress.processed_transactions += block->transactions.size();
+                progress.processed_gas += block->header.gas_used;
+
+                // Always flush history for single processed block (no batching)
+                state_buffer.write_history_to_db(write_change_sets);
+
+                const auto now{std::chrono::steady_clock::now()};
+                if (signal_check_time <= now) {
+                    if (SignalHandler::signalled()) {
+                        block_buffer.terminate_and_release_all();
+                        block_provider_thread.join();
+                        txn.abort();
+                        return SILKWORM_TERMINATION_SIGNAL;
+                    }
+                    signal_check_time = now + 5s;
+                }
+                if (log_time <= now) {
+                    progress.batch_state_perc = float(state_buffer.current_batch_state_size()) / float(max_batch_size);
+                    progress.end_time = now;
+                    log::Info{"[4/12 Execution] Executed blocks",  // NOLINT(*-unused-raii)
+                              log_args_for_exec_progress(progress, block->header.number)};
+                    log_time = now + 20s;
+                }
+                ++block_number;
             }
-            if (log_time <= now) {
-                progress.gas_state_perc = float(gas_batch_size) / float(gas_max_batch_size);
-                progress.end_time = now;
-                log::Info{"[4/12 Execution] Executed blocks",  // NOLINT(*-unused-raii)
-                          log_args_for_exec_progress(progress, block->header.number)};
-                log_time = now + 20s;
-            }
+
+            StopWatch sw{/*auto_start=*/true};
+            log::Info{"[4/12 Execution] Flushing state",  // NOLINT(*-unused-raii)
+                      log_args_for_exec_flush(state_buffer, max_batch_size, block->header.number)};
+            state_buffer.write_state_to_db();
+            db::stages::write_stage_progress(txn, db::stages::kExecutionKey, block->header.number);
+            txn.commit_and_renew();
+            const auto [elapsed, _]{sw.stop()};
+            log::Info{"[4/12 Execution] Commit state+history",  // NOLINT(*-unused-raii)
+                      log_args_for_exec_commit(sw.since_start(elapsed), db_path)};
         }
 
-        log::Info{"[4/12 Execution] Flushing state",  // NOLINT(*-unused-raii)
-                  log_args_for_exec_flush(state_buffer, max_batch_size, max_block)};
-        state_buffer.write_state_to_db();
-        StopWatch sw{/*auto_start=*/true};
-        txn.commit_and_stop();
-        const auto [elapsed, _]{sw.stop()};
-        log::Info("[4/12 Execution] Commit state+history",  // NOLINT(*-unused-raii)
-                  log_args_for_exec_commit(sw.since_start(elapsed), db_path));
         block_provider_thread.join();
         return SILKWORM_OK;
 

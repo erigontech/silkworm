113bb398a4cfd3703b1eb8fdf412e35bc83d7098
yperbasis <andrey.ashikhmin@gmail.com>
2024-03-26 14:58:21 +0100

    Remove Görli support

--- silkworm/core/protocol/rule_set_test.cpp
+++ silkworm/core/protocol/rule_set_test.cpp
@@ -32,8 +32,6 @@ TEST_CASE("Rule Set factory") {
     CHECK(rule_set);
     rule_set = rule_set_factory(test::kLondonConfig);  // No-proof rule set
     CHECK(rule_set);
-    rule_set = rule_set_factory(kGoerliConfig);  // Clique rule set
-    CHECK(rule_set);
     rule_set = rule_set_factory(ChainConfig{.rule_set_config = bor::Config{}});
     CHECK(rule_set);
     rule_set = rule_set_factory(ChainConfig{.rule_set_config = NoPreMergeConfig{}});

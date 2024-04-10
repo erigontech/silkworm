0d4d6a2ce508a6f7babddbd43c46eb2925e8ce38
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-04-10 22:29:57 +0200

    use non static method

--- silkworm/core/protocol/rule_set.cpp
+++ silkworm/core/protocol/rule_set.cpp
@@ -273,7 +273,7 @@ static RuleSetPtr pre_merge_rule_set(const ChainConfig& chain_config) {
 }
 
 RuleSetPtr rule_set_factory(const ChainConfig& chain_config) {
-    SILKWORM_ASSERT(ChainConfig::check_pre_merge_config(chain_config));
+    SILKWORM_ASSERT(chain_config.check_pre_merge_config());
 
     RuleSetPtr rule_set{pre_merge_rule_set(chain_config)};
     if (chain_config.terminal_total_difficulty) {

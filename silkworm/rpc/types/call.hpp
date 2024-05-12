4d3a3dfedf3ef11c7adc2085a9c7a857dac2547a
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-05-12 23:25:01 +0200

    fix after review

--- silkworm/rpc/types/call.hpp
+++ silkworm/rpc/types/call.hpp
@@ -49,12 +49,7 @@ struct Call {
                                                        const std::optional<AccessList>& override_access_list = std::nullopt,
                                                        const std::optional<uint64_t> override_nonce = std::nullopt) const {
         silkworm::Transaction txn{};
-        if (from) {
-            txn.set_sender(*from);
-        } else {
-            constexpr auto zero_address = 0x0000000000000000000000000000000000000000_address;
-            txn.set_sender(zero_address);
-        }
+        txn.set_sender(from ? *from : evmc::address{});
         txn.to = to;
         if (override_nonce) {
             txn.nonce = *override_nonce;

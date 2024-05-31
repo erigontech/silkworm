b4595f85a1c474c0b9836debf4fa63bc19a23a31
canepat <16927169+canepat@users.noreply.github.com>
2024-06-01 00:09:35 +0200

    print linker command find specific package version

--- cmake/toolchain/wasi.cmake
+++ cmake/toolchain/wasi.cmake
@@ -17,7 +17,7 @@
 set(CMAKE_C_COMPILER /opt/wasi-sdk/bin/clang)
 set(CMAKE_CXX_COMPILER /opt/wasi-sdk/bin/clang++)
 
-set(CMAKE_SKIP_RPATH TRUE)
+set(CMAKE_EXE_LINKER_FLAGS "-Xlinker -v -v")
 
 add_compile_definitions(CATCH_CONFIG_NO_POSIX_SIGNALS JSON_HAS_FILESYSTEM=0)
 

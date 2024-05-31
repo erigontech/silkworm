ba3457a61feca5af8f95554bc5f8066e252a47ce
canepat <16927169+canepat@users.noreply.github.com>
2024-05-31 08:00:55 +0200

    try to skip RPATH

--- cmake/toolchain/wasi.cmake
+++ cmake/toolchain/wasi.cmake
@@ -17,6 +17,8 @@
 set(CMAKE_C_COMPILER /opt/wasi-sdk/bin/clang)
 set(CMAKE_CXX_COMPILER /opt/wasi-sdk/bin/clang++)
 
+set(CMAKE_SKIP_RPATH TRUE)
+
 add_compile_definitions(CATCH_CONFIG_NO_POSIX_SIGNALS JSON_HAS_FILESYSTEM=0)
 
 include(${CMAKE_CURRENT_LIST_DIR}/cxx20.cmake)

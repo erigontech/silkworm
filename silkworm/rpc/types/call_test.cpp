3858c765ad08f3d0b617b94f2a7b5d77e6f0790e
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-06-12 18:53:23 +0200

    not permitted set gasPrice & max_fee || max_prio

--- silkworm/rpc/types/call_test.cpp
+++ silkworm/rpc/types/call_test.cpp
@@ -178,7 +178,7 @@ TEST_CASE("create call with no gas price and valid max_fee_per_gas and max_prior
         0x99f9b87991262f6ba471f09758cde1c0fc1de734_address,  // from
         0x5df9b87991262f6ba471f09758cde1c0fc1de734_address,  // to
         235,                                                 // gas
-        0,                                                   // gas_price
+        std::nullopt,                                        // gas_price
         10000,                                               // max_fee_per_gas
         10000,                                               // max_priority_fee_per_gas
         31337,                                               // value
@@ -187,8 +187,8 @@ TEST_CASE("create call with no gas price and valid max_fee_per_gas and max_prior
     CHECK(txn.sender() == 0x99f9b87991262f6ba471f09758cde1c0fc1de734_address);
     CHECK(txn.to == 0x5df9b87991262f6ba471f09758cde1c0fc1de734_address);
     CHECK(txn.gas_limit == 235);
-    CHECK(txn.max_fee_per_gas == 0);
-    CHECK(txn.max_priority_fee_per_gas == 0);
+    CHECK(txn.max_fee_per_gas == 10000);
+    CHECK(txn.max_priority_fee_per_gas == 10000);
     CHECK(txn.value == 31337);
     CHECK(txn.data == *silkworm::from_hex("001122aabbcc"));
 }

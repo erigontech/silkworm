3bff3763dfb01af0732e5f8d0231dcf9b770d8cd
lupin012 <58134934+lupin012@users.noreply.github.com>
2024-07-04 07:56:40 +0200

    fix reward

--- silkworm/core/protocol/rule_set_test.cpp
+++ silkworm/core/protocol/rule_set_test.cpp
@@ -36,7 +36,7 @@ TEST_CASE("Rule Set factory") {
     CHECK(rule_set->compute_reward(Block{}).miner == 0);
     rule_set = rule_set_factory(test::kLondonConfig);  // No-proof rule set
     CHECK(rule_set);
-    CHECK(rule_set->compute_reward(Block{}).miner == 0);
+    CHECK(rule_set->compute_reward(Block{}).miner == 2000000000000000000);
     rule_set = rule_set_factory(ChainConfig{.rule_set_config = bor::Config{}});
     CHECK(rule_set);
     CHECK(rule_set->compute_reward(Block{}).miner == 0);

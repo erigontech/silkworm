erigontech/silkworm

commit 199db69cf686cb679250eb69f05b11b2290f0621

db: support for E3 data format in remote state reader (#2253)

silkworm/rpc/daemon.cpp
@@ -32,6 +32,7 @@
 #include <silkworm/db/kv/api/direct_client.hpp>
 #include <silkworm/db/kv/grpc/client/remote_client.hpp>
 #include <silkworm/db/snapshot_bundle_factory_impl.hpp>
+#include <silkworm/db/state/version.hpp>
 #include <silkworm/infra/common/ensure.hpp>
 #include <silkworm/infra/common/log.hpp>
 #include <silkworm/infra/concurrency/private_service.hpp>
@@ -247,24 +248,14 @@ Daemon::Daemon(DaemonSettings settings, std::optional<mdbx::env> chaindata_env)
 
     // Create the unique KV state-changes stream feeding the state cache
     auto& context = context_pool_.next_context();
-    auto& io_context = *context.io_context();
-    auto& grpc_context = *context.grpc_context();
-    auto* state_cache{must_use_shared_service<db::kv::api::StateCache>(io_context)};
-    auto* backend{must_use_private_service<rpc::ethbackend::BackEnd>(io_context)};
-    if (settings_.standalone) {
-        kv_client_ = std::make_unique<db::kv::grpc::client::RemoteClient>(
-            create_channel_, grpc_context, state_cache, block_provider(backend), block_number_from_txn_hash_provider(backend));
-    } else {
-        // TODO(canepat) finish implementation and clean-up composition of objects here
-        db::kv::api::StateChangeRunner runner{io_context.get_executor()};
-        db::kv::api::ServiceRouter router{runner.state_changes_calls_channel()};
-        kv_client_ = std::make_unique<db::kv::api::DirectClient>(
-            std::make_shared<db::kv::api::DirectService>(router, *chaindata_env_, state_cache));
-    }
+    kv_client_ = make_kv_client(context);
     state_changes_stream_ = std::make_unique<db::kv::StateChangesStream>(context, *kv_client_);
 
     // Set compatibility with Erigon RpcDaemon at JSON RPC level
     compatibility::set_erigon_json_api_compatibility_required(settings_.erigon_json_rpc_compatibility);
+
+    // Schedule the retrieval of Erigon data storage model as first task on the execution contexts
+    schedule_data_format_retrieval();
 
     // Load JSON RPC specification for Ethereum API
     rpc::json_rpc::Validator::load_specification();
@@ -317,6 +308,35 @@ void Daemon::add_shared_services() {
         add_shared_service<db::kv::api::StateCache>(io_context, std::move(state_cache));
         add_shared_service(io_context, filter_storage);
         add_shared_service<engine::ExecutionEngine>(io_context, std::move(engine));
+    }
+}
+
+std::unique_ptr<db::kv::api::Client> Daemon::make_kv_client(rpc::ClientContext& context) {
+    auto& io_context = *context.io_context();
+    auto& grpc_context = *context.grpc_context();
+    auto* state_cache{must_use_shared_service<db::kv::api::StateCache>(io_context)};
+    auto* backend{must_use_private_service<rpc::ethbackend::BackEnd>(io_context)};
+    if (settings_.standalone) {
+        return std::make_unique<db::kv::grpc::client::RemoteClient>(
+            create_channel_, grpc_context, state_cache, block_provider(backend), block_number_from_txn_hash_provider(backend));
+    } else {
+        // TODO(canepat) finish implementation and clean-up composition of objects here
+        db::kv::api::StateChangeRunner runner{io_context.get_executor()};
+        db::kv::api::ServiceRouter router{runner.state_changes_calls_channel()};
+        return std::make_unique<db::kv::api::DirectClient>(
+            std::make_shared<db::kv::api::DirectService>(router, *chaindata_env_, state_cache));
+    }
+}
+
+void Daemon::schedule_data_format_retrieval() {
+    // Schedule the retrieval of Erigon data storage model as first task on all the execution contexts
+    // This ensures that the data format is set in any case *before* any API request handling happens
+    for (size_t i{0}; i < context_pool_.num_contexts(); ++i) {
+        auto& context = context_pool_.next_context();
+        concurrency::spawn_future(*context.io_context(), [this, &context]() -> Task<void> {
+            const auto kv_client = make_kv_client(context);
+            co_await db::state::set_data_format(*kv_client);
+        });
     }
 }
 

cmd/dev/grpc_toolbox.cpp
@@ -1233,6 +1233,7 @@ int main(int argc, char* argv[]) {
         "\tkv_seek_async\t\t\tquery using SEEK the Erigon/Silkworm Key-Value (KV) remote interface to database\n"
         "\tkv_seek_async_callback\t\tquery using SEEK the Erigon/Silkworm Key-Value (KV) remote interface to database\n"
         "\tkv_seek_both\t\t\tquery using SEEK_BOTH the Erigon/Silkworm Key-Value (KV) remote interface to database\n"
+        "\tkv_domain_get\t\tquery the Erigon/Silkworm Key-Value (KV) remote interface to data storage using DOMAIN_GET\n"
         "\tkv_history_seek\t\tquery using HISTORY_SEEK the Erigon/Silkworm Key-Value (KV) remote interface to data storage\n"
         "\tkv_index_range\t\tquery using INDEX_RANGE the Erigon/Silkworm Key-Value (KV) remote interface to data storage\n"
         "\tkv_history_range\t\tquery using HISTORY_RANGE the Erigon/Silkworm Key-Value (KV) remote interface to data storage\n"

cmd/test/ethereum.cpp
@@ -145,7 +145,7 @@ bool post_check(const InMemoryState& state, const nlohmann::json& expected) {
         }
 
         auto expected_code{j["code"].get<std::string>()};
-        Bytes actual_code{state.read_code(account->code_hash)};
+        Bytes actual_code{state.read_code(address, account->code_hash)};
         if (actual_code != from_hex(expected_code)) {
             std::cout << "Code mismatch for " << entry.key() << ":\n"
                       << to_hex(actual_code) << " != " << expected_code << std::endl;

silkworm/core/state/in_memory_state.cpp
@@ -37,7 +37,7 @@ std::optional<Account> InMemoryState::read_account(const evmc::address& address)
     return it->second;
 }
 
-ByteView InMemoryState::read_code(const evmc::bytes32& code_hash) const noexcept {
+ByteView InMemoryState::read_code(const evmc::address& /*address*/, const evmc::bytes32& code_hash) const noexcept {
     auto it{code_.find(code_hash)};
     if (it == code_.end()) {
         return {};

silkworm/core/state/in_memory_state.hpp
@@ -38,7 +38,7 @@ class InMemoryState : public State {
 
     std::optional<Account> read_account(const evmc::address& address) const noexcept override;
 
-    ByteView read_code(const evmc::bytes32& code_hash) const noexcept override;
+    ByteView read_code(const evmc::address& address, const evmc::bytes32& code_hash) const noexcept override;
 
     evmc::bytes32 read_storage(const evmc::address& address, uint64_t incarnation,
                                const evmc::bytes32& location) const noexcept override;

silkworm/core/state/intra_block_state.cpp
@@ -219,7 +219,7 @@ ByteView IntraBlockState::get_code(const evmc::address& address) const noexcept 
         return it->second;
     }
 
-    ByteView code{db_.read_code(code_hash)};
+    ByteView code{db_.read_code(address, code_hash)};
     existing_code_[code_hash] = code;
     return code;
 }

silkworm/core/state/state.hpp
@@ -38,7 +38,7 @@ class State : public BlockState {
 
     [[nodiscard]] virtual std::optional<Account> read_account(const evmc::address& address) const noexcept = 0;
 
-    [[nodiscard]] virtual ByteView read_code(const evmc::bytes32& code_hash) const noexcept = 0;
+    [[nodiscard]] virtual ByteView read_code(const evmc::address& address, const evmc::bytes32& code_hash) const noexcept = 0;
 
     [[nodiscard]] virtual evmc::bytes32 read_storage(const evmc::address& address, uint64_t incarnation,
                                                      const evmc::bytes32& location) const noexcept = 0;

silkworm/db/buffer.cpp
@@ -524,7 +524,7 @@ std::optional<Account> Buffer::read_account(const evmc::address& address) const 
     return db_account;
 }
 
-ByteView Buffer::read_code(const evmc::bytes32& code_hash) const noexcept {
+ByteView Buffer::read_code(const evmc::address& /*address*/, const evmc::bytes32& code_hash) const noexcept {
     if (auto it{hash_to_code_.find(code_hash)}; it != hash_to_code_.end()) {
         return it->second;
     }

commit 4ee454c0ff3ef4bd1ba780dd8704b55e56289748

wasm: fix interface after PR 2253 (#2280)
This PR fixes the WASM build broken after PR #2253 that changed the signature of silkworm::State::read_code method to include the account address, necessary for E3 Temporal KV DomainGet.

silkworm/rpc/daemon.cpp
@@ -319,13 +319,12 @@ std::unique_ptr<db::kv::api::Client> Daemon::make_kv_client(rpc::ClientContext& 
     if (settings_.standalone) {
         return std::make_unique<db::kv::grpc::client::RemoteClient>(
             create_channel_, grpc_context, state_cache, block_provider(backend), block_number_from_txn_hash_provider(backend));
-    } else {
-        // TODO(canepat) finish implementation and clean-up composition of objects here
-        db::kv::api::StateChangeRunner runner{io_context.get_executor()};
-        db::kv::api::ServiceRouter router{runner.state_changes_calls_channel()};
-        return std::make_unique<db::kv::api::DirectClient>(
-            std::make_shared<db::kv::api::DirectService>(router, *chaindata_env_, state_cache));
     }
+    // TODO(canepat) finish implementation and clean-up composition of objects here
+    db::kv::api::StateChangeRunner runner{io_context.get_executor()};
+    db::kv::api::ServiceRouter router{runner.state_changes_calls_channel()};
+    return std::make_unique<db::kv::api::DirectClient>(
+        std::make_shared<db::kv::api::DirectService>(router, *chaindata_env_, state_cache));
 }
 
 void Daemon::schedule_data_format_retrieval() {

silkworm/wasm/silkworm_wasm_api.cpp
@@ -157,9 +157,9 @@ Account* state_read_account_new(const State* state, const uint8_t* address) {
     return out;
 }
 
-Bytes* state_read_code_new(const State* state, const uint8_t* code_hash) {
+Bytes* state_read_code_new(const State* state, const uint8_t* address, const uint8_t* code_hash) {
     auto out{new Bytes};
-    *out = state->read_code(bytes32_from_ptr(code_hash));
+    *out = state->read_code(address_from_ptr(address), bytes32_from_ptr(code_hash));
     return out;
 }
 

silkworm/wasm/silkworm_wasm_api.hpp
@@ -100,7 +100,7 @@ SILKWORM_EXPORT uint8_t* state_root_hash_new(const silkworm::InMemoryState* stat
 SILKWORM_EXPORT silkworm::Account* state_read_account_new(const silkworm::State* state, const uint8_t* address);
 
 // Result has to be freed with delete_bytes
-SILKWORM_EXPORT silkworm::Bytes* state_read_code_new(const silkworm::State* state, const uint8_t* code_hash);
+SILKWORM_EXPORT silkworm::Bytes* state_read_code_new(const silkworm::State* state, const uint8_t* address, const uint8_t* code_hash);
 
 // Result has to be freed with delete_bytes
 SILKWORM_EXPORT silkworm::Bytes* state_read_storage_new(const silkworm::State* state, const uint8_t* address,
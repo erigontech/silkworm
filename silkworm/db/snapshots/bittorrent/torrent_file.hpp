d8658d2c5f5400a3909d4fb828a9be722b84e808
battlmonstr <battlmonstr@users.noreply.github.com>
2024-08-30 13:30:17 +0200

    snapshots: torrent file creation

---
+++ silkworm/db/snapshots/bittorrent/torrent_file.hpp
@@ -0,0 +1,40 @@
+/*
+   Copyright 2024 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#pragma once
+
+#include <filesystem>
+
+#include <libtorrent/add_torrent_params.hpp>
+
+#include <silkworm/core/common/bytes.hpp>
+
+namespace silkworm::snapshots::bittorrent {
+
+class TorrentFile {
+  public:
+    TorrentFile(ByteView data);
+
+    static TorrentFile from_source_file(std::filesystem::path source_file_path, std::time_t creation_date = 0);
+
+    const lt::add_torrent_params& params() const { return params_; }
+    Bytes to_bytes() const;
+
+  private:
+    lt::add_torrent_params params_;
+};
+
+}  // namespace silkworm::snapshots::bittorrent

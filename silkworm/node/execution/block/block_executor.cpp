bebbd47cf48b1eced8a6b04fe241488cfefeb602
Bartosz Zawistowski <bartosz.zawistowski@erigon.tech>
2024-09-09 17:02:11 +0200

    Compilation fixes

--- silkworm/node/execution/block/block_executor.cpp
+++ silkworm/node/execution/block/block_executor.cpp
@@ -19,6 +19,8 @@
 #include <silkworm/infra/common/log.hpp>
 #include <silkworm/node/execution/block/block_executor.hpp>
 
+#include <tuple>
+
 namespace silkworm::execution::block {
 
 using namespace std::chrono_literals;
@@ -28,7 +30,7 @@ static log::Args log_args_for_exec_progress(BlockExecutor::ExecutionProgress& pr
     static auto float_to_string = [](float f) -> std::string {
         const auto size = std::snprintf(nullptr, 0, "%.1f", static_cast<double>(f));
         std::string s(static_cast<size_t>(size + 1), '\0');                                  // +1 for null terminator
-        [[maybe_unused]] std::snprintf(s.data(), s.size(), "%.1f", static_cast<double>(f));  // certain to fit
+        std::ignore = std::snprintf(s.data(), s.size(), "%.1f", static_cast<double>(f));  // certain to fit
         return s.substr(0, s.size() - 1);                                                    // remove null terminator
     };
 

849c8868a96ddafa424986b97af82e58b0d5dd5f
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-09-29 21:14:49 +0200

    add script

---
+++ .github/workflows/e2_run_tests.sh
@@ -0,0 +1,3 @@
+:
+python3 ./run_tests.py --continue --blockchain mainnet --jwt $1 --display-only-fail --port 8545 -x admin_,eth_mining,eth_getWork,eth_coinbase,eth_createAccessList/test_16.json,engine_,net_,web3_,txpool_,eth_submitWork,eth_submitHashrate,eth_protocolVersion,erigon_nodeInfo --transport_type http,websocket
+return $?

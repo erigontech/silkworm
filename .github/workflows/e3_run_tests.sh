086fbcfb3800a9e4ce07e11a832d76d3c5870d11
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-09-29 21:38:18 +0200

    add e3 script

---
+++ .github/workflows/e3_run_tests.sh
@@ -0,0 +1,3 @@
+:
+python3 ./run_tests.py --continue --blockchain mainnet --jwt $1 --display-only-fail --port 8545 -x admin_,eth_mining,eth_getWork,eth_coinbase,eth_createAccessList/test_16.json,engine_,net_,web3_,txpool_,eth_submitWork,eth_submitHashrate,eth_protocolVersion,erigon_nodeInfo,debug_accountRange,debug_getModifiedAccounts,debug_storageRangeAt,erigon_getBalanceChangesInBlock,ots_getTransactionBySenderAndNonce,parity_listStorageKeys,ots_getContractCreator,erigon_getLatestLogs,eth_getLogs,erigon_getBlockReceiptsByBlockHash,parity_getBlockReceipts,eth_getTransactionReceipt,ots_getBlockDetails,ots_getBlockDetailsByHash,ots_getBlockTransactions,erigon_getLogsByHash,eth_feeHistory,eth_getBlockReceipts --transport_type http,websocket
+exit $?

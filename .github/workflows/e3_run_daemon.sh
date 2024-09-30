f321e0c9accaa921d3c123bf5dec9d7b40da26f8
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-09-30 22:41:23 +0200

    move some shell on script

--- .github/workflows/e3_run_daemon.sh
+++ .github/workflows/e3_run_daemon.sh
@@ -1,2 +1,7 @@
 :
-./rpcdaemon --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws
+echo "Silkworm RpcDaemon starting..."
+./rpcdaemon --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws &
+RPC_DAEMON_PID=$!
+echo "Silkworm RpcDaemon started"
+exit $RPC_DAEMON_PID
+

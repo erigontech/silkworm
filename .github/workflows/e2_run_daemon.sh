cb1edba11b24a40610767ea1c7ca349a49040ae4
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-09-30 22:41:18 +0200

    move some shell on script

--- .github/workflows/e2_run_daemon.sh
+++ .github/workflows/e2_run_daemon.sh
@@ -1,2 +1,6 @@
 :
-./rpcdaemon --datadir $1 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws
+echo "Silkworm RpcDaemon starting..."
+./rpcdaemon --datadir $1 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws &
+RPC_DAEMON_PID=$!
+echo "Silkworm RpcDaemon started"
+exit $RPC_DAEMON_PID

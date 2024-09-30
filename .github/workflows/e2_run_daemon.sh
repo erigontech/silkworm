3502e0299911d0aee2b360bd111349967e033021
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-09-30 22:56:16 +0200

    remove echo

--- .github/workflows/e2_run_daemon.sh
+++ .github/workflows/e2_run_daemon.sh
@@ -2,5 +2,4 @@
 echo "Silkworm RpcDaemon starting..."
 ./rpcdaemon --datadir $1 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws &
 RPC_DAEMON_PID=$!
-echo "Silkworm RpcDaemon started"
 exit $RPC_DAEMON_PID

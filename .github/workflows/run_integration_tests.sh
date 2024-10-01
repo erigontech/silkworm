c1545f28f32e19d36a73f31625fe4339fb039aad
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-10-01 20:45:25 +0200

    run tests single script for e2 e e3

---
+++ .github/workflows/run_integration_tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 Erigon2/Erigon3 <integration-dir> <jwt-file> <saved failed tests>"
+  exit 1
+fi
+
+set +e # Disable exit on error
+cd $2
+rm -rf ./mainnet/results/
+ 
+if [ $1 == 'Erigon2' ]; then
+   python3 ./run_tests.py --continue --blockchain mainnet --jwt $3 --display-only-fail --port 8545 -x admin_,eth_mining,eth_getWork,eth_coinbase,eth_createAccessList/test_16.json,engine_,net_,web3_,txpool_,eth_submitWork,eth_submitHashrate,eth_protocolVersion,erigon_nodeInfo --transport_type http,websocket
+else
+   python3 ./run_tests.py --continue --blockchain mainnet --jwt $3 --display-only-fail --port 8545 -x engine_,\
+debug_accountRange,\
+debug_getModifiedAccounts,\
+debug_storageRangeAt,\
+erigon_getBalanceChangesInBlock,\
+ots_getTransactionBySenderAndNonce,\
+parity_listStorageKeys,\
+ots_getContractCreator,\
+erigon_getLatestLogs,\
+eth_getLogs,\
+erigon_getBlockReceiptsByBlockHash,\
+parity_getBlockReceipts,\
+eth_getTransactionReceipt,\
+ots_getBlockDetails,\
+ots_getBlockDetailsByHash,\
+ots_getBlockTransactions,\
+erigon_getLogsByHash,\
+eth_feeHistory,\
+eth_getBlockReceipts,\
+ots_searchTransactionsAfter,\
+ots_searchTransactionsBefore --transport_type http,websocket
+fi
+
+failed_test=$?
+
+# Check test runner exit status
+if [ $failed_test -eq 0 ]; then
+    echo "tests completed successfully"
+else
+    echo "error detected during tests"
+
+    # Save failed results to a directory with timestamp and commit hash
+    cp -r $2/mainnet/results/ $4
+fi
+exit $failed_test
+

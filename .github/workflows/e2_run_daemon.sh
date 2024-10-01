e20940869e8c18ecfd01fa05868a5c06a4c3fb86
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-10-01 18:16:16 +0200

    put on same script run erigon2/erigon3 according param

--- .github/workflows/e2_run_daemon.sh
+++ .github/workflows/e2_run_daemon.sh
@@ -1,5 +1,25 @@
-:
+#!/bin/bash
+trap : SIGTERM SIGINT
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 Erigon2/Erigon3 <datadir> <jwtfile>"
+  exit -1
+fi
+
 echo "Silkworm RpcDaemon starting..."
-./rpcdaemon --datadir $1 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws &
-RPC_DAEMON_PID=$!
-exit $RPC_DAEMON_PID
+if [ $1 == 'Erigon2' ]; then
+./rpcdaemon --datadir $2 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $3 --skip_protocol_check --ws &
+else
+./rpcdaemon --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $2 --skip_protocol_check --ws &
+fi
+
+PID=$!
+
+wait $PID
+
+if [[ $? -gt 128 ]]
+then
+    kill $PID
+fi
+
+exit 0 

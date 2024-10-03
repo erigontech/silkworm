97b2d5f77a83235daff38fee032b0770ebc66d37
Bartosz Zawistowski <bartosz.zawistowski@erigon.tech>
2024-10-03 17:27:59 +0200

    Some progress with requests

---
+++ silkworm/core/types/request.cpp
@@ -0,0 +1,89 @@
+/*
+Copyright 2022 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "request.hpp"
+
+#include <type_traits>
+
+#include <silkworm/core/rlp/decode_vector.hpp>
+#include <silkworm/core/rlp/encode.hpp>
+#include <silkworm/core/types/address.hpp>
+
+namespace silkworm {
+
+static rlp::Header compute_header(const DepositRequest& request) {
+    rlp::Header header{.list = true};
+    header.payload_length += rlp::length(request.pub_key);
+    header.payload_length += rlp::length(request.withdrawal_credentials);
+    header.payload_length += rlp::length(request.amount);
+    header.payload_length += rlp::length(request.signature);
+    header.payload_length += rlp::length(request.index);
+    return header;
+}
+
+void DepositRequest::encode(Bytes& to) const {
+    const auto header = compute_header(*this);
+    using underlying = std::underlying_type_t<Request::RequestType>;
+    rlp::encode(to, static_cast<underlying>(Request::RequestType::DepositRequestType));
+    rlp::encode_header(to, header);
+    rlp::encode(to, pub_key);
+    rlp::encode(to, withdrawal_credentials);
+    rlp::encode(to, amount);
+    rlp::encode(to, signature);
+    rlp::encode(to, index);
+}
+
+size_t DepositRequest::length() const {
+    rlp::Header header{.list = true};
+    header.payload_length += rlp::length(pub_key);
+    header.payload_length += rlp::length(withdrawal_credentials);
+    header.payload_length += rlp::length(amount);
+    header.payload_length += rlp::length(signature);
+    header.payload_length += rlp::length(index);
+    return rlp::length_of_length(header.payload_length) + header.payload_length;
+}
+
+void WithdrawalRequest::encode(Bytes& to) const {
+}
+
+size_t WithdrawalRequest::length() const {
+    return 0;
+}
+
+void ConsolidationRequest::encode(Bytes& to) const {
+}
+
+size_t ConsolidationRequest::length() const {
+    return 0;
+}
+
+namespace rlp {
+
+    size_t length(const Request& request) {
+        return request.length();
+    }
+
+    void encode(Bytes& to, const Request& request) {
+        request.encode(to);
+    }
+
+    DecodingResult decode(ByteView& from, Request& to, Leftover mode) noexcept {
+        return {};
+    }
+
+}  // namespace rlp
+
+}  // namespace silkworm
---
+++ silkworm/core/types/request.hpp
@@ -0,0 +1,80 @@
+/*
+Copyright 2024 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#pragma once
+
+#include <evmc/evmc.hpp>
+
+#include <silkworm/core/common/bytes.hpp>
+#include <silkworm/core/rlp/decode.hpp>
+#include <silkworm/core/types/hash.hpp>
+
+namespace silkworm {
+
+static auto constexpr kBLSKeyLen = 48;
+static auto constexpr kBLSSignatureLen = 96;
+
+using BLSKey = std::array<uint8_t, kBLSKeyLen>;
+using BLSSignature = std::array<uint8_t, kBLSSignatureLen>;
+
+struct Request {
+    enum class RequestType : uint8_t {
+        DepositRequestType = 0,
+        WithdrawalRequestType = 1,
+        ConsolidationRequestType = 2
+    };
+
+    virtual ~Request() = default;
+    virtual void encode(Bytes& to) const = 0;
+    virtual size_t length() const = 0;
+};
+
+struct DepositRequest final : public Request {
+    std::array<uint8_t, kBLSKeyLen> pub_key;
+    Hash withdrawal_credentials;
+    uint64_t amount = 0;
+    BLSSignature signature;
+    uint64_t index = 0;
+
+    void encode(Bytes& to) const override;
+    size_t length() const override;
+};
+
+struct WithdrawalRequest final : public Request {
+    evmc::address source_address;
+    std::array<uint8_t, kBLSKeyLen> validator_pub_key;
+    uint64_t amount{};
+
+    void encode(Bytes& to) const override;
+    size_t length() const override;
+};
+
+struct ConsolidationRequest final : public Request {
+    evmc::address source_address;
+    BLSKey source_pub_key;
+    BLSKey target_pub_key;
+
+    void encode(Bytes& to) const override;
+    size_t length() const override;
+};
+
+namespace rlp {
+    size_t length(const Request&);
+    void encode(Bytes& to, const Request&);
+    DecodingResult decode(ByteView& from, Request& to, Leftover mode = Leftover::kProhibit) noexcept;
+}  // namespace rlp
+
+}  // namespace silkworm

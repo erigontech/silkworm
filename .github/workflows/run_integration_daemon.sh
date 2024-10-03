dac63afa89d1a87526d645dfb8d527d2627bf999
canepat <16927169+canepat@users.noreply.github.com>
2024-10-03 17:11:42 +0200

    Update run_integration_daemon.sh

--- .github/workflows/run_integration_daemon.sh
+++ .github/workflows/run_integration_daemon.sh
@@ -10,7 +10,7 @@ echo "Silkworm RpcDaemon starting..."
 if [ $1 == 'Erigon2' ]; then
 ./rpcdaemon --datadir $2 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $3 --skip_protocol_check --ws &
 else
-./rpcdaemon --eth.addr localhost:51515 --engine.addr localhost:51516 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $3 --skip_protocol_check --ws &
+./rpcdaemon --eth.addr 127.0.0.1:51515 --engine.addr 127.0.0.1:51516 --api admin,debug,eth,parity,erigon,trace,web3,txpool,ots,net --log.verbosity 1 --erigon_compatibility --jwt $3 --skip_protocol_check --ws &
 fi
 
 PID=$!

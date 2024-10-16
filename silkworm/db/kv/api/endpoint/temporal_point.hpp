368e39b333e05c7efdbc1f101f9870d2fd8d9de7
canepat <16927169+canepat@users.noreply.github.com>
2024-10-16 11:06:02 +0200

    Update temporal_point.hpp

--- silkworm/db/kv/api/endpoint/temporal_point.hpp
+++ silkworm/db/kv/api/endpoint/temporal_point.hpp
@@ -43,14 +43,9 @@ struct DomainPointQuery {
     Bytes key;
     std::optional<Timestamp> timestamp;  // not present means 'latest state' (no history lookup)
     Bytes sub_key;
+
+    auto operator<=>(const DomainPointQuery&) const = default;
 };
-inline bool operator==(const db::kv::api::DomainPointQuery& dpq1, const db::kv::api::DomainPointQuery& dpq2) {
-    return (dpq1.tx_id == dpq2.tx_id &&
-            dpq1.table == dpq2.table &&
-            dpq1.key == dpq2.key &&
-            dpq1.timestamp == dpq2.timestamp &&
-            dpq1.sub_key == dpq2.sub_key);
-}
 
 using DomainPointResult = PointResult;
 

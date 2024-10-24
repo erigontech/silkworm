05557c585fd5e61517aee85c2bd50c9440dc415f
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-10-24 07:10:47 +0200

    add providers

---
+++ silkworm/db/chain/providers.hpp
@@ -0,0 +1,42 @@
+/*
+   Copyright 2023 The Silkworm Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#pragma once
+
+#include <functional>
+
+#include <silkworm/core/common/base.hpp>
+#include <silkworm/core/types//block.hpp>
+#include <silkworm/core/types//hash.hpp>
+#include <silkworm/infra/concurrency/task.hpp>
+
+namespace silkworm::db::chain {
+
+using BlockProvider = std::function<Task<bool>(BlockNum, HashAsSpan, bool, Block&)>;
+using BlockNumberFromTxnHashProvider = std::function<Task<BlockNum>(HashAsSpan)>;
+using BlockNumberFromBlockHashProvider = std::function<Task<BlockNum>(HashAsSpan)>;
+using CanonicalBlockHashFromNumberProvider = std::function<Task<evmc::bytes32>(BlockNum)>;
+using CanonicalBodyForStorageProvider = std::function<Task<Bytes>(BlockNum)>;
+
+struct Providers {
+    BlockProvider block;
+    BlockNumberFromTxnHashProvider block_number_from_txn_hash;
+    BlockNumberFromBlockHashProvider block_number_from_hash;
+    CanonicalBlockHashFromNumberProvider canonical_block_hash_from_number;
+    CanonicalBodyForStorageProvider canonical_body_for_storage;
+};
+
+}  // namespace silkworm::db::chain

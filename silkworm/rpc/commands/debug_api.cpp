ec8552ee58e9ad0cf230080ea25b49258ffc9098
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-10-26 15:40:53 +0200

    align error to erigon

--- silkworm/rpc/commands/debug_api.cpp
+++ silkworm/rpc/commands/debug_api.cpp
@@ -443,7 +443,11 @@ Task<void> DebugRpcApi::handle_debug_trace_call(const nlohmann::json& request, j
     } catch (const std::exception& e) {
         SILK_ERROR << "exception: " << e.what() << " processing request: " << request.dump();
         std::ostringstream oss;
-        oss << "block " << block_number_or_hash.number() << "(" << silkworm::to_hex(block_number_or_hash.hash()) << ") not found";
+        if (block_number_or_hash.hash())
+           oss << "block " << silkworm::to_hex(block_number_or_hash.hash()) << " not found";
+        else {
+           oss << "block " << block_number_or_hash.number() << " not found";
+        }
         const Error error{kServerError, oss.str()};
         stream.write_json_field("error", error);
     } catch (...) {

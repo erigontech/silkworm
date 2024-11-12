7e39e23c6de46f338af44922fbbee027a3217281
battlmonstr <battlmonstr@users.noreply.github.com>
2024-11-12 15:04:24 +0100

    fix gmake

--- cmake/run_smoke_tests.cmake
+++ cmake/run_smoke_tests.cmake
@@ -32,6 +32,7 @@ if(NOT CMAKE_EXECUTABLE_SUFFIX)
   list(FILTER COMMANDS EXCLUDE REGEX "\\.")
 endif()
 list(FILTER COMMANDS EXCLUDE REGEX "core_test") # this is a unit test
+list(FILTER COMMANDS EXCLUDE REGEX "Makefile")
 
 # TODO: fix check_log_indices --help
 list(FILTER COMMANDS EXCLUDE REGEX "check_log_indices")

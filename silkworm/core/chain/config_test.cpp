1f9b04980f38bc47eaf7b2d6025ab842c85bd2ff
battlmonstr <battlmonstr@users.noreply.github.com>
2024-11-15 09:49:03 +0100

    Revert "debug"
    This reverts commit 109cb0abd1939487984cf2f63971f2c08c692c86.

--- silkworm/core/chain/config_test.cpp
+++ silkworm/core/chain/config_test.cpp
@@ -26,14 +26,6 @@ using namespace evmc::literals;
 
 namespace silkworm {
 
-TEST_CASE("test1") {
-    CHECK(false);
-}
-
-TEST_CASE("test2") {
-    abort();
-}
-
 TEST_CASE("Known configs") {
     static_assert(kKnownChainConfigs.size() == kKnownChainNameToId.size());
     for (const auto& [_, id] : kKnownChainNameToId) {

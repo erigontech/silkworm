65c934cdf0e3cf05f096b7096e21ab5fbf9fc90d
Paweł Bylica <pawel@ethereum.org>
2024-12-03 16:25:02 +0100

    core: move get_block_hash() from EvmHost to EVM (#2553)

--- silkworm/core/execution/evm.cpp
+++ silkworm/core/execution/evm.cpp
@@ -479,17 +479,17 @@ evmc_tx_context EvmHost::get_tx_context() const noexcept {
     return context;
 }
 
-evmc::bytes32 EvmHost::get_block_hash(int64_t block_num) const noexcept {
+evmc::bytes32 EVM::get_block_hash(int64_t block_num) noexcept {
     SILKWORM_ASSERT(block_num >= 0);
-    const uint64_t current_block_num{evm_.block_.header.number};
+    const uint64_t current_block_num{block_.header.number};
     SILKWORM_ASSERT(static_cast<uint64_t>(block_num) < current_block_num);
     const uint64_t new_size_u64{current_block_num - static_cast<uint64_t>(block_num)};
     SILKWORM_ASSERT(std::in_range<size_t>(new_size_u64));
     const size_t new_size{static_cast<size_t>(new_size_u64)};
 
-    std::vector<evmc::bytes32>& hashes{evm_.block_hashes_};
+    std::vector<evmc::bytes32>& hashes{block_hashes_};
     if (hashes.empty()) {
-        hashes.push_back(evm_.block_.header.parent_hash);
+        hashes.push_back(block_.header.parent_hash);
     }
 
     const size_t old_size{hashes.size()};
@@ -498,7 +498,7 @@ evmc::bytes32 EvmHost::get_block_hash(int64_t block_num) const noexcept {
     }
 
     for (size_t i{old_size}; i < new_size; ++i) {
-        std::optional<BlockHeader> header{evm_.state().db().read_header(current_block_num - i, hashes[i - 1])};
+        std::optional<BlockHeader> header{state().db().read_header(current_block_num - i, hashes[i - 1])};
         if (!header) {
             break;
         }
@@ -506,6 +506,10 @@ evmc::bytes32 EvmHost::get_block_hash(int64_t block_num) const noexcept {
     }
 
     return hashes[new_size - 1];
+}
+
+evmc::bytes32 EvmHost::get_block_hash(int64_t block_num) const noexcept {
+    return evm_.get_block_hash(block_num);
 }
 
 void EvmHost::emit_log(const evmc::address& address, const uint8_t* data, size_t data_size,

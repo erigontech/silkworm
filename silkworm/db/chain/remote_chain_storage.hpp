86b5e2a26db7fe714f32bb7d67729a19a8b2c5f5
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-12-14 10:41:35 +0100

    remove db::chain::remove_head_header_hash()

--- silkworm/db/chain/remote_chain_storage.hpp
+++ silkworm/db/chain/remote_chain_storage.hpp
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <optional>
+
 #include <silkworm/db/chain/providers.hpp>
 #include <silkworm/db/kv/api/transaction.hpp>
 
@@ -68,6 +70,8 @@ class RemoteChainStorage : public ChainStorage {
 
     Task<std::optional<BlockNum>> read_block_num_by_transaction_hash(const evmc::bytes32& transaction_hash) const override;
     Task<std::optional<Transaction>> read_transaction_by_idx_in_block(BlockNum block_num, uint64_t txn_id) const override;
+
+    Task<std::pair<std::optional<BlockHeader>, std::optional<Hash>>> read_head_header_and_hash() const override;
 
   protected:
     Providers& providers() { return providers_; }

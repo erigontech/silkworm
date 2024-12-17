85bc5256774183bfee2eaba3d8a4f4f5482eb6ba
lupin012 <58134934+lupin012@users.noreply.github.com.>
2024-12-17 11:33:09 +0100

    provides  txn_id on estimate_gas()

--- silkworm/rpc/core/estimate_gas_oracle.hpp
+++ silkworm/rpc/core/estimate_gas_oracle.hpp
@@ -90,7 +90,7 @@ class EstimateGasOracle {
     EstimateGasOracle(const EstimateGasOracle&) = delete;
     EstimateGasOracle& operator=(const EstimateGasOracle&) = delete;
 
-    Task<intx::uint256> estimate_gas(const Call& call, const silkworm::Block& latest_block, std::optional<BlockNum> block_num_for_gas_limit = {});
+    Task<intx::uint256> estimate_gas(const Call& call, const silkworm::Block& latest_block, TxnId txn_id, std::optional<BlockNum> block_num_for_gas_limit = {});
 
   protected:
     virtual ExecutionResult try_execution(EVMExecutor& executor, const silkworm::Block& _block, const silkworm::Transaction& transaction);

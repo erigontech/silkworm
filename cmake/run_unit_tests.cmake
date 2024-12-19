bd009a0f8c40167af8cfe97bdc4681d335b171b4
canepat <16927169+canepat@users.noreply.github.com>
2024-12-19 22:10:34 +0100

    ci: fix TSAN suppressions (#2612)

--- cmake/run_unit_tests.cmake
+++ cmake/run_unit_tests.cmake
@@ -19,6 +19,11 @@ if(NOT SILKWORM_BUILD_DIR)
 endif()
 file(REAL_PATH "${SILKWORM_BUILD_DIR}" SILKWORM_BUILD_DIR)
 
+if(NOT SILKWORM_PROJECT_DIR)
+  set(SILKWORM_PROJECT_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
+endif()
+file(REAL_PATH "${SILKWORM_PROJECT_DIR}" SILKWORM_PROJECT_DIR)
+
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
   set(CMAKE_EXECUTABLE_SUFFIX ".exe")
 endif()
@@ -43,6 +48,10 @@ string(TIMESTAMP TIME "%s")
 message("For all tests --rng-seed=${TIME}")
 message("")
 
+if("${SILKWORM_SANITIZE}" STREQUAL "thread")
+  set(ENV{TSAN_OPTIONS} "suppressions=${SILKWORM_PROJECT_DIR}/tools/sanitizer/tsan_suppressions.txt")
+endif()
+
 foreach(TEST_COMMAND IN LISTS TEST_COMMANDS)
   file(RELATIVE_PATH TEST_COMMAND_REL_PATH "${SILKWORM_BUILD_DIR}" "${TEST_COMMAND}")
   message("Running ${TEST_COMMAND_REL_PATH}...")
@@ -50,10 +59,6 @@ foreach(TEST_COMMAND IN LISTS TEST_COMMANDS)
   if(SILKWORM_CLANG_COVERAGE)
     get_filename_component(TEST_COMMAND_NAME "${TEST_COMMAND}" NAME)
     set(ENV{LLVM_PROFILE_FILE} "${TEST_COMMAND_NAME}.profraw")
-  endif()
-
-  if("${SILKWORM_SANITIZE}" STREQUAL "thread")
-    set(ENV{TSAN_OPTIONS} "suppressions=tsan_suppressions.txt")
   endif()
 
   execute_process(COMMAND "${TEST_COMMAND}" "--rng-seed=${TIME}" "--min-duration=2" RESULT_VARIABLE EXIT_CODE)

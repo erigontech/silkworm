62b69b02c3214aa0a69577ed42e6e095547bacf9
Sixtysixter <20945591+Sixtysixter@users.noreply.github.com>
2024-12-23 16:20:08 +0100

    rpcdaemon: fix use-after-free in erigon_getLatestLogs (#2617)

--- silkworm/rpc/commands/eth_api.cpp
+++ silkworm/rpc/commands/eth_api.cpp
@@ -1516,7 +1516,8 @@ Task<void> EthereumRpcApi::handle_eth_new_filter(const nlohmann::json& request, 
     auto tx = co_await database_->begin();
 
     try {
-        LogsWalker logs_walker{*block_cache_, *tx, *backend_, workers_};
+        auto storage = tx->create_storage();
+        LogsWalker logs_walker(*block_cache_, *tx, *storage, *backend_, workers_);
 
         const auto [start, end] = co_await logs_walker.get_block_nums(filter);
         filter.start = start;
@@ -1600,7 +1601,9 @@ Task<void> EthereumRpcApi::handle_eth_get_filter_logs(const nlohmann::json& requ
     auto tx = co_await database_->begin();
 
     try {
-        LogsWalker logs_walker{*block_cache_, *tx, *backend_, workers_};
+        auto storage = tx->create_storage();
+        LogsWalker logs_walker(*block_cache_, *tx, *storage, *backend_, workers_);
+
         const auto [start, end] = co_await logs_walker.get_block_nums(filter);
 
         if (filter.start != start && filter.end != end) {
@@ -1649,7 +1652,9 @@ Task<void> EthereumRpcApi::handle_eth_get_filter_changes(const nlohmann::json& r
     auto tx = co_await database_->begin();
 
     try {
-        LogsWalker logs_walker{*block_cache_, *tx, *backend_, workers_};
+        auto storage = tx->create_storage();
+        LogsWalker logs_walker(*block_cache_, *tx, *storage, *backend_, workers_);
+
         const auto [start, end] = co_await logs_walker.get_block_nums(filter);
 
         std::vector<Log> logs;
@@ -1717,7 +1722,9 @@ Task<void> EthereumRpcApi::handle_eth_get_logs(const nlohmann::json& request, st
     auto tx = co_await database_->begin();
 
     try {
-        LogsWalker logs_walker{*block_cache_, *tx, *backend_, workers_};
+        auto storage = tx->create_storage();
+        LogsWalker logs_walker(*block_cache_, *tx, *storage, *backend_, workers_);
+
         const auto [start, end] = co_await logs_walker.get_block_nums(filter);
         if (start == end && start == std::numeric_limits<std::uint64_t>::max()) {
             auto error_msg = "invalid eth_getLogs filter block_hash: " + filter.block_hash.value();

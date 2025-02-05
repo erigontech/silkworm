aad77aeefd82f26ac2308da40d60d8c698ef8a2f
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-02-05 06:56:38 +0100

    convert_validation_result from routine to method

--- silkworm/rpc/core/evm_executor.cpp
+++ silkworm/rpc/core/evm_executor.cpp
@@ -166,7 +166,7 @@ void EVMExecutor::reset() {
     execution_processor_.reset();
 }
 
-ExecutionResult convert_validation_result(const ValidationResult& result, const Block& block, const silkworm::Transaction& txn, const EVM& evm, const intx::uint256& owned_funds) {
+ExecutionResult EVMExecutor::convert_validation_result(const ValidationResult& result, const Block& block, const silkworm::Transaction& txn, const EVM& evm) {
     std::string from = address_to_hex(*txn.sender());
     switch (result) {
         case ValidationResult::kMaxPriorityFeeGreaterThanMax: {
@@ -194,6 +194,7 @@ ExecutionResult convert_validation_result(const ValidationResult& result, const 
         }
 
         case ValidationResult::kInsufficientFunds: {
+            auto owned_funds = execution_processor_.intra_block_state().get_balance(*txn.sender());
             const intx::uint256 base_fee_per_gas{block.header.base_fee_per_gas.value_or(0)};
 
             const intx::uint256 effective_gas_price{txn.max_fee_per_gas >= base_fee_per_gas ? txn.effective_gas_price(base_fee_per_gas)
@@ -235,12 +236,7 @@ ExecutionResult EVMExecutor::call(
 
     const auto result = execution_processor_.call(txn, tracers, refund);
     if (result.validation_result != ValidationResult::kOk) {
-        intx::uint256 owned_funds{0};
-        if (result.validation_result == ValidationResult::kInsufficientFunds) {
-            owned_funds = execution_processor_.intra_block_state().get_balance(*txn.sender());
-        }
-
-        return convert_validation_result(result.validation_result, block, txn, evm, owned_funds);
+        return convert_validation_result(result.validation_result, block, txn, evm);
     }
 
     ExecutionResult exec_result{result.status, result.gas_left, result.data};

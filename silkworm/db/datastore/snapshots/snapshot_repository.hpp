ce1a24f94eeb69740f02bcf82323fce81fa3e43e
canepat <16927169+canepat@users.noreply.github.com>
2025-02-13 22:07:20 +0100

    db: speedup SnapshotRepository reopening

--- silkworm/db/datastore/snapshots/snapshot_repository.hpp
+++ silkworm/db/datastore/snapshots/snapshot_repository.hpp
@@ -23,6 +23,7 @@
 #include <mutex>
 #include <optional>
 #include <ranges>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -109,7 +110,7 @@ class SnapshotRepository : public SnapshotRepositoryROAccess {
     Step max_end_step() const;
 
     SnapshotPathList get_files(std::string_view ext) const;
-    std::vector<StepRange> list_dir_file_ranges() const;
+    std::set<StepRange> list_dir_file_ranges() const;
 
     bool is_stale_index_path(const SnapshotPath& index_path) const;
     SnapshotPathList stale_index_paths() const;

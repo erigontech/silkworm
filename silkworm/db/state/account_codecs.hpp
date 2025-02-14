306067c3c35bacdcc0af0d674ab5c802f79e8775
canepat <16927169+canepat@users.noreply.github.com>
2025-02-14 22:40:43 +0100

    db: fix account codec for self-destructed

--- silkworm/db/state/account_codecs.hpp
+++ silkworm/db/state/account_codecs.hpp
@@ -19,7 +19,6 @@
 #include <silkworm/core/types/account.hpp>
 #include <silkworm/db/datastore/kvdb/codec.hpp>
 #include <silkworm/db/datastore/snapshots/common/codec.hpp>
-#include <silkworm/infra/common/decoding_exception.hpp>
 
 #include "account_codec.hpp"
 #include "silkworm/db/util.hpp"
@@ -27,18 +26,22 @@
 namespace silkworm::db::state {
 
 struct AccountKVDBCodec : public datastore::kvdb::Codec {
-    Account value;
+    std::optional<Account> value;
     Bytes data;
 
     ~AccountKVDBCodec() override = default;
 
     datastore::kvdb::Slice encode() override {
-        data = AccountCodec::encode_for_storage_v3(value);
+        if (!value) {
+            return {};
+        }
+        data = AccountCodec::encode_for_storage_v3(*value);
         return datastore::kvdb::to_slice(data);
     }
 
     void decode(datastore::kvdb::Slice slice) override {
-        value = unwrap_or_throw(AccountCodec::from_encoded_storage_v3(datastore::kvdb::from_slice(slice)));
+        auto account = AccountCodec::from_encoded_storage_v3(datastore::kvdb::from_slice(slice));
+        if (account) value = std::move(*account);
     }
 };
 
@@ -46,21 +49,22 @@ static_assert(datastore::kvdb::EncoderConcept<AccountKVDBCodec>);
 static_assert(datastore::kvdb::DecoderConcept<AccountKVDBCodec>);
 
 struct AccountSnapshotsCodec : public snapshots::Codec {
-    Account value;
+    std::optional<Account> value;
     Bytes word;
 
     ~AccountSnapshotsCodec() override = default;
 
     ByteView encode_word() override {
-        word = AccountCodec::encode_for_storage_v3(value);
+        if (!value) {
+            return {};
+        }
+        word = AccountCodec::encode_for_storage_v3(*value);
         return word;
     }
 
     void decode_word(ByteView input_word) override {
         auto account = AccountCodec::from_encoded_storage_v3(input_word);
-        if (!account)
-            throw DecodingException{account.error(), "AccountSnapshotsCodec failed to decode Account"};
-        value = std::move(*account);
+        if (account) value = std::move(*account);
     }
 };
 

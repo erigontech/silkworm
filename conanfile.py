7016c7a19740aeeae005267147d77ea36951a9ab
lupin012 <58134934+lupin012@users.noreply.github.com.>
2025-04-03 13:42:48 +0200

    update make e conan

--- conanfile.py
+++ conanfile.py
@@ -36,6 +36,7 @@ class SilkwormRecipe(ConanFile):
         self.requires('spdlog/1.12.0')
         self.requires('sqlitecpp/3.3.0')
         self.requires('tomlplusplus/3.3.0')
+        self.requires('libdeflate/1.23')
 
     def configure(self):
         self.options['asio-grpc'].local_allocator = 'boost_container'

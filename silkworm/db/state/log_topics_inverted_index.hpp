ec2b38ada4a8665dd8791b40931d49ebcbff559c
Bartosz Zawistowski <bartosz.zawistowski@erigon.tech>
2025-04-16 12:03:28 +0200

    Adding high level wrappers for inverted indexes put queries

--- silkworm/db/state/log_topics_inverted_index.hpp
+++ silkworm/db/state/log_topics_inverted_index.hpp
@@ -3,13 +3,17 @@
 
 #pragma once
 
+#include <silkworm/db/datastore/kvdb/inverted_index_put_query.hpp>
 #include <silkworm/db/datastore/snapshots/common/raw_codec.hpp>
 #include <silkworm/db/datastore/snapshots/segment/kv_segment_reader.hpp>
 
+#include "address_codecs.hpp"
 #include "hash_decoder.hpp"
 
 namespace silkworm::db::state {
 
 using LogTopicsInvertedIndexKVSegmentReader = snapshots::segment::KVSegmentReader<HashSnapshotsDecoder, snapshots::RawDecoder<Bytes>>;
+
+using LogTopicsToInvertedIndexPutQuery = datastore::kvdb::InvertedIndexPutQuery<AddressKVDBEncoder>;
 
 }  // namespace silkworm::db::state
